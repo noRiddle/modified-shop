fea2ae1bd3bca70b4be347c9a1a819ad92f6088d
Tomcraft1980 <hallo@modified-shop.org>
2012-11-12 17:44:47 +0000

    Added r3928 (fix link) to trunk
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@3930 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/external/smarty/plugins/outputfilter.note.php
+++ includes/external/smarty/plugins/outputfilter.note.php
@@ -22,7 +22,7 @@
 function smarty_outputfilter_note($tpl_output, &$smarty) {
   global $PHP_SELF;
   
-  $cop='<div class="copyright"><a style="text-decoration:none;" '.((basename($PHP_SELF)=='index.php' && $_SERVER['QUERY_STRING']=='')?'href="http://www.modified-shop.org" target="_blank"':'href="'.xtc_href_link('copyright.php')).'"><span style="color:#B0347E;">mod</span><span style="color:#6D6D6D;">ified eCommerce Shopsoftware</span></a><span style="color:#555555;">' . '&nbsp;' . '&copy;' . date('Y') . '&nbsp;' . 'provides no warranty and is redistributable under the </span><a style="color:#555555;text-decoration:none;" href="http://www.fsf.org/licensing/licenses/gpl.txt" target="_blank">GNU General Public License</a></div>';
+  $cop='<div class="copyright"><a style="text-decoration:none;" '.((basename($PHP_SELF)=='index.php' && $_SERVER['QUERY_STRING']=='')?'href="http://www.modified-shop.org" target="_blank"':'href="'.xtc_href_link('copyright.php').'"').'><span style="color:#B0347E;">mod</span><span style="color:#6D6D6D;">ified eCommerce Shopsoftware</span></a><span style="color:#555555;">' . '&nbsp;' . '&copy;' . date('Y') . '&nbsp;' . 'provides no warranty and is redistributable under the </span><a style="color:#555555;text-decoration:none;" href="http://www.fsf.org/licensing/licenses/gpl.txt" target="_blank">GNU General Public License</a></div>';
 
   // making output W3C-Conform: replace ampersands, rest is covered by the modified shopstat_functions.php  
   $tpl_output = preg_replace("/&(?!(amp;|&|#[0-9]+;|[a-z0-9]+;))/i", "&amp;", $tpl_output);
