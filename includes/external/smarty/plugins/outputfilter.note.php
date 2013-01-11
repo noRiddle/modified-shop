6b090e88fa1421fca6f3c0dc0254de64e55f4078
Tomcraft1980 <hallo@modified-shop.org>
2013-01-11 14:36:07 +0000

    Added r4212 & r4238 from SP1e to trunk
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@4246 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/external/smarty/plugins/outputfilter.note.php
+++ includes/external/smarty/plugins/outputfilter.note.php
@@ -22,7 +22,7 @@
 function smarty_outputfilter_note($tpl_output, &$smarty) {
   global $PHP_SELF;
   
-  $cop='<div class="copyright"><a '.((basename($PHP_SELF)=='index.php' && $_SERVER['QUERY_STRING']=='')?'href="http://www.modified-shop.org" target="_blank"':'href="'.xtc_href_link('copyright.php').'"').'><span class="cop_magenta">mod</span><span class="cop_grey">ified eCommerce Shopsoftware</span></a><span class="cop_gpl">' . '&nbsp;' . '&copy;2009-' . date('Y') . '&nbsp;' . 'provides no warranty and is redistributable under the '.((basename($PHP_SELF)=='index.php' && $_SERVER['QUERY_STRING']=='')?'<a class="cop_gpl" href="http://www.gnu.org/licenses/gpl-2.0.html" target="_blank">GNU General Public License (Version 2)</a>':'GNU General Public License (Version 2)').'</span></div>';
+  $cop='<div class="copyright">'.((basename($PHP_SELF)=='index.php' && $_SERVER['QUERY_STRING']=='')?'<a href="http://www.modified-shop.org" target="_blank">':'').'<span class="cop_magenta">mod</span><span class="cop_grey">ified eCommerce Shopsoftware &copy; 2009-' . date('Y') . '</span>'.((basename($PHP_SELF)=='index.php' && $_SERVER['QUERY_STRING']=='')?'</a>':'').'</div>';
 
   // making output W3C-Conform: replace ampersands, rest is covered by the modified shopstat_functions.php  
   $tpl_output = preg_replace("/&(?!(amp;|&|#[0-9]+;|[a-z0-9]+;))/i", "&amp;", $tpl_output);
@@ -32,4 +32,10 @@ function smarty_outputfilter_note($tpl_output, &$smarty) {
 
   return $tpl_output.$cop;
 }
+
+   # SIE SIND IM BEGRIFF ETWAS ZU ÄNDERN, WAS NICHT FAIR IST. SIE MÖCHTEN MIT
+   # DIESER SOFTWARE GELD VERDIENEN ODER KUNDEN GEWINNEN. SIE HABEN NICHT STUNDEN 
+   # UND MONATE VERBRACHT DIESE SOFTWARE ZU ENTWICKELN UND ZU VERBESSEREN. ALS
+   # DANKESCHÖN AN DIE ENTWICKLER UND CODER LASSEN SIE DIESE DATEI, WIE SIE IST 
+   # ODER KRATZEN SIE AUCH VON IHREN ELEKTROGERÄTEN IM HAUS DIE MARKENZEICHEN AB!!!!
 ?>
