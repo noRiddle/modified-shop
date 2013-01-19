1fdbcfe726b2f31c42eecbd89903e62ae391b780
Tomcraft1980 <hallo@modified-shop.org>
2013-01-19 15:38:02 +0000

    Revised masterpayment_callback.php language files in /tags/modified-shop-1.06 and trunk (props Der_Micha)
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@4337 79ac0487-9261-4f26-a34d-858dee0dcce0

--- lang/english/masterpayment_callback.php
+++ lang/english/masterpayment_callback.php
@@ -1,11 +1,11 @@
 <?php
 $_masterpaymentCallbackMessages = array();
 
-$_masterpaymentCallbackMessages['SUCCESS'] = 'The transaction has been completed successfully.';
-$_masterpaymentCallbackMessages['PENDING'] = 'Die Transaktion wurde erfolgreich durchgeführt. Auf die Aktivierung der Transaktion durch den Händler wird gewartet.';
-$_masterpaymentCallbackMessages['SCHEDULED'] = 'Die Transaktion wurde vom Händler aktiviert.';
-$_masterpaymentCallbackMessages['REVOKED'] = 'Der Händler hat die Transaktion widerrufen / abgebrochen.';
-$_masterpaymentCallbackMessages['REFUSED_RISK'] = 'Die Transaktion wurde leider abgelehnt.';
-$_masterpaymentCallbackMessages['FAILED'] = 'Die Transaktion ist fehlgeschlagen.';
-$_masterpaymentCallbackMessages['CANCELLED'] = 'Die Transaktion wurde abgebrochen.';
+$_masterpaymentCallbackMessages['SUCCESS'] = 'The transaction was completed successfully.';
+$_masterpaymentCallbackMessages['PENDING'] = 'The transaction is pendung. Waiting for the transaction to be activated by the merchant.';
+$_masterpaymentCallbackMessages['SCHEDULED'] = 'The transaction has been activated.';
+$_masterpaymentCallbackMessages['REVOKED'] = 'The transaction has been revoked.';
+$_masterpaymentCallbackMessages['REFUSED_RISK'] = 'The transaction was refused.';
+$_masterpaymentCallbackMessages['FAILED'] = 'The transaction has failed.';
+$_masterpaymentCallbackMessages['CANCELLED'] = 'The transaction has been canceled.';
 ?>
--- lang/german/masterpayment_callback.php
+++ lang/german/masterpayment_callback.php
@@ -1,10 +1,10 @@
 <?php
 $_masterpaymentCallbackMessages = array();
 
-$_masterpaymentCallbackMessages['SUCCESS'] = 'The transaction has been completed successfully.';
-$_masterpaymentCallbackMessages['PENDING'] = 'Die Transaktion wurde erfolgreich durchgeführt. Auf die Aktivierung der Transaktion durch den Händler wird gewartet.';
-$_masterpaymentCallbackMessages['SCHEDULED'] = 'Die Transaktion wurde vom Händler aktiviert.';
-$_masterpaymentCallbackMessages['REVOKED'] = 'Der Händler hat die Transaktion widerrufen / abgebrochen.';
+$_masterpaymentCallbackMessages['SUCCESS'] = 'Die Transaktion wurde erfolgreich durchgef&uuml;hrt.';
+$_masterpaymentCallbackMessages['PENDING'] = 'Die Transaktion ist in Bearbeitung. Auf die Aktivierung der Transaktion durch den H&auml;ndler wird gewartet.';
+$_masterpaymentCallbackMessages['SCHEDULED'] = 'Die Transaktion wurde vom H&auml;ndler aktiviert.';
+$_masterpaymentCallbackMessages['REVOKED'] = 'Der H&auml;ndler hat die Transaktion widerrufen / abgebrochen.';
 $_masterpaymentCallbackMessages['REFUSED_RISK'] = 'Die Transaktion wurde leider abgelehnt.';
 $_masterpaymentCallbackMessages['FAILED'] = 'Die Transaktion ist fehlgeschlagen.';
 $_masterpaymentCallbackMessages['CANCELLED'] = 'Die Transaktion wurde abgebrochen.';
