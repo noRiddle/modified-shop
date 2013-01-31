cb77230a68a61727f9ff84384c0d9da26b7244d1
Tomcraft1980 <hallo@modified-shop.org>
2013-01-31 20:07:08 +0000

    Added r4384 from /tags/modified-shop-1.06 to trunk
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@4386 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/includes/head.php
+++ admin/includes/head.php
@@ -17,7 +17,7 @@
    --------------------------------------------------------------*/
   defined( '_VALID_XTC' ) or die( 'Direct Access to this location is not allowed.' );
 ?>
-<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html <?php echo HTML_PARAMS; ?>>
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=<?php echo $_SESSION['language_charset']; ?>">
