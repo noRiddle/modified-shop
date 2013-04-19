1db0283525f9ef981636eb69688e55b8db65f38f
Tomcraft1980 <hallo@modified-shop.org>
2013-04-19 16:17:07 +0000

    Release tag for 1.06 rev 4642
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@4642 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/includes/version.php
+++ admin/includes/version.php
@@ -3,4 +3,4 @@
 defined( '_VALID_XTC' ) or die( 'Direct Access to this location is not allowed.' );
 
 // Define the project version
-define('PROJECT_VERSION', 'modified eCommerce Shopssoftware v1.06 dated: 2013-01-14');
+define('PROJECT_VERSION', 'modified eCommerce Shopssoftware v1.06 rev 4642 dated: 2013-04-20');
