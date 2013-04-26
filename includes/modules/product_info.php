bd52fd8825780f28cda0408036884097b8ab5719
Tomcraft1980 <hallo@modified-shop.org>
2013-04-26 15:56:59 +0000

    Bugfixes Klarna payment module
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@4650 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/modules/product_info.php
+++ includes/modules/product_info.php
@@ -45,7 +45,9 @@ if (!is_object($product) || !$product->isProduct()) {
 
   $info_smarty = new Smarty;
   $info_smarty->assign('tpl_path', 'templates/'.CURRENT_TEMPLATE.'/');
-  include_once(DIR_WS_INCLUDES.'modules/payment/klarna/display_klarna_price.php'); // Klarna payment module integration
+  if(strpos(MODULE_PAYMENT_INSTALLED, 'klarna_partPayment.php') !== false && strpos($_SESSION['customers_status']['customers_status_payment_unallowed'], 'klarna_partPayment') === false){
+    include_once(DIR_WS_INCLUDES.'modules/payment/klarna/display_klarna_price.php'); // Klarna payment module integration
+  }
 
   // defaults
   $hide_qty = 0;
