c2cfeac46b55b87e7a796daa7b585d2966ceea57
Tomcraft1980 <hallo@modified-shop.org>
2013-05-03 12:46:58 +0000

    Added r1570 from trunk to /tags/modified-shop-1.06
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@4695 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/header.php
+++ includes/header.php
@@ -49,7 +49,7 @@ if(strpos(MODULE_PAYMENT_INSTALLED, 'shopgate.php') !== false && strpos($_SESSIO
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" <?php echo HTML_PARAMS; ?>>
+<html <?php echo HTML_PARAMS; ?>>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=<?php echo $_SESSION['language_charset']; ?>" /> 
 <meta http-equiv="Content-Style-Type" content="text/css" />
