31fc9d460b2d1cc9539a51c17263e2a7dc22f331
modGTB <gerhard@modified-shop.org>
2013-06-02 13:34:25 +0000

    Fix Link using function get_all_get_params
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@4837 79ac0487-9261-4f26-a34d-858dee0dcce0

--- templates/xtc5/source/boxes/languages.php
+++ templates/xtc5/source/boxes/languages.php
@@ -18,10 +18,7 @@
   // include needed functions
   require_once(DIR_FS_INC . 'xtc_get_all_get_params.inc.php');
 
-  //BOF - 2010-02-28 - Fix Undefined variable: lng
-  //if (!isset($lng) && !is_object($lng)) {
   if (!isset($lng) || !is_object($lng)) {
-  //EOF - 2010-02-28 - Fix Undefined variable: lng
     include(DIR_WS_CLASSES . 'language.php');
     $lng = new language;
   }
@@ -30,33 +27,23 @@
   $count_lng='';
   reset($lng->catalog_languages);
   while (list($key, $value) = each($lng->catalog_languages)) {
-  $count_lng++;
-	//BOF - Dokuman - 2010-01-29 - code optimization
- 	//$languages_string .= ' <a href="' . xtc_href_link(basename($PHP_SELF), 'language=' . $key.'&'.xtc_get_all_get_params(array('language', 'currency')), $request_type) . '">' . xtc_image('lang/' .  $value['directory'] .'/' . $value['image'], $value['name']) . '</a> ';
-    if (SEARCH_ENGINE_FRIENDLY_URLS == 'true') {
-      $languages_string .= ' <a href="' . xtc_href_link(basename($PHP_SELF), 'language=' . $key.'&'.xtc_get_all_get_params(array('language', 'currency')), $request_type) . '">' . xtc_image('lang/' .  $value['directory'] .'/' . $value['image'], $value['name']) . '</a> ';
-    } else {
-      $languages_string .= ' <a href="' . xtc_href_link(basename($PHP_SELF), 'language=' . $key.'&amp;'.xtc_get_all_get_params(array('language', 'currency')), $request_type) . '">' . xtc_image('lang/' .  $value['directory'] .'/' . $value['image'], $value['name']) . '</a> ';
-    }
-	//EOF - Dokuman - 2010-01-29 - code optimization
+    $count_lng++;
+    $languages_string .= ' <a href="' . xtc_href_link(basename($PHP_SELF), xtc_get_all_get_params(array('language', 'currency')) . 'language=' . $key, $request_type) . '">' . xtc_image('lang/' .  $value['directory'] .'/' . $value['image'], $value['name']) . '</a> ';
   }
 
   // dont show box if there's only 1 language
   if ($count_lng > 1 ) {
 
- $box_smarty = new smarty;
- $box_smarty->assign('tpl_path','templates/'.CURRENT_TEMPLATE.'/'); 
- $box_content='';
- $box_smarty->assign('BOX_CONTENT', $languages_string);
- $box_smarty->assign('language', $_SESSION['language']);
+    $box_smarty = new smarty;
+    $box_smarty->assign('tpl_path','templates/'.CURRENT_TEMPLATE.'/'); 
+    $box_content='';
+    $box_smarty->assign('BOX_CONTENT', $languages_string);
+    $box_smarty->assign('language', $_SESSION['language']);
 
-
-    	  // set cache ID
-
-  $box_smarty->caching = 0;
-  $box_languages= $box_smarty->fetch(CURRENT_TEMPLATE.'/boxes/box_languages.html');
-  	
+    // set cache ID
+    $box_smarty->caching = 0;
+    $box_languages= $box_smarty->fetch(CURRENT_TEMPLATE.'/boxes/box_languages.html');
 
     $smarty->assign('box_LANGUAGES',$box_languages);
   }
-   ?>
+?>
