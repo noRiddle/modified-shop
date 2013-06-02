d27a27d11b633b4e578634d9e4a3caf230f1d78e
Tomcraft1980 <hallo@modified-shop.org>
2013-06-02 19:38:39 +0000

    Bugfix on r4840
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@4841 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/specials.php
+++ admin/specials.php
@@ -203,11 +203,10 @@ require (DIR_WS_INCLUDES.'head.php');
               $new_price=xtc_round($new_price,PRICE_PRECISION);              
               ?>
                   <td>
-                  <form name="new_special" <?php echo 'action="' . xtc_href_link(FILENAME_SPECIALS, xtc_get_all_get_params(array('action', 'info', 'sID')) . 'action=' . $form_action, 'NONSSL') . '"'; ?> method="post">
-                    <?php
-                    xtc_draw_form('new_special', FILENAME_SPECIALS, xtc_get_all_get_params(array('action', 'info', 'sID')) . 'action=' . $form_action);
-                    if ($form_action == 'update') echo xtc_draw_hidden_field('specials_id', $sID);
-                    ?>
+									<?php
+									xtc_draw_form('new_special', FILENAME_SPECIALS, xtc_get_all_get_params(array('action', 'info', 'sID')) . 'action=' . $form_action);
+									if ($form_action == 'update') echo xtc_draw_hidden_field('specials_id', $sID);
+									?>
                     echo xtc_draw_hidden_field('products_up_id', $sInfo->products_id);
                     ?>
                     <br />
