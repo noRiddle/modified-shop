f16ae64f02719c1b0850ee86aac0c4a0cf56d2cc
Tomcraft1980 <hallo@modified-shop.org>
2013-06-05 19:30:42 +0000

    Added r4871 from /tags/modified-shop-1.06 to trunk
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@4872 79ac0487-9261-4f26-a34d-858dee0dcce0

--- checkout_payment.php
+++ checkout_payment.php
@@ -145,11 +145,11 @@ $module_smarty = new Smarty;
 $order_total = $xtPrice->xtcFormat($order->info['total'],false); //web28 2012-04-27 - rounded $order_total
 if ($order_total > 0) {
   if (isset ($_GET['payment_error']) && is_object(${ $_GET['payment_error'] }) && ($error = ${$_GET['payment_error']}->get_error())) {
-    $smarty->assign('error', htmlspecialchars($error['error']));
+    $smarty->assign('error', '<p class="errormessage">'. htmlspecialchars($error['error']).'</p>');
   }
   // BOF - Tomcraft - 2009-10-03 - Paypal Express Modul
   if(isset($_SESSION['reshash']['FORMATED_ERRORS'])) {
-    $smarty->assign('error', $_SESSION['reshash']['FORMATED_ERRORS']);
+    $smarty->assign('error', '<p class="errormessage">'. $_SESSION['reshash']['FORMATED_ERRORS'].'</p>');
   }
   // EOF - Tomcraft - 2009-10-03 - Paypal Express Modul
   $selection = $payment_modules->selection();
