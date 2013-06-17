5e0fa26f58be323f8f655705114c145fc30fad36
Tomcraft1980 <hallo@modified-shop.org>
2013-06-17 11:50:32 +0000

    Fixed file header on r4942
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@4944 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/includes/modules/external/image_processing_step.php
+++ admin/includes/modules/external/image_processing_step.php
@@ -2,10 +2,10 @@
 /* -----------------------------------------------------------------------------------------
    $Id: image_processing_step.php 2992 2012-06-07 16:59:49Z web28 $
 
-   xtcModified - community made shopping
-   http://www.xtc-modified.org
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
 
-   Copyright (c) 2010 xtcModified
+   Copyright (c) 2009 - 2013 [www.modified-shop.org]
    -----------------------------------------------------------------------------------------
    based on:
    (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
