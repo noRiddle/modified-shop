16486c21d49cf1749c0f8421291ccfcbfb7491f9
creopard <support@creopard.de>
2013-06-28 10:37:42 +0000

    Check for defined constant MODULE_PAYMENT_INSTALLED
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@4988 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/header.php
+++ includes/header.php
@@ -42,7 +42,7 @@ elseif (isset($error) && ($error == CATEGORIE_NOT_FOUND || $error == TEXT_PRODUC
 }
 
 /******** SHOPGATE **********/
-if(strpos(MODULE_PAYMENT_INSTALLED, 'shopgate.php') !== false && strpos($_SESSION['customers_status']['customers_status_payment_unallowed'], 'shopgate') === false){
+if(defined('MODULE_PAYMENT_INSTALLED') && strpos(MODULE_PAYMENT_INSTALLED, 'shopgate.php') !== false && strpos($_SESSION['customers_status']['customers_status_payment_unallowed'], 'shopgate') === false){
   include_once (DIR_FS_EXTERNAL.'shopgate/base/includes/header.php');
 }
 /******** SHOPGATE **********/
@@ -54,7 +54,7 @@ if(strpos(MODULE_PAYMENT_INSTALLED, 'shopgate.php') !== false && strpos($_SESSIO
 <meta http-equiv="Content-Style-Type" content="text/css" />
 <?php
 /******** SHOPGATE **********/
-if(strpos(MODULE_PAYMENT_INSTALLED, 'shopgate.php') !== false && strpos($_SESSION['customers_status']['customers_status_payment_unallowed'], 'shopgate') === false){
+if(defined('MODULE_PAYMENT_INSTALLED') && strpos(MODULE_PAYMENT_INSTALLED, 'shopgate.php') !== false && strpos($_SESSION['customers_status']['customers_status_payment_unallowed'], 'shopgate') === false){
   echo $shopgateJsHeader;
 }
 /******** SHOPGATE **********/
