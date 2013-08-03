d6f892e8dda0b4f9582a9650f751545f071855bf
Tomcraft1980 <hallo@modified-shop.org>
2013-08-03 14:42:58 +0000

    Revised r5297
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@5303 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/includes/configuration_installer.php
+++ admin/includes/configuration_installer.php
@@ -44,10 +44,6 @@ $values_group_update = array();
 //configuration_group_id 9 --- "Lagerverwaltungs Optionen"
   $values[] = "(NULL, 'STOCK_CHECKOUT_UPDATE_PRODUCTS_STATUS', 'true', '9', '20', NULL, NOW(), NULL, 'xtc_cfg_select_option(array(\'true\', \'false\'),');";
 //configuration_group_id 10 --- "Logging Optionen"
-  $values_update[] = array (
-                           'values' => "configuration_group_id = '10', configuration_value = 'query.log'",
-                           'configuration_key' => 'STORE_PAGE_PARSE_TIME_LOG'
-                           );
 
 //configuration_group_id 11 --- "Cache Optionen"
 
