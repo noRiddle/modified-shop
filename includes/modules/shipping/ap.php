fe95eedf60c282b0a79388419540e4a3f384bea4
Tomcraft1980 <hallo@modified-shop.org>
2013-08-05 19:10:00 +0000

    Bugfix on r5035 / r5038
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@5328 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/modules/shipping/ap.php
+++ includes/modules/shipping/ap.php
@@ -38,7 +38,7 @@
 
       if ( ($this->enabled == true) && ((int)MODULE_SHIPPING_AP_ZONE > 0) && is_object($order) ) {
         $check_flag = false;
-        $check_query = xtc_db_query("select zone_id from " . TABLE_AP_TO_GEO_ZONES . " where geo_zone_id = '" . MODULE_SHIPPING_AP_ZONE . "' and zone_country_id = '" . $order->delivery['country']['id'] . "' order by zone_id");
+        $check_query = xtc_db_query("select zone_id from " . TABLE_ZONES_TO_GEO_ZONES . " where geo_zone_id = '" . MODULE_SHIPPING_AP_ZONE . "' and zone_country_id = '" . $order->delivery['country']['id'] . "' order by zone_id");
         while ($check = xtc_db_fetch_array($check_query)) {
           if ($check['zone_id'] < 1) {
             $check_flag = true;
