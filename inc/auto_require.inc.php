3ad2d23c336e0a432cb287c66c28dece2b44081d
creopard <support@creopard.de>
2013-08-23 09:57:55 +0000

    Fix Strict Standard using end()-function
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@5412 79ac0487-9261-4f26-a34d-858dee0dcce0

--- inc/auto_require.inc.php
+++ inc/auto_require.inc.php
@@ -15,7 +15,12 @@ function auto_require($dir, $ext='php')
     //echo $dir.'<br>';
     if ($extra_files = @scandir($dir)) {            
         foreach ($extra_files as $filename) {
-            $file_ext = end(explode('.',$filename));
+            // BOF - DokuMan - 2013-08-23 fix: "Strict Standards: Only variables should be passed by reference"
+            //$file_ext = end(explode('.',$filename));
+            //The problem is, that end() requires a reference, because it modifies the internal representation of the array
+            $filename_array = explode('.',$filename);
+            $file_ext = end($filename_array);
+            // EOF - DokuMan - 2013-08-23 fix: "Strict Standards: Only variables should be passed by reference"
             if ($file_ext == $ext) { 
                 require($dir . $filename);           
             }
