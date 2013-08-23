a8371fd5ddf063f6080116d52ffa47a047165834
creopard <support@creopard.de>
2013-08-23 12:40:37 +0000

    Check for defined constant MODULE_PAYMENT_INSTALLED
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@5420 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/modules/product_info.php
+++ includes/modules/product_info.php
@@ -23,7 +23,7 @@
    ---------------------------------------------------------------------------------------*/
 
 /******* SHOPGATE **********/
-if(MODULE_PAYMENT_SHOPGATE_STATUS=='True' && strpos($_SESSION['customers_status']['customers_status_payment_unallowed'], 'shopgate') === false){
+if(defined('MODULE_PAYMENT_SHOPGATE_STATUS') && MODULE_PAYMENT_SHOPGATE_STATUS=='True' && strpos($_SESSION['customers_status']['customers_status_payment_unallowed'], 'shopgate') === false){
   include_once DIR_FS_EXTERNAL.'shopgate/base/includes/modules/product_info.php';
 }
 /******* SHOPGATE **********/
@@ -45,7 +45,7 @@ if (!is_object($product) || !$product->isProduct()) {
 
   $info_smarty = new Smarty;
   $info_smarty->assign('tpl_path', DIR_WS_BASE.'templates/'.CURRENT_TEMPLATE.'/');
-  if(MODULE_PAYMENT_KLARNA_PARTPAYMENT_STATUS=='True' && strpos($_SESSION['customers_status']['customers_status_payment_unallowed'], 'klarna_partPayment') === false){
+  if(defined('MODULE_PAYMENT_KLARNA_PARTPAYMENT_STATUS') && MODULE_PAYMENT_KLARNA_PARTPAYMENT_STATUS=='True' && strpos($_SESSION['customers_status']['customers_status_payment_unallowed'], 'klarna_partPayment') === false){
     include_once(DIR_WS_INCLUDES.'modules/payment/klarna/display_klarna_price.php'); // Klarna payment module integration
   }
 
