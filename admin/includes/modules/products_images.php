8949ecd79dbc010e596af6a8beb5bb978f6d6879
web28 <web28@modified-shop.org>
2013-08-27 17:32:23 +0000

    new layout for images upload
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@5439 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/includes/modules/products_images.php
+++ admin/includes/modules/products_images.php
@@ -15,28 +15,57 @@ defined('_VALID_XTC') or die('Direct Access to this location is not allowed.');
 //include needed functions
 require_once (DIR_FS_INC.'xtc_get_products_mo_images.inc.php');
 
+//clearstatcache();
+
 // show images
 if ($_GET['action'] == 'new_product') {
 
   echo '<div class="div_box">';
-
-  // display images fields:
+  // display images fields:  
+  $rowspan = ' rowspan="'. 3 ).'"';
+  ?>
+  <table class="tableConfig borderall">
+    <tr>
+      <td class="dataTableConfig col-left"><?php echo TEXT_PRODUCTS_IMAGE; ?></td>
+      <td class="dataTableConfig col-middle"><?php echo $pInfo->products_image; ?></td>
+      <td class="dataTableConfig col-right"<?php echo $rowspan;?>><?php echo $pInfo->products_image ? xtc_image(DIR_WS_CATALOG_INFO_IMAGES.$pInfo->products_image, 'Standard Image') :xtc_draw_separator('pixel_trans.gif', PRODUCT_IMAGE_INFO_WIDTH, 10); ?></td>
+    </tr>
+    <tr>
+      <td class="dataTableConfig col-left"><?php echo TEXT_PRODUCTS_IMAGE; ?></td>
+      <td class="dataTableConfig col-middle"><?php echo xtc_draw_file_field('products_image', false, 'class="imgupload"'); ?></td>      
+    </tr>    
+    <tr>
+      <td class="dataTableConfig col-left"><?php echo TEXT_DELETE; ?></td>
+      <td class="dataTableConfig col-middle"><?php echo xtc_draw_selection_field('del_pic', 'checkbox', $pInfo->products_image); ?></td>      
+    </tr>
+  </table>
   
-  if ($pInfo->products_image) {
-    echo '<div class="main txta-c flt-l mrg5" style="width:'. (PRODUCT_IMAGE_THUMBNAIL_WIDTH + 15).'px;">'.xtc_image(DIR_WS_CATALOG_THUMBNAIL_IMAGES.$pInfo->products_image, 'Standard Image').'<br />'.xtc_draw_selection_field('del_pic', 'checkbox', $pInfo->products_image).' '.TEXT_DELETE.'</div>';
-  }
-  echo '<div class="main flt-l mrg5">'.TEXT_PRODUCTS_IMAGE.'<br />'.xtc_draw_file_field('products_image').'<br />'.'&nbsp;'.$pInfo->products_image.xtc_draw_hidden_field('products_previous_image_0', $pInfo->products_image).'</div>';
-
+  <?php
+  echo xtc_draw_hidden_field('products_previous_image_0', $pInfo->products_image);
+  
   // display MO PICS
   if (MO_PICS > 0) {
     $mo_images = xtc_get_products_mo_images($pInfo->products_id);
     for ($i = 0; $i < MO_PICS; $i ++) {
-      echo '<div class="clear">'.xtc_draw_separator('pixel_black.gif', '100%', '1').'</div>'.PHP_EOL;
-     
-      if ($mo_images[$i]["image_name"]) {
-        echo '<div class="main txta-c flt-l mrg5" style="width:'. (PRODUCT_IMAGE_THUMBNAIL_WIDTH + 15).'px;">'.xtc_image(DIR_WS_CATALOG_THUMBNAIL_IMAGES.$mo_images[$i]["image_name"], 'Image '. ($i +1)).'<br />'.xtc_draw_selection_field('del_mo_pic[]', 'checkbox', $mo_images[$i]["image_name"]).' '.TEXT_DELETE.'</div>';
-      } 
-      echo '<div class="main flt-l mrg5">'.TEXT_PRODUCTS_IMAGE.' '. ($i +1).'<br />'.xtc_draw_file_field('mo_pics_'.$i).'<br />'.xtc_draw_separator('pixel_trans.gif', '24', '15').'&nbsp;'.$mo_images[$i]["image_name"].xtc_draw_hidden_field('products_previous_image_'. ($i +1), $mo_images[$i]["image_name"]).'</div>';
+      ?>
+      <div class="clear">&nbsp;</div>
+      <table class="tableConfig borderall">
+        <tr>
+          <td class="dataTableConfig col-left"><?php echo TEXT_PRODUCTS_IMAGE.' '. ($i +1); ?></td>
+          <td class="dataTableConfig col-middle"><?php echo $mo_images[$i]['image_name']; ?></td>
+          <td class="dataTableConfig col-right"<?php echo $rowspan;?>><?php echo $mo_images[$i]['image_name'] ? xtc_image(DIR_WS_CATALOG_INFO_IMAGES.$mo_images[$i]['image_name'], 'Image '. ($i +1)) : xtc_draw_separator('pixel_trans.gif', PRODUCT_IMAGE_INFO_WIDTH, 10); ?></td>
+        </tr>
+        <tr>
+          <td class="dataTableConfig col-left"><?php echo TEXT_PRODUCTS_IMAGE.' '. ($i +1); ?></td>
+          <td class="dataTableConfig col-middle"><?php echo xtc_draw_file_field('mo_pics_'.$i, false, 'class="imgupload"'); ?></td>      
+        </tr>        
+        <tr>
+          <td class="dataTableConfig col-left"><?php echo TEXT_DELETE; ?></td>
+          <td class="dataTableConfig col-middle"><?php echo xtc_draw_selection_field('del_mo_pic[]', 'checkbox', $mo_images[$i]['image_name']); ?></td>      
+        </tr>
+      </table>
+      <?php
+      echo xtc_draw_hidden_field('products_previous_image_'. ($i +1), $mo_images[$i]['image_name']);
     }
   }
   echo '<div style="clear:both;"></div>';
