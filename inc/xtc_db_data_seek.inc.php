606b0975fefe6fdaea5da6af9d2c7cc90774e4f2
Tomcraft1980 <hallo@modified-shop.org>
2013-09-03 14:07:28 +0000

    Fixed file header on r5463
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@5468 79ac0487-9261-4f26-a34d-858dee0dcce0

--- inc/xtc_db_data_seek.inc.php
+++ inc/xtc_db_data_seek.inc.php
@@ -1,20 +1,21 @@
 <?php
 /* -----------------------------------------------------------------------------------------
-   $Id$   
+   $Id$
 
-   XT-Commerce - community made shopping
-   http://www.xt-commerce.com
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
 
-   Copyright (c) 2003 XT-Commerce
+   Copyright (c) 2009 - 2013 [www.modified-shop.org]
    -----------------------------------------------------------------------------------------
-   based on: 
+   based on:
    (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
-   (c) 2002-2003 osCommerce(database.php,v 1.19 2003/03/22); www.oscommerce.com 
-   (c) 2003	 nextcommerce (xtc_db_data_seek.inc.php,v 1.3 2003/08/13); www.nextcommerce.org
+   (c) 2002-2003 osCommerce(database.php,v 1.19 2003/03/22); www.oscommerce.com
+   (c) 2003 nextcommerce (xtc_db_data_seek.inc.php,v 1.3 2003/08/13); www.nextcommerce.org
+   (c) 2006 XT-Commerce (xtc_db_data_seek.inc.php 899 2005-04-29 02);
 
-   Released under the GNU General Public License 
+   Released under the GNU General Public License
    ---------------------------------------------------------------------------------------*/
-   
+
   function xtc_db_data_seek($db_query, $row_number,$cq=false) {
 
     if (defined('DB_CACHE') && DB_CACHE == 'true' && $cq) { //Dokuman - 2011-02-11 - check for defined DB_CACHE
