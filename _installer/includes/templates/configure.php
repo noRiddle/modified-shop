80ebbbd4affe64a80758780f4880c68dba1fe336
web28 <web28@modified-shop.org>
2013-09-11 17:08:56 +0000

    change configure template
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@5686 79ac0487-9261-4f26-a34d-858dee0dcce0

--- _installer/includes/templates/configure.php
+++ _installer/includes/templates/configure.php
@@ -27,13 +27,8 @@ $file_contents =
 '  define(\'DIR_WS_CATALOG\', \'' . $_POST['DIR_WS_CATALOG'] . '\'); // relative path required' . PHP_EOL .
 '  define(\'DIR_FS_CATALOG\', DIR_FS_DOCUMENT_ROOT);' . PHP_EOL .
 '' . PHP_EOL .
-'// defines for admin' . PHP_EOL .
-'  define(\'HTTP_CATALOG_SERVER\', HTTP_SERVER);' . PHP_EOL .
-'  define(\'HTTPS_CATALOG_SERVER\', HTTPS_SERVER);' . PHP_EOL .
-'' . PHP_EOL .
 '// secure SSL' . PHP_EOL .
 '  define(\'ENABLE_SSL\', ' . (($_POST['ENABLE_SSL'] == 'true') ? 'true' : 'false') . '); // secure webserver for checkout procedure?' . PHP_EOL .
-'  define(\'ENABLE_SSL_CATALOG\', ((ENABLE_SSL === true) ? \'true\' : \'false\'));' . PHP_EOL .
 '  define(\'USE_SSL_PROXY\', ' . (($_POST['USE_SSL_PROXY'] == 'true') ? 'true' : 'false') . '); // using SSL proxy?' . PHP_EOL .
 '' . PHP_EOL .
 '// define our database connection' . PHP_EOL .
@@ -47,13 +42,10 @@ $file_contents =
 '  define(\'DB_SERVER_CHARSET\', \'' . DB_SERVER_CHARSET . '\'); // set db charset \'utf8\' or \'latin1\'' . PHP_EOL . '?>';
 '' . PHP_EOL .
 '  if (DB_DATABASE != \'\') {' . PHP_EOL . 
-'  // admin directory' . PHP_EOL . 
-'    require_once (DIR_FS_CATALOG . \'inc/set_admin_directory.inc.php\');' . PHP_EOL .
-'    set_admin_directory();' . PHP_EOL .
-'' . PHP_EOL .
 '  // include standard settings' . PHP_EOL .
 '    if (defined(\'RUN_MODE_ADMIN\')) {' . PHP_EOL .
-'      require (DIR_FS_CATALOG.DIR_ADMIN.\'includes/paths.php\');' . PHP_EOL .
+'      define(\'DIR_FS_ADMIN\', realpath(dirname(__FILE__) . \'/../\') . \'/\');' . PHP_EOL .
+'      require (DIR_FS_ADMIN.\'includes/paths.php\');' . PHP_EOL .
 '    } else {' . PHP_EOL .
 '      require (DIR_FS_CATALOG.\'includes/paths.php\');' . PHP_EOL .
 '    }' . PHP_EOL .
