8947893f76264586c3ba2f8aee5d832bf969a2fa
modGTB <gerhard@modified-shop.org>
2013-09-11 08:51:22 +0000

    update janolaw with new functions
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@5656 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/external/application_top/janolaw.php
+++ includes/external/application_top/janolaw.php
@@ -1,10 +1,4 @@
 <?php
-
-require_once(DIR_FS_CATALOG.'includes/janolaw/janolaw.php');
-$coo_janolaw = new janolaw();
-if($coo_janolaw->get_status() == true) {
-  $coo_janolaw->get_page_content('agb', true, true, 'checkout-agb');
-  $coo_janolaw->get_page_content('datenschutzerklaerung', true, true, 'checkout-datenschutzerklaerung');
-  $coo_janolaw->get_page_content('impressum', true, true, 'checkout-impressum');
-  $coo_janolaw->get_page_content('widerrufsbelehrung', true, true, 'checkout-widerrufsbelehrung');
-}
+  require_once(DIR_FS_EXTERNAL.'janolaw/janolaw.php');
+  $janolaw = new janolaw_content();
+?>
