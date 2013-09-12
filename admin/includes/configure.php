44e212fbb340d047cad9880474b03e7424d85ec4
web28 <web28@modified-shop.org>
2013-09-12 08:07:18 +0000

    compatibility for modified eCommerce Shopsoftware 1.06 files
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@5723 79ac0487-9261-4f26-a34d-858dee0dcce0

---
+++ admin/includes/configure.php
@@ -0,0 +1,25 @@
+<?php
+/* --------------------------------------------------------------
+
+  modified eCommerce Shopsoftware
+  http://www.modified.org-shop
+
+   Copyright (c) 2009 - 2012 modified eCommerce Shopsoftware
+   Released under the GNU General Public License (Version 2)
+   [http://www.gnu.org/licenses/gpl-2.0.html]
+  --------------------------------------------------------------
+  based on:
+  (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
+  (c) 2002-2003 osCommerce (configure.php,v 1.13 2003/02/10); www.oscommerce.com
+  (c) 2003 XT-Commerce (configure.php)
+
+  Released under the GNU General Public License
+  --------------------------------------------------------------*/
+
+// compatibility for modified eCommerce Shopsoftware 1.06 files
+if (file_exists('../includes/local/configure.php')) {
+  include('../includes/local/configure.php');
+} else {
+  require('../includes/configure.php');
+}
+?>
