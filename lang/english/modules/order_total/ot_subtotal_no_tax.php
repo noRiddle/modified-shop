951fa3832083b0b6d4fdc2c5df4947e10521e92c
Tomcraft1980 <hallo@modified-shop.org>
2013-09-14 08:44:49 +0000

    Revised english language definitions for order_total modules
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@5757 79ac0487-9261-4f26-a34d-858dee0dcce0

--- lang/english/modules/order_total/ot_subtotal_no_tax.php
+++ lang/english/modules/order_total/ot_subtotal_no_tax.php
@@ -16,11 +16,11 @@
    Released under the GNU General Public License 
    ---------------------------------------------------------------------------------------*/
 
-  define('MODULE_ORDER_TOTAL_SUBTOTAL_NO_TAX_TITLE', '<b>Total, no-tax</b>');
-  define('MODULE_ORDER_TOTAL_SUBTOTAL_NO_TAX_DESCRIPTION', 'Order Sub-Total');
+  define('MODULE_ORDER_TOTAL_SUBTOTAL_NO_TAX_TITLE', '<b>Total, no tax</b>');
+  define('MODULE_ORDER_TOTAL_SUBTOTAL_NO_TAX_DESCRIPTION', 'Order subtotal');
 
-  define('MODULE_ORDER_TOTAL_SUBTOTAL_NO_TAX_STATUS_TITLE','Display Sub-Total without tax');
-  define('MODULE_ORDER_TOTAL_SUBTOTAL_NO_TAX_STATUS_DESC','Do you want to display the order sub-total cost?');
+  define('MODULE_ORDER_TOTAL_SUBTOTAL_NO_TAX_STATUS_TITLE','Display subtotal without tax');
+  define('MODULE_ORDER_TOTAL_SUBTOTAL_NO_TAX_STATUS_DESC','Do you want to display the order subtotal cost?');
 
   define('MODULE_ORDER_TOTAL_SUBTOTAL_NO_TAX_SORT_ORDER_TITLE','Sort Order');
   define('MODULE_ORDER_TOTAL_SUBTOTAL_NO_TAX_SORT_ORDER_DESC', 'Sort order of display.');
