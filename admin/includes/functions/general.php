9fddcc20724a2b31881ca8ecdf7992e44801350b
modGTB <gerhard@modified-shop.org>
2013-10-22 16:38:38 +0000

    use new function with parameter for admin
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@5955 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/includes/functions/general.php
+++ admin/includes/functions/general.php
@@ -2701,7 +2701,7 @@ function xtc_output_string($string, $translate = false, $protected = false) {
     $email_fields = '';
     for ($i=0, $n=count($languages); $i<$n; $i++) {
       $email_fields .= xtc_image(DIR_WS_LANGUAGES . $languages[$i]['directory'] .'/admin/images/'. $languages[$i]['image'], $languages[$i]['name'], '18px');
-      $email_fields .= xtc_draw_input_field(trim($parameters[1]).'[' . strtoupper($languages[$i]['code']) . ']', parse_email_language_value($parameters[0], $languages[$i]['code']), 'style="margin-left:2px; width:360px"');
+      $email_fields .= xtc_draw_input_field(trim($parameters[1]).'[' . strtoupper($languages[$i]['code']) . ']', parse_email_language_value($parameters[0], $languages[$i]['code'], true), 'style="margin-left:2px; width:360px"');
     }
     
     return $email_fields;
