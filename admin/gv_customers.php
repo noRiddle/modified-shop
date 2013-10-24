dfce6f8649c4b2fafdbc8bf7dd3edc55c4c3c741
modGTB <gerhard@modified-shop.org>
2013-10-24 09:47:05 +0000

    commit r5964 from tags to trunk
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@5965 79ac0487-9261-4f26-a34d-858dee0dcce0

---
+++ admin/gv_customers.php
@@ -0,0 +1,102 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id:$
+
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
+
+   Copyright (c) 2009 - 2013 [www.modified-shop.org]
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
+
+  require('includes/application_top.php');
+
+  require(DIR_WS_CLASSES . 'currencies.php');
+  $currencies = new currencies();
+
+require (DIR_WS_INCLUDES.'head.php');
+?>
+
+</head>
+<body marginwidth="0" marginheight="0" topmargin="0" bottommargin="0" leftmargin="0" rightmargin="0" bgcolor="#FFFFFF">
+<!-- header //-->
+<?php require(DIR_WS_INCLUDES . 'header.php'); ?>
+<!-- header_eof //-->
+<!-- body //-->
+<table border="0" width="100%" cellspacing="2" cellpadding="2">
+  <tr>
+    <td class="columnLeft2" width="<?php echo BOX_WIDTH; ?>" valign="top"><table border="0" width="<?php echo BOX_WIDTH; ?>" cellspacing="1" cellpadding="1" class="columnLeft">
+<!-- left_navigation //-->
+<?php require(DIR_WS_INCLUDES . 'column_left.php'); ?>
+<!-- left_navigation_eof //-->
+    </table></td>
+<!-- body_text //-->
+    <td class="boxCenter" width="100%" valign="top"><table border="0" width="100%" cellspacing="0" cellpadding="2">
+      <tr>
+        <td width="100%"><table border="0" width="100%" cellspacing="0" cellpadding="0">
+          <tr>
+            <td class="pageHeading"><?php echo HEADING_TITLE; ?></td>
+            <td class="pageHeading" align="right"><?php echo xtc_draw_separator('pixel_trans.gif', HEADING_IMAGE_WIDTH, HEADING_IMAGE_HEIGHT); ?></td>
+          </tr>
+        </table></td>
+      </tr>
+      <tr>
+        <td><table border="0" width="100%" cellspacing="0" cellpadding="0">
+          <tr>
+            <td valign="top"><table border="0" width="100%" cellspacing="0" cellpadding="2">
+              <tr class="dataTableHeadingRow">
+                <td class="dataTableHeadingContent"><?php echo TABLE_HEADING_GV_ID; ?></td>
+                <td class="dataTableHeadingContent"><?php echo TABLE_HEADING_GV_NAME; ?></td>
+                <td class="dataTableHeadingContent" align="center"><?php echo TABLE_HEADING_GV_AMOUNT; ?></td>
+              </tr>
+<?php
+  $gv_query_raw = "SELECT * 
+                     FROM " . TABLE_COUPON_GV_CUSTOMER . " cgc
+                     JOIN " . TABLE_CUSTOMERS . " c
+                          ON c.customers_id = cgc.customer_id";
+  $gv_split = new splitPageResults($_GET['page'], '20', $gv_query_raw, $gv_query_numrows);
+  $gv_query = xtc_db_query($gv_query_raw);
+  while ($gv_list = xtc_db_fetch_array($gv_query)) {
+    if (((!$_GET['cID']) || ($_GET['cID'] == $gv_list['customers_id'])) && (!$gInfo)) {
+    $gInfo = new objectInfo($gv_list);
+    }
+    if ( (is_object($gInfo)) && ($gv_list['coupon_id'] == $gInfo->coupon_id) ) {
+      echo '              <tr class="dataTableRowSelected" onmouseover="this.style.cursor=\'pointer\'" onclick="document.location.href=\'' . xtc_href_link(FILENAME_CUSTOMERS, 'cID=' . $gInfo->customers_id . '&action=edit') . '\'">' . "\n";
+    } else {
+      echo '              <tr class="dataTableRow" onmouseover="this.className=\'dataTableRowOver\';this.style.cursor=\'pointer\'" onmouseout="this.className=\'dataTableRow\'" onclick="document.location.href=\'' . xtc_href_link(FILENAME_CUSTOMERS, 'cID=' . $gv_list['customers_id']) . '\'">' . "\n";
+    }
+?>
+                <td class="dataTableContent"><?php echo $gv_list['customers_id']; ?></td>
+                <td class="dataTableContent"><?php echo $gv_list['customers_firstname'] . ' ' . $gv_list['customers_lastname']; ?></td>
+                <td class="dataTableContent" align="center"><?php echo $currencies->format($gv_list['amount']); ?></td>
+              </tr>
+<?php
+  }
+?>
+              <tr>
+                <td colspan="5"><table border="0" width="100%" cellspacing="0" cellpadding="2">
+                  <tr>
+                    <td class="smallText" valign="top"><?php echo $gv_split->display_count($gv_query_numrows, '20', $_GET['page'], TEXT_DISPLAY_NUMBER_OF_GIFT_VOUCHERS); ?></td>
+                    <td class="smallText" align="right"><?php echo $gv_split->display_links($gv_query_numrows, '20', MAX_DISPLAY_PAGE_LINKS, $_GET['page']); ?></td>
+                  </tr>
+                </table></td>
+              </tr>
+            </table></td>
+
+          </tr>
+        </table></td>
+      </tr>
+    </table></td>
+<!-- body_text_eof //-->
+  </tr>
+</table>
+<!-- body_eof //-->
+
+<!-- footer //-->
+<?php require(DIR_WS_INCLUDES . 'footer.php'); ?>
+<!-- footer_eof //-->
+<br />
+</body>
+</html>
+<?php require(DIR_WS_INCLUDES . 'application_bottom.php'); ?>
