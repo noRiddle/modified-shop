747f0bbaa395b3fc71c2fb024a2250e5bafdcf88
modGTB <gerhard@modified-shop.org>
2013-11-07 13:05:47 +0000

    optimize findologic api
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@6030 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/external/findologic/findologic_config.inc.php
+++ includes/external/findologic/findologic_config.inc.php
@@ -13,6 +13,7 @@
    Released under the GNU General Public License 
    ---------------------------------------------------------------------------------------*/
 
+  define('FL_FS_API', DIR_FS_CATALOG . 'api/findologic/');
   define('FL_SHOP_ID', MODULE_FINDOLOGIC_SHOP_ID);
   define('FL_SHOP_URL', HTTP_SERVER.DIR_WS_CATALOG); // Changed to static value
   define('FL_SERVICE_URL', MODULE_FINDOLOGIC_SERVICE_URL);
@@ -24,4 +25,5 @@
   define('FL_LANG', MODULE_FINDOLOGIC_LANG);
   define('CUSTOMER_GROUP', MODULE_FINDOLOGIC_CUSTOMER_GROUP);
   define('CURRENCY', MODULE_FINDOLOGIC_CURRENCY);
+
 ?>
