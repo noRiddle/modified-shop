3da3540012d41eb2f184ca3cd37fc1af866ce4b1
web28 <web28@modified-shop.org>
2013-11-08 10:47:26 +0000

    fix @6034, add missing file
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@6036 79ac0487-9261-4f26-a34d-858dee0dcce0

---
+++ includes/shipping_estimate.php
@@ -0,0 +1,156 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id: shipping_estimate.php 6035 2013-11-08 10:46:36Z web28 $
+
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
+
+   Copyright (c) 2009 - 2013 [www.modified-shop.org]
+   -----------------------------------------------------------------------------------------
+   based on:
+   (c) 2003 nextcommerce (write_customers_status.php,v 1.8 2003/08/1); www.nextcommerce.org
+   (c) 2006 xtCommerce (write_customers_status.php)
+   
+   Released under the GNU General Public License
+   --------------------------------------------------------------------------------------- 
+   
+   based on Third Party contribution:
+   Customers Status v3.x  (c) 2002-2003 Copyright Elari elari@free.fr | www.unlockgsm.com/dload-osc/ | CVS : http://cvs.sourceforge.net/cgi-bin/viewcvs.cgi/elari/?sortby=date#dirlist
+
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
+// 2011-10-30
+// http://rpa-com.de -web28- add support for //MAXIMALE VERSANDKOSTEN
+// http://neunzehn83.de/blog/2011/03/09/xtc-land-dropdown-im-warenkorb/
+// http://www.xtc-modified.org/forum/topic.php?id=9883
+
+if (!defined('SHOW_ALWAYS_LANG_DROPDOWN')) {
+  define('SHOW_ALWAYS_LANG_DROPDOWN', true); // true: Zeigt immer das Länderauswahlfeld an - false: Zeigt Länderauswahlfeld nur bei nicht eingeloggten Kunden
+}
+require_once (DIR_WS_CLASSES.'order.php');
+require_once (DIR_FS_INC.'xtc_get_country_list.inc.php');
+
+$order = new order();
+$total_weight = $_SESSION['cart']->show_weight();
+$total_count = $_SESSION['cart']->count_contents();
+  
+$selected = isset($_SESSION['customer_country_id']) ? $_SESSION['customer_country_id'] : STORE_COUNTRY;
+if (!isset($_SESSION['customer_id']) || SHOW_ALWAYS_LANG_DROPDOWN) {
+  if (isset($_SESSION['country'])) {
+    $selected = $_SESSION['country'];
+  } else {
+    //$selected = STORE_COUNTRY;
+  }
+  $module_smarty->assign('SELECT_COUNTRY', _SHIPPING_TO. xtc_get_country_list(array ('name' => 'country'), (int)$selected, 'onchange="this.form.submit()"'));
+}
+
+if (!isset($order->delivery['country']['iso_code_2']) || $order->delivery['country']['iso_code_2'] == ''  || SHOW_ALWAYS_LANG_DROPDOWN) {
+  unset($_SESSION['shipping']);
+  $delivery_zone_query = xtc_db_query("SELECT countries_id, 
+                                              countries_iso_code_2, 
+                                              countries_name 
+                                         FROM ".TABLE_COUNTRIES." 
+                                        WHERE countries_id = '". (int)$selected."'
+                                     ");
+  $delivery_zone = xtc_db_fetch_array($delivery_zone_query);
+  
+  $order->delivery['country']['iso_code_2'] = $delivery_zone['countries_iso_code_2'];
+  $order->delivery['country']['title'] = $delivery_zone['countries_name'];
+  $order->delivery['country']['id'] = $delivery_zone['countries_id'];
+}
+
+$_SESSION['delivery_zone'] = $order->delivery['country']['iso_code_2'];
+require (DIR_WS_CLASSES.'shipping.php');
+$shipping = new shipping;
+$quotes = $shipping->quote();
+
+$free_shipping = $free_shipping_freeamount = false;
+if (defined('MODULE_ORDER_TOTAL_SHIPPING_FREE_SHIPPING') && (MODULE_ORDER_TOTAL_SHIPPING_FREE_SHIPPING == 'true')) {
+  switch (MODULE_ORDER_TOTAL_SHIPPING_DESTINATION) {
+    case 'national' :
+      if ($order->delivery['country']['id'] == STORE_COUNTRY)
+        $pass = true;
+      break;
+    case 'international' :
+      if ($order->delivery['country']['id'] != STORE_COUNTRY)
+        $pass = true;
+      break;
+    case 'both' :
+      $pass = true;
+      break;
+    default :
+      $pass = false;
+      break;
+  }
+  if (($pass == true) && ($order->info['total'] >= $xtPrice->xtcFormat(MODULE_ORDER_TOTAL_SHIPPING_FREE_SHIPPING_OVER, false, 0, true))) {
+    $free_shipping = true;
+  }
+}
+$has_freeamount = false;
+foreach ($quotes as $quote) {
+  if ($quote['id'] == 'freeamount') {
+    $has_freeamount = true;
+    if (isset($quote['methods'])) {
+      $free_shipping_freeamount = true;
+      break;
+    }
+  }
+}
+include (DIR_WS_LANGUAGES.$_SESSION['language'].'/modules/order_total/ot_shipping.php');
+
+//$total = $_SESSION['cart']->show_total();
+
+$shipping_content = array ();
+if ($free_shipping == true) {
+    $shipping_content[] = array(
+        'NAME' => FREE_SHIPPING_TITLE,
+        'VALUE' => $xtPrice->xtcFormat(0, true, 0, true)
+    );
+} else if ($free_shipping_freeamount) {
+    $shipping_content[] = array(
+        'NAME' => $quote['module'] . ' - ' . $quote['methods'][0]['title'],
+        'VALUE' => $xtPrice->xtcFormat(0, true, 0, true)
+    );
+} else {
+    if ($has_freeamount) {
+      $module_smarty->assign('FREE_SHIPPING_INFO', sprintf(FREE_SHIPPING_DESCRIPTION, $xtPrice->xtcFormat(MODULE_ORDER_TOTAL_SHIPPING_FREE_SHIPPING_OVER, true, 0, true)));
+    }
+    $i = 0;
+    foreach ($quotes AS $quote) {
+      if ($quote['id'] != 'freeamount') { 
+        //BOC web28 Error Fix
+        if (trim($quote['error']) == '') {      
+          $quote['methods'][0]['cost'] = $xtPrice->xtcCalculateCurr($quote['methods'][0]['cost']);
+          $total += ((isset($quote['tax']) && $quote['tax'] > 0) ? $xtPrice->xtcAddTax($quote['methods'][0]['cost'],$quote['tax']) : (!empty($quote['methods'][0]['cost']) ? $quote['methods'][0]['cost'] : '0'));
+          $shipping_content[$i] = array(
+            'NAME' => $quote['module'] . ' - ' . $quote['methods'][0]['title'],
+            'VALUE' => $xtPrice->xtcFormat(((isset($quote['tax']) && $quote['tax'] > 0) ? $xtPrice->xtcAddTax($quote['methods'][0]['cost'],$quote['tax']) : (!empty($quote['methods'][0]['cost']) ? $quote['methods'][0]['cost'] : '0')), true)
+            );
+        } else {
+          $shipping_content[$i] = array(
+            'NAME' => $quote['error'] . ' - ' . $quote['methods'][0]['title'],
+            'VALUE' => ''
+            );
+        }
+        //EOC web28 Error Fix
+        $i++;
+      }
+    }
+}
+
+if (sizeof($quotes) < 1) {
+  $shipping_content[] = array('NAME' => _MODULE_INVALID_SHIPPING_ZONE);
+}
+if (sizeof($shipping_content) < 1) {  
+  $shipping_content[] = array('NAME' => _MODULE_UNDEFINED_SHIPPING_RATE); 
+}
+
+#unset($_SESSION['billto']);
+unset($_SESSION['delivery_zone']);
+$module_smarty->assign('shipping_content', $shipping_content);
+$module_smarty->assign('COUNTRY', $order->delivery['country']['title']);
+
+if (count($shipping_content) <= 1) {
+  $module_smarty->assign('total', $xtPrice->xtcFormat($total, true));
+}
+?>
