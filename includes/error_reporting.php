a0b5d9f100f160a97db3b113e6efc62015899ea0
modGTB <gerhard@modified-shop.org>
2013-11-23 10:15:00 +0000

    missing header
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@6079 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/error_reporting.php
+++ includes/error_reporting.php
@@ -1,4 +1,14 @@
 <?php
+/* -----------------------------------------------------------------------------------------
+   $Id$
+
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
+
+   Copyright (c) 2009 - 2013 [www.modified-shop.org]
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
 
 /**
  * Error handler, passes flow over the exception logger with new ErrorException.
