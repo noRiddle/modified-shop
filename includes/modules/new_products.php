9b68a5c6a815b1a668a204fd40cdbcebd648f3b0
modGTB <gerhard@modified-shop.org>
2014-02-11 16:09:58 +0000

    fix notice
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@6309 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/modules/new_products.php
+++ includes/modules/new_products.php
@@ -90,7 +90,7 @@ if (sizeof($module_content) >= 1) {
     $module_smarty->caching = 1;
     $module_smarty->cache_lifetime = CACHE_LIFETIME;
     $module_smarty->cache_modified_check = CACHE_CHECK;
-    $cache_id = $new_products_category_id.$_SESSION['language'].$_SESSION['customers_status']['customers_status_name'].$_SESSION['currency'];
+    $cache_id = (isset($new_products_category_id) ? $new_products_category_id : '0').$_SESSION['language'].$_SESSION['customers_status']['customers_status_name'].$_SESSION['currency'];
     if ((!isset ($new_products_category_id)) || ($new_products_category_id == '0')) {
       $module = $module_smarty->fetch(CURRENT_TEMPLATE.'/module/new_products_default.html', $cache_id);
     } else {
