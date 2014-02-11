087cdde094d399889121382067734c834de48901
modGTB <gerhard@modified-shop.org>
2014-02-11 15:58:56 +0000

    update installer
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@6307 79ac0487-9261-4f26-a34d-858dee0dcce0

--- _installer/includes/check_permissions.php
+++ _installer/includes/check_permissions.php
@@ -9,6 +9,22 @@
 
    Copyright (c) 2009 - 2013 [www.modified-shop.org]
    --------------------------------------------------------------*/
+
+  function scanDirectories($rootDir, $allData=array()) {
+    $invisibleFileNames = array(".", "..", ".svn");
+    $dirContent = scandir($rootDir);
+    foreach($dirContent as $key => $content) {
+      $path = $rootDir.'/'.$content;
+      if (!in_array($content, $invisibleFileNames)) {
+        if (is_file($path) && is_readable($path)) {
+          $allData[] = str_replace(DIR_FS_CATALOG, '', $path);
+        } elseif (is_dir($path) && is_readable($path)) {
+          $allData = scanDirectories($path, $allData);
+        }
+      }
+    }
+    return $allData;
+  }
    
   // file and folder permission checks
   $error_flag = false;
@@ -31,7 +47,6 @@
                           'dirs' => array(DIR_ADMIN.'backups',
                                           DIR_ADMIN.'images/graphs',
                                           DIR_ADMIN.'images/icons',
-                                          DIR_ADMIN.'includes/magnalister',
                                           'cache',
                                           'export',
                                           'export/idealo_realtime',
@@ -52,6 +67,8 @@
                                           'log',
                                           'templates_c',
                                      ),
+                          'rdirs' => array(DIR_ADMIN.'includes/magnalister',
+                                     ),
                           );
   
   // login as ftp user to change permissions of every file and directory
@@ -69,12 +86,18 @@
       $ftp_message = LOGIN_NOT_POSSIBLE;
     }
 
+    foreach ($files_to_check['rdirs'] as $dir) {
+      $files_to_check['files'] = scanDirectories(DIR_FS_CATALOG.$dir, $files_to_check['files']);
+    }
+
     foreach ($files_to_check as $type => $files) {
-      foreach ($files as $file) {
-        if (!ftp_site($ftp, 'CHMOD 0777 '.$path.$file)) {
-          if ($type=='files') $error_flag = true;
-          else if ($type='dirs') $folder_flag = true;
-          $ftp_message .= CHMOD_WAS_NOT_SUCCESSFUL.'<br />';
+      if ($type != 'rdirs') {
+        foreach ($files as $file) {
+          if (!ftp_site($ftp, 'CHMOD 0777 '.$path.$file)) {
+            if ($type == 'files') $error_flag = true;
+            if ($type == 'dirs') $folder_flag = true;
+            $ftp_message .= CHMOD_WAS_NOT_SUCCESSFUL.'<br />';
+          }
         }
       }
     }
@@ -96,16 +119,31 @@
   // new testing of file permissions
   foreach ($files_to_check as $type => $files) {
     foreach ($files as $file) {
-      if (!is_writeable(DIR_FS_CATALOG.$file)) {
-        if ($type=='files') {
-          $error_flag = true;
-          $file_flag = true;
-          $message .= '<strong>'.TEXT_WRONG_FILE_PERMISSION.'</strong>'.DIR_FS_CATALOG.$file.'<br />';
+      if ($type != 'rdirs') {
+        if (!is_writeable(DIR_FS_CATALOG.$file)) {
+          if ($type == 'files') {
+            $error_flag = true;
+            $file_flag = true;
+            $message .= '<strong>'.TEXT_WRONG_FILE_PERMISSION.'</strong>'.DIR_FS_CATALOG.$file.'<br />';
+          }
+          if ($type == 'dirs') {
+            $error_flag = true;
+            $folder_flag = true;
+            $message .= '<strong>'.TEXT_WRONG_FOLDER_PERMISSION.'</strong>'.DIR_FS_CATALOG.$file.'<br />';
+          }
         }
-        else if ($type='dirs') {
-          $error_flag=true;
-          $folder_flag = true;
-          $message .= '<strong>'.TEXT_WRONG_FOLDER_PERMISSION.'</strong>'.DIR_FS_CATALOG.$file.'<br />';
+      } else {
+        foreach ($files_to_check['rdirs'] as $dir) {
+          $rfiles_to_check[$dir] = scanDirectories(DIR_FS_CATALOG.$dir, array());
+        }
+        foreach ($rfiles_to_check as $dir) {
+          foreach ($dir as $file) {
+            if (!is_writeable(DIR_FS_CATALOG.$file) && $rfolder_flag !== true) {
+              $error_flag = true;
+              $rfolder_flag = true;
+              $message .= '<strong>'.TEXT_WRONG_RFOLDER_PERMISSION.'</strong>'.DIR_FS_CATALOG.dirname($file).'<br />';
+            }
+          }
         }
       }
     }
