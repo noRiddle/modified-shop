8789b131a84eec1408f5eb0e1cb8b35d6508c693
modGTB <gerhard@modified-shop.org>
2014-02-13 15:05:01 +0000

    new captcha
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@6330 79ac0487-9261-4f26-a34d-858dee0dcce0

--- display_vvcodes.php
+++ display_vvcodes.php
@@ -14,10 +14,33 @@
    ---------------------------------------------------------------------------------------*/
 
   require ('includes/application_top.php');
+  
+  /*
   require_once (DIR_FS_INC.'xtc_render_vvcode.inc.php');
   require_once (DIR_FS_INC.'xtc_random_charcode.inc.php');
 
   $visual_verify_code = xtc_random_charcode(6);
   $_SESSION['vvcode'] = strtoupper($visual_verify_code);
   $vvimg = vvcode_render_code($visual_verify_code);
+  */
+  
+  // include captcha class
+  require_once (DIR_FS_EXTERNAL.'captcha/php-captcha.inc.php');
+
+  // load fonts
+  $aFonts = array();
+  if ($dir= opendir(DIR_WS_INCLUDES.'fonts/')){
+    while  (($file = readdir($dir)) !==false) {
+      if (is_file(DIR_WS_INCLUDES.'fonts/'.$file) and (strstr(strtoupper($file),'.TTF'))){
+        $aFonts[] = DIR_FS_CATALOG.'/includes/fonts/'.$file;
+      }
+    }
+    closedir($dir);
+  }
+
+  // create new image
+  $oPhpCaptcha = new PhpCaptcha($aFonts, 200, 50);
+  $oPhpCaptcha->UseColour(true);
+  $oPhpCaptcha->Create();
+  
 ?>
