18942362194150bcd58b2cfbe437bf3ef6e901a2
modGTB <gerhard@modified-shop.org>
2014-03-21 11:15:13 +0000

    extend reviews
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@6408 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/modules/product_reviews.php
+++ includes/modules/product_reviews.php
@@ -18,28 +18,29 @@
 
   // create smarty elements
   $module_smarty = new Smarty;
-  $module_smarty->assign('tpl_path',DIR_WS_BASE.'templates/'.CURRENT_TEMPLATE.'/');
+  $module_smarty->assign('tpl_path', DIR_WS_BASE.'templates/'.CURRENT_TEMPLATE.'/');
 
   // include needed functions
   require_once (DIR_FS_INC.'xtc_row_number_format.inc.php');
   require_once (DIR_FS_INC.'xtc_date_short.inc.php');
-
-  if (isset($products_options_data)) {
-    $info_smarty->assign('options', $products_options_data);
-  }
+  
+  $button_preview = '';
   if ($_SESSION['customers_status']['customers_status_write_reviews'] == 1) {
     $button_preview = '<a href="'.xtc_href_link(FILENAME_PRODUCT_REVIEWS_WRITE, 'products_id='.$product->data['products_id']).'">'.xtc_image_button('button_write_review.gif', IMAGE_BUTTON_WRITE_REVIEW).'</a>';
-  } else {
-    $button_preview = '';
   }
+  
   $module_smarty->assign('BUTTON_WRITE', $button_preview);
   $module_smarty->assign('language', $_SESSION['language']);
   $module_smarty->caching = 0;
-  if (($_SESSION['customers_status']['customers_status_read_reviews'] == 1 && ($product->getReviewsCount() > 0) || $_SESSION['customers_status']['customers_status_write_reviews'] == 1)) {
+  
+  $reviews_count = $product->getReviewsCount();
+  
+  $module = '';
+  if (($_SESSION['customers_status']['customers_status_read_reviews'] == '1' && $reviews_count > 0) || $_SESSION['customers_status']['customers_status_write_reviews'] == 1) {    
+    $module_smarty->assign('reviews_count', $reviews_count);
     $module_smarty->assign('module_content', $product->getReviews());
     $module = $module_smarty->fetch(CURRENT_TEMPLATE.'/module/products_reviews.html');
-  } else {
-    $module = '';
   }
+  
   $info_smarty->assign('MODULE_products_reviews', $module);
 ?>
