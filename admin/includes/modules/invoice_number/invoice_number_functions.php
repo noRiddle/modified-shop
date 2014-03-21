f98f00845d9f78375e573c54e2d30010e62c1780
web28 <web28@modified-shop.org>
2014-03-21 14:48:45 +0000

    add css for invoice number
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@6409 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/includes/modules/invoice_number/invoice_number_functions.php
+++ admin/includes/modules/invoice_number/invoice_number_functions.php
@@ -28,11 +28,11 @@ function add_table_infos_ibillnr($order)
     if (defined('MODULE_INVOICE_NUMBER_STATUS') && MODULE_INVOICE_NUMBER_STATUS == 'True') {
         $html .= '<tr>'.PHP_EOL;
         $html .= '  <td class="main"><b>'.ENTRY_INVOICE_NUMBER.'</b></td>'.PHP_EOL;
-        $html .= '  <td class="main">'.($order->info['ibn_billnr'] == '' ? NOT_ASSIGNED : $order->info['ibn_billnr']).'</td>'.PHP_EOL;
+        $html .= '  <td class="main">'.($order->info['ibn_billnr'] == '' ? '<span class="not_assigned">'.NOT_ASSIGNED.'<span>' : $order->info['ibn_billnr']).'</td>'.PHP_EOL;
         $html .= '</tr>'.PHP_EOL;
         $html .= '<tr>'.PHP_EOL;
         $html .= '  <td class="main"><b>'.ENTRY_INVOICE_DATE.'</b></td>'.PHP_EOL;
-        $html .= '  <td class="main">'. ($order->info['ibn_billdate'] == '0000-00-00'? NOT_ASSIGNED : xtc_date_short($order->info['ibn_billdate'])).'</td>'.PHP_EOL;
+        $html .= '  <td class="main">'. ($order->info['ibn_billdate'] == '0000-00-00'? '<span class="not_assigned">'.NOT_ASSIGNED.'<span>' : xtc_date_short($order->info['ibn_billdate'])).'</td>'.PHP_EOL;
         $html .= '</tr>'.PHP_EOL;
     }
     return $html;
@@ -43,7 +43,7 @@ function add_btn_ibillnr($order,$oID)
     $html = '';
     if (defined('MODULE_INVOICE_NUMBER_STATUS') && MODULE_INVOICE_NUMBER_STATUS == 'True') {
       if ($order->info['ibn_billnr'] == '') {
-        $html = '              <a class="button" href="'.xtc_href_link(FILENAME_ORDERS, 'page='.$_GET['page'].'&oID='.$oID.'&action=edit&action2=set_ibillnr').'">'. BUTTON_BILL .'</a>'.PHP_EOL;
+        $html = '              <a class="button ibillnr-btn" href="'.xtc_href_link(FILENAME_ORDERS, 'page='.$_GET['page'].'&oID='.$oID.'&action=edit&action2=set_ibillnr').'">'. BUTTON_BILL .'</a>'.PHP_EOL;
       }
     }
     return $html;
