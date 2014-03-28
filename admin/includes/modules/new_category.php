6ed319203024abffb8fab96ef9aa23a4be7d7dfd
web28 <web28@modified-shop.org>
2014-03-28 10:39:12 +0000

    move lang_tabs code in separate file
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@6490 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/includes/modules/new_category.php
+++ admin/includes/modules/new_category.php
@@ -159,37 +159,8 @@
       ?>
       <!-- EOF Customers group block //-->
 
-      <link rel="stylesheet" type="text/css" href="includes/lang_tabs_menu/lang_tabs_menu.css" />
-      <script type="text/javascript" src="includes/lang_tabs_menu/lang_tabs_menu.js"></script>
-      <?php
-      $langtabs = '<div class="tablangmenu"><ul>';
-      $csstabstyle = 'border: 1px solid #aaaaaa; padding: 4px; width: 99%; margin-top: -1px; margin-bottom: 10px; float: left; background: #f3f3f3;';
-      $csstab = '<style type="text/css">' .  '#tab_lang_0' . '{display: block;' . $csstabstyle . '}';
-      $csstab_nojs = '<style type="text/css">';
-      for ($i = 0, $n = sizeof($languages); $i < $n; $i++) {
-        $tabtmp = "\'tab_lang_$i\'," ;
-        $langtabs.= '<li onclick="showTab('. $tabtmp. $n.')" style="cursor: pointer;" id="tabselect_' . $i .'">' .xtc_image(DIR_WS_LANGUAGES . $languages[$i]['directory'].'/admin/images/'.$languages[$i]['image'], $languages[$i]['name']) . ' ' . $languages[$i]['name'].  '</li>';
-        if($i > 0) $csstab .= '#tab_lang_' . $i .'{display: none;' . $csstabstyle . '}';
-        $csstab_nojs .= '#tab_lang_' . $i .'{display: block;' . $csstabstyle . '}';
-      }
-      $csstab .= '</style>';
-      $csstab_nojs .= '</style>';
-      $langtabs.= '</ul></div>';
-      //echo $csstab;
-      //echo $langtabs;
-      ?>
-      <div style="padding:5px;">
-        <?php if (USE_ADMIN_LANG_TABS != 'false') { ?>
-        <script type="text/javascript">
-          document.write('<?php echo ($csstab);?>');
-          document.write('<?php echo ($langtabs);?>');
-          //alert ("TEST");
-        </script>
-        <?php } else echo ($csstab_nojs);?>
-        <noscript>
-          <?php echo ($csstab_nojs);?>
-        </noscript>
         <?php
+        include('includes/lang_tabs.php');
         for ($i=0; $i<sizeof($languages); $i++) {
           echo ('<div id="tab_lang_' . $i . '">');
           $lng_image = '<div style="float:left;margin-right:5px;">'.xtc_image(DIR_WS_LANGUAGES.$languages[$i]['directory'].'/admin/images/'.$languages[$i]['image']).'</div>';
