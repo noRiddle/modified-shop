f89bb77f63677ad1cd325dcafd240d779216e5fd
Tomcraft1980 <hallo@modified-shop.org>
2014-08-08 09:33:39 +0000

    Added style definitions to error_reporting.php
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@6771 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/includes/application_bottom.php
+++ admin/includes/application_bottom.php
@@ -28,7 +28,7 @@
   // new error handling
   if (is_array($error_exceptions) && count($error_exceptions) > 0) {
     if ((DISPLAY_ERROR_REPORTING == 'all') || DISPLAY_ERROR_REPORTING == 'admin') {
-      echo '<div style="width:1000px; margin:20px auto;">' . PHP_EOL .
+      echo '<div style="width:1000px; margin:20px auto; font-family: Verdana,Arial,sans-serif; font-size: 10px;">' . PHP_EOL .
              '<h2 style="color: rgb(190, 50, 50);">Exception Occured:</h2>' . PHP_EOL;
              echo implode(PHP_EOL, $error_exceptions);
       echo '</div>';
--- includes/application_bottom.php
+++ includes/application_bottom.php
@@ -51,7 +51,7 @@ foreach(auto_require(DIR_FS_CATALOG.'includes/extra/application_bottom/','php') 
 // new error handling
 if (is_array($error_exceptions) && count($error_exceptions) > 0) {
   if ((DISPLAY_ERROR_REPORTING == 'all') || (DISPLAY_ERROR_REPORTING == 'admin' && $_SESSION['customers_status']['customers_status'] == '0')) {
-    echo '<div style="width:1000px; margin:20px auto;">' . PHP_EOL .
+    echo '<div style="width:1000px; margin:20px auto; font-family: Verdana,Arial,sans-serif; font-size: 10px;">' . PHP_EOL .
            '<h2 style="color: rgb(190, 50, 50);">Exception Occured:</h2>' . PHP_EOL;
            echo implode(PHP_EOL, $error_exceptions);
     echo '</div>';
