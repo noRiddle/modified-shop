57076401eaf6b7b7f5b1dc761cb972b471734f4c
Tomcraft1980 <hallo@modified-shop.org>
2014-10-10 10:26:01 +0000

    Revert changes from r6825
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@6826 79ac0487-9261-4f26-a34d-858dee0dcce0

--- inc/xtc_set_banner_status.inc.php
+++ inc/xtc_set_banner_status.inc.php
@@ -2,27 +2,35 @@
 /* -----------------------------------------------------------------------------------------
    $Id: xtc_set_banner_status.inc.php 899 2005-04-29 02:40:57Z hhgag $   
 
-   XT-Commerce - community made shopping
-   http://www.xt-commerce.com
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
 
-   Copyright (c) 2003 XT-Commerce
+   Copyright (c) 2009 - 2013 [www.modified-shop.org]
    -----------------------------------------------------------------------------------------
    based on: 
    (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
    (c) 2002-2003 osCommerce(banner.php,v 1.10 2003/02/11); www.oscommerce.com 
    (c) 2003	 nextcommerce (xtc_set_banner_status.inc.php,v 1.3 2003/08/13); www.nextcommerce.org
-
+   (c) 2003 XT-Commerce
+   
    Released under the GNU General Public License 
    ---------------------------------------------------------------------------------------*/
    
-// Sets the status of a banner
+  // Sets the status of a banner
   function xtc_set_banner_status($banners_id, $status) {
     if ($status == '1') {
-      return xtc_db_query("update " . TABLE_BANNERS . " set status = '1', date_status_change = now(), date_scheduled = NULL where banners_id = '" . $banners_id . "'");
+      xtc_db_query("UPDATE " . TABLE_BANNERS . " 
+                       SET status = '".(int)$status."', 
+                           date_status_change = now(), 
+                           date_scheduled = NULL 
+                     WHERE banners_id = '" . (int)$banners_id . "'");
     } elseif ($status == '0') {
-      return xtc_db_query("update " . TABLE_BANNERS . " set status = '0', date_status_change = now() where banners_id = '" . $banners_id . "'");
+      xtc_db_query("UPDATE " . TABLE_BANNERS . " 
+                       SET status = '".(int)$status."', 
+                           date_status_change = now(), 
+                     WHERE banners_id = '" . (int)$banners_id . "'");
     } else {
       return -1;
     }
   }
- ?>
+?>
