5060f8197539d709e496d4cc84b03115b1ce95d9
modGTB <gerhard@modified-shop.org>
2015-01-08 16:45:08 +0000

    fix upcoming products
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@7005 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/modules/upcoming_products.php
+++ includes/modules/upcoming_products.php
@@ -27,15 +27,17 @@ if (MAX_DISPLAY_UPCOMING_PRODUCTS != '0') {
   $expected_query = xtDBquery("SELECT p.products_id, 
                                       pd.products_name, 
                                       products_date_available as date_expected
-                                 FROM ".TABLE_PRODUCTS." p, 
-                                      ".TABLE_PRODUCTS_DESCRIPTION." pd
+                                 FROM ".TABLE_PRODUCTS." p
+                                 JOIN ".TABLE_PRODUCTS_DESCRIPTION." pd
+                                      ON p.products_id = pd.products_id
+                                         AND pd.language_id = ".$_SESSION['languages_id']."
+                                         AND trim(pd.products_name) != ''
                                 WHERE to_days(products_date_available) >= to_days(now())
-                                  AND p.products_id = pd.products_id
-                                  ".PRODUCTS_CONDITIONS_P."
-                                  AND trim(pd.products_name) != ''
-                                  AND pd.language_id = ".$_SESSION['languages_id']."
+                                  AND p.products_status = '1'
+                                      ".PRODUCTS_CONDITIONS_P."
                              ORDER BY ".EXPECTED_PRODUCTS_FIELD." ".EXPECTED_PRODUCTS_SORT."
                                 LIMIT ".MAX_DISPLAY_UPCOMING_PRODUCTS);
+                                
   if (xtc_db_num_rows($expected_query,true) > 0) {
     $module_content = array ();
     while ($expected = xtc_db_fetch_array($expected_query,true)) {
