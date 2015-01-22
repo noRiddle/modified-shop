5294028943cbaeaa4a28d811cbeba209a444f075
web28 <web28@modified-shop.org>
2015-01-22 01:59:32 +0000

    preparing for products_attributes_iframe
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@7122 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/includes/modules/new_product.php
+++ admin/includes/modules/new_product.php
@@ -231,7 +231,11 @@
         <input type="submit" class="button" name="prod_update" value="<?php echo BUTTON_UPDATE; ?>" <?php echo $confirm_save_entry;?>>
         <?php
         if (isset($_GET['pID']) && $_GET['pID'] > 0) {
-          echo '&nbsp;&nbsp;<a class="button" href="' . xtc_href_link('new_attributes.php','cpath='. $cPath . $catfunc->page_parameter.'&current_product_id='.$_GET['pID'].'&action=edit&oldaction=new_product').'" onclick="this.blur()">'.BUTTON_EDIT_ATTRIBUTES.'</a>';
+					if (is_file('includes/modules/products_attributes_iframe.php')) {
+					  include_once("includes/modules/products_attributes_iframe.php");
+					} else {
+            echo '&nbsp;&nbsp;<a class="button" href="' . xtc_href_link('new_attributes.php','cpath='. $cPath . $catfunc->page_parameter.'&current_product_id='.$_GET['pID'].'&action=edit&oldaction=new_product').'" onclick="this.blur()">'.BUTTON_EDIT_ATTRIBUTES.'</a>';
+          }
           echo '&nbsp;&nbsp;<a class="button" href="' . xtc_href_link(FILENAME_CONTENT_MANAGER, xtc_get_all_get_params(array('action')) . 'last_action='.$_GET['action'].'&action=new_products_content'.'&set=product') . '">' . BUTTON_NEW_CONTENT . '</a>';
           echo '&nbsp;&nbsp;<a class="button" href="' . xtc_catalog_href_link('product_info.php', 'products_id=' . $_GET['pID']) . '" target="_blank">' . BUTTON_VIEW_PRODUCT . '</a>';
         }
