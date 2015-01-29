7fb43594796877e9fff08f734c35751a6ff63821
modGTB <gerhard@modified-shop.org>
2015-01-29 12:21:33 +0000

    layout categories
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@7237 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/includes/modules/new_category.php
+++ admin/includes/modules/new_category.php
@@ -96,7 +96,8 @@
         </tr>
         <tr>
           <td class="main"><?php echo TEXT_EDIT_PRODUCT_SORT_ORDER; ?>:</td>
-          <td class="main"><?php echo xtc_draw_pull_down_menu('products_sorting',$order_array,((xtc_not_null($cInfo->products_sorting))?$cInfo->products_sorting:$default_value), 'style="width: 160px"'); ?>&nbsp;<?php echo xtc_draw_pull_down_menu('products_sorting2',$order_array_desc,$cInfo->products_sorting2, 'style="width: 160px"'); ?></td>
+          <td class="main"><?php echo xtc_draw_pull_down_menu('products_sorting',$order_array,((xtc_not_null($cInfo->products_sorting))?$cInfo->products_sorting:$default_value), 'style="width: 160px"'); ?><br/>
+                           <?php echo xtc_draw_pull_down_menu('products_sorting2',$order_array_desc,$cInfo->products_sorting2, 'style="width: 160px"'); ?></td>
         </tr>
         <tr>
           <td class="main"><?php echo TEXT_EDIT_SORT_ORDER; ?></td>
