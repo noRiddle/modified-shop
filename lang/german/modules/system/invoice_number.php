d28fcea48e27b837f9d9dc2fbada664615550c98
modGTB <gerhard@modified-shop.org>
2015-02-20 01:48:46 +0000

    fix entity
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@7549 79ac0487-9261-4f26-a34d-858dee0dcce0

--- lang/german/modules/system/invoice_number.php
+++ lang/german/modules/system/invoice_number.php
@@ -16,7 +16,7 @@ define('MODULE_INVOICE_NUMBER_STATUS_INFO','Aktiv');
 define('MODULE_INVOICE_NUMBER_TEXT_TITLE', 'Modul Neue Rechnungsnummer');
 define('MODULE_INVOICE_NUMBER_TEXT_BTN', 'Bearbeiten');
 define('MODULE_INVOICE_NUMBER_TEXT_DESCRIPTION', ' Modul Neue Rechnungsnummer');    
-define('MODULE_INVOICE_NUMBER_IBN_BILLNR_TITLE', 'N&aumlchste Rechnungsnummer');
+define('MODULE_INVOICE_NUMBER_IBN_BILLNR_TITLE', 'N&auml;chste Rechnungsnummer');
 define('MODULE_INVOICE_NUMBER_IBN_BILLNR_DESC', 'Bei Zuweisung einer Bestellung wird diese Nummer als n&auml;chstes vergeben.');
 define('MODULE_INVOICE_NUMBER_IBN_BILLNR_FORMAT_TITLE', 'Rechnungsnummer Format');
 define('MODULE_INVOICE_NUMBER_IBN_BILLNR_FORMAT_DESC', 'Aufbauschema Rechn.Nr.: {n}=laufende Nummer, {d}=Tag, {m}=Monat, {y}=Jahr, <br>z.B. "100{n}-{d}-{m}-{y}" ergibt "10099-28-02-2007"');
