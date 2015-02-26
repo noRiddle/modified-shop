f5a9d7b432c4f00f25f0f8786f2e6af4e738bda5
h-h-h <h-h-h@modified-shop.org>
2015-02-26 09:15:15 +0000

    extend add_select for extra hook points with example
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@7616 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/define_add_select.php
+++ includes/define_add_select.php
@@ -9,16 +9,23 @@
    -----------------------------------------------------------------------------------------
    Released under the GNU General Public License 
    ---------------------------------------------------------------------------------------*/
-  
+/*
+example for default.php
+can be used in extra hook points:
+
+if (!isset($add_select_default)) $add_select_default = array();
+$add_select_default[] = 'p.products_extra_field';
+
+*/
   // used in /includes/modules/default.php
-  define('ADD_SELECT_DEFAULT', 'p.products_manufacturers_model, ');
+  define('ADD_SELECT_DEFAULT', 'p.products_manufacturers_model, '.(isset($add_select_default)?implode(', ', $add_select_default):''));
   
   // used in /advanced_search_result.php
-  define('ADD_SELECT_SEARCH', 'p.products_manufacturers_model, ');
+  define('ADD_SELECT_SEARCH', 'p.products_manufacturers_model, '.(isset($add_select_search)?implode(', ', $add_select_search):''));
   
   // used in /includes/classes/product.php
-  define('ADD_SELECT_PRODUCT', '');
+  define('ADD_SELECT_PRODUCT', (isset($add_select_product)?implode(', ', $add_select_product):''));
 
   // used in /includes/classes/shopping_cart.php
-  define('ADD_SELECT_CART', '');
+  define('ADD_SELECT_CART', (isset($add_select_cart)?implode(', ', $add_select_cart):''));
 ?>
