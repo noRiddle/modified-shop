65ac40e131dca197606e2a8f12b03df68e3ba9bb
h-h-h <h-h-h@modified-shop.org>
2015-03-01 16:44:56 +0000

    fix r7685
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@7686 79ac0487-9261-4f26-a34d-858dee0dcce0

--- inc/seo_url_mod.php
+++ inc/seo_url_mod.php
@@ -1,9 +1,10 @@
 <?php
 
 ### shopstat SEO URL
-function seo_url_mod($page, $parameters, $connection, $link, $separator) {
+function seo_url_mod($link, $page, $parameters, $connection, $link, $separator) {
   require_once(DIR_FS_INC . 'shopstat_functions.inc.php');
-  if($seolink = shopstat_getSEO($page, $parameters, $connection)){
+  $mode = !isset($_POST['catalog_link']) ? 'user' : 'admin';
+  if($seolink = shopstat_getSEO($page, $parameters, $connection, true, true, $mode)){
     $link      = $seolink;
     $elements  = parse_url($link);
     $separator = (isset($elements['query']) ? '&' : '?');
