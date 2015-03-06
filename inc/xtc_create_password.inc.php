e84067f411207ad3618ff1aa70428d511e26ef24
web28 <web28@modified-shop.org>
2015-03-06 17:09:36 +0000

    better random number generator
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@7783 79ac0487-9261-4f26-a34d-858dee0dcce0

--- inc/xtc_create_password.inc.php
+++ inc/xtc_create_password.inc.php
@@ -18,7 +18,7 @@
     $chars = array( 'a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E', 'f', 'F', 'g', 'G', 'h', 'H', 'i', 'I', 'j', 'J',  'k', 'K', 'l', 'L', 'm', 'M', 'n','N', 'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R', 's', 'S', 't', 'T',  'u', 'U', 'v','V', 'w', 'W', 'x', 'X', 'y', 'Y', 'z', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0');
 
     $max_chars = count($chars) - 1;
-    srand( (double) microtime()*1000000);
+    mt_srand( (double) microtime()*1000000);
 
     $rand_str = '';
     for($i=0;$i<$length;$i++) {
