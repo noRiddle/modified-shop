63eeae855f78113de5cc25f0380d544916bbc08d
modGTB <gerhard@modified-shop.org>
2015-04-07 14:37:29 +0000

    update eustandardtransfer
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@8079 79ac0487-9261-4f26-a34d-858dee0dcce0

--- lang/german/modules/payment/eustandardtransfer.php
+++ lang/german/modules/payment/eustandardtransfer.php
@@ -55,4 +55,10 @@
   define('MODULE_PAYMENT_EUSTANDARDTRANSFER_ALLOWED_TITLE' , 'Erlaubte Zonen');
   define('MODULE_PAYMENT_EUSTANDARDTRANSFER_ALLOWED_DESC' , 'Geben Sie <b>einzeln</b> die Zonen an, welche f&uuml;r dieses Modul erlaubt sein sollen. (z.B. AT,DE (wenn leer, werden alle Zonen erlaubt))');
 
+  define('MODULE_PAYMENT_EUSTANDARDTRANSFER_ZONE_TITLE' , 'Zahlungszone');
+  define('MODULE_PAYMENT_EUSTANDARDTRANSFER_ZONE_DESC' , 'Wenn eine Zone ausgew&auml;hlt ist, gilt die Zahlungsmethode nur f&uuml;r diese Zone.');
+  
+  define('MODULE_PAYMENT_EUSTANDARDTRANSFER_ORDER_STATUS_ID_TITLE' , 'Bestellstatus festlegen');
+  define('MODULE_PAYMENT_EUSTANDARDTRANSFER_ORDER_STATUS_ID_DESC' , 'Bestellungen, welche mit diesem Modul gemacht werden, auf diesen Status setzen');
+
 ?>
