cc3032e61f77c18c6c66948ae900dce4ec2a49f1
modGTB <gerhard@modified-shop.org>
2015-04-09 15:19:02 +0000

    fix #431
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@8125 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/includes/modules/email_preview/email_preview.php
+++ admin/includes/modules/email_preview/email_preview.php
@@ -7,7 +7,7 @@ if ($email_preview) {
     $txt_mail = str_replace('[SIGNATUR]', $txt_signatur, $txt_mail);
     $email_div = email_preview_tabs();
     $email_div .= '<div id="email_preview_html">'.$html_mail.'</div>'.PHP_EOL;
-    $email_div .= '<div id="email_preview_txt" style="display:none">'.nl2br($txt_mail).'</div>'.PHP_EOL;
+    $email_div .= '<div id="email_preview_txt" style="display:none">'.encode_utf8(nl2br($txt_mail)).'</div>'.PHP_EOL;
     //xtc_redirect(xtc_href_link(FILENAME_ORDERS, 'oID='.$order_id.'&action=edit'));
     echo $email_div;
     exit;
