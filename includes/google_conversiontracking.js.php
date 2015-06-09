72fca0396fe73cb056487e4bb8bef4f97764c964
modGTB <gerhard@modified-shop.org>
2015-06-09 09:18:41 +0000

    update google conversion tracking
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@8276 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/google_conversiontracking.js.php
+++ includes/google_conversiontracking.js.php
@@ -9,49 +9,26 @@
 
    Released under the GNU General Public License
    ---------------------------------------------------------------------------------------*/
-?>
 
-<?php
 require_once (DIR_FS_INC.'get_order_total.inc.php');
 $total = get_order_total($last_order);
 ?>
 
 <!-- Google Code for Purchase Conversion Page -->
 <script type="text/javascript">
-/* <![CDATA[ */
-var google_conversion_id = <?php echo GOOGLE_CONVERSION_ID; ?>;
-var google_conversion_language = "<?php echo GOOGLE_LANG; ?>";
-var google_conversion_format = "2";
-var google_conversion_color = "ffffff";
-var google_conversion_label = "<?php echo GOOGLE_CONVERSION_LABEL; ?>";
-var google_conversion_value = "<?php echo $total; ?>";
-var google_remarketing_only = false;
-/* ]]> */
+  /* <![CDATA[ */
+  var google_conversion_id = <?php echo GOOGLE_CONVERSION_ID; ?>;
+  var google_conversion_language = "<?php echo GOOGLE_LANG; ?>";
+  var google_conversion_format = "2";
+  var google_conversion_color = "ffffff";
+  var google_conversion_label = "<?php echo GOOGLE_CONVERSION_LABEL; ?>";
+  var google_conversion_value = "<?php echo $total; ?>";
+  var google_remarketing_only = false;
+  /* ]]> */
 </script>
-<?php
-//BOF - Dokuman - 2009-08-19 - BUGFIX: #0000223 SSL/NONSSL check for google conversiontracking
-if ($request_type=='NONSSL') { 
-//EOF - Dokuman - 2009-08-19 - BUGFIX: #0000223 SSL/NONSSL check for google conversiontracking
-?>
-<script language="JavaScript" src="http://www.googleadservices.com/pagead/conversion.js">
-</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js"></script>
 <noscript>
-<div style="display:inline;">
-<img height="1" width="1" style="border-style:none;" alt="" src="http://www.googleadservices.com/pagead/conversion/<?php echo GOOGLE_CONVERSION_ID; ?>/?value=0&amp;label=<?php echo GOOGLE_CONVERSION_LABEL; ?>&amp;guid=ON&amp;script=0"/>
-</div>
+  <div style="display:inline;">
+    <img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/<?php echo GOOGLE_CONVERSION_ID; ?>/?value=<?php echo $total; ?>&amp;label=<?php echo GOOGLE_CONVERSION_LABEL; ?>&amp;guid=ON&amp;script=0"/>
+  </div>
 </noscript>
-<?php
-//BOF - Dokuman - 2009-08-19 - BUGFIX: #0000223 SSL/NONSSL check for google conversiontracking
-}else{
-?>
-<script type="text/javascript" src="https://www.googleadservices.com/pagead/conversion.js">
-</script>
-<noscript>
-<div style="display:inline;">
-<img height="1" width="1" style="border-style:none;" alt="" src="https://www.googleadservices.com/pagead/conversion/<?php echo GOOGLE_CONVERSION_ID; ?>/?value=0&amp;label=<?php echo GOOGLE_CONVERSION_LABEL; ?>&amp;guid=ON&amp;script=0"/>
-</div>
-</noscript>
-<?php
-}
-//EOF - Dokuman - 2009-08-19 - BUGFIX: #0000223 SSL/NONSSL check for google conversiontracking
-?>
