715f1d69466f96441fb48b3668a59ebdc09599a4
web28 <web28@modified-shop.org>
2015-09-11 22:36:20 +0000

    add iframe_box
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@8796 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/includes/modules/products_attributes_iframe.php
+++ admin/includes/modules/products_attributes_iframe.php
@@ -19,67 +19,21 @@ if (!defined('USE_ATTRIBUTES_IFRAME')) {
   define ('USE_ATTRIBUTES_IFRAME','true');
 }
 
+if (is_file(DIR_WS_MODULES.'iframe_box.php')) {
+  include_once(DIR_WS_MODULES.'iframe_box.php');
+}
+
 if (defined('USE_ATTRIBUTES_IFRAME') && USE_ATTRIBUTES_IFRAME == 'true') {
 
   function attributes_iframe_link($pID, $icon=false)
   {
     global $icon_padding;
     if ($icon) {
-      $link = '<a href="javascript:showproduct_attribute(' . $pID . ');">' . xtc_image(DIR_WS_ICONS . 'icon_edit_attr.gif', BUTTON_EDIT_ATTRIBUTES,'', '', $icon_padding). '</a>';
+      $link = '<a href="javascript:iframeBox_show('. $pID .', \''.BUTTON_EDIT_ATTRIBUTES.'\' , \''.NEW_ATTRIBUTES_IFRAME_FILENAME.'\',\'&action=edit\');">' . xtc_image(DIR_WS_ICONS . 'icon_edit_attr.gif', BUTTON_EDIT_ATTRIBUTES,'', '', $icon_padding). '</a>';
     } else {
-      $link = '<a href="javascript:showproduct_attribute(' . $pID . ');" class="button">'. BUTTON_EDIT_ATTRIBUTES.'</a>';
+      $link = '<a href="javascript:iframeBox_show('. $pID .', \''.BUTTON_EDIT_ATTRIBUTES.'\' , \''.NEW_ATTRIBUTES_IFRAME_FILENAME.'\',\'&action=edit\');" class="button">'. BUTTON_EDIT_ATTRIBUTES.'</a>';
     }
     return $link;
   }
-?>
-    
-  <style>
-    #bgboxshow {
-        display:none; background:#ccc; position:fixed; width:100%; height:100%; top:0px; left:0px; z-index:9000; opacity:0.8; filter:alpha(opacity=80); /* For IE8 and earlier */
-      }
-    #bgboxshow_product_attribute {
-        width:95%; height:900px; max-height:900px; margin:auto; position:fixed; top:50%; left:50%; margin-top:-450px; margin-left:-48%; display:none; z-index:9001; 
-      }
-    @media screen and (max-height: 920px) { #bgboxshow_product_attribute{max-height:800px; height:800px; margin-top:-400px;} }
-    @media screen and (max-height: 820px) { #bgboxshow_product_attribute{max-height:700px; height:700px; margin-top:-350px;} }
-    @media screen and (max-height: 720px) { #bgboxshow_product_attribute{max-height:600px; height:600px; margin-top:-300px;} }  
-    @media screen and (max-height: 620px) { #bgboxshow_product_attribute{max-height:500px; height:500px; margin-top:-250px;} }
-    @media screen and (max-height: 520px) { #bgboxshow_product_attribute{max-height:400px; height:400px; margin-top:-200px;} }    
-    #bgboxshow_edit {
-        width:100%; height:100%; background:#FFF; border:#000 1px solid; padding:1px;  
-        box-shadow: 0 0 10px #000; -moz-box-shadow: 0 0 10px #000; -webkit-box-shadow: 0 0 10px #000); 
-      }
-    .bgboxshow_title { 
-        font-family: Verdana,Arial, Helvetica, sans-serif; font-size: 13px; background:#555; color:#FFF; font-weight:bold; padding:3px; margin-bottom:1px;
-      }
-    .bgboxshow_close {
-        width: 30px; height: 30px; background-image: url('images/close.png'); position: absolute; top:-10px; right:-17px; z-index: 1103; cursor: pointer; 
-      }
-  </style>
-  
-  <div id="bgboxshow" onclick="bgboxshow_close()"></div> 
-    <div id="bgboxshow_product_attribute">
-      <div id="bgboxshow_edit">
-    </div>
-  </div>
-  
-  <script type="text/javascript">
-    function showproduct_attribute(pID) {
-        if (document.getElementById("bgboxshow_product_attribute").style.display =="none" || document.getElementById("bgboxshow_product_attribute").style.display == "") {
-            document.getElementById("bgboxshow").style.display = "block";
-            document.getElementById("bgboxshow_product_attribute").style.display = "block"; 
-            document.getElementById('bgboxshow_edit').innerHTML = 
-             '<div class="bgboxshow_title"><?php echo BUTTON_EDIT_ATTRIBUTES; ?></div><div class="bgboxshow_close" onclick="bgboxshow_close()"> </div>' +
-             '<iframe name="new_attributes_iframe" src="<?php echo NEW_ATTRIBUTES_IFRAME_FILENAME; ?>?iframe=1&action=edit&current_product_id=' + pID + '" marginwidth="0" marginheight="0" width="100%" height="94%" border="0" frameborder="0"> </iframe>';
-        } else {
-            document.getElementById('bgboxshow_edit').innerHTML= '';
-        }                 
-    }
-    function bgboxshow_close(){
-        document.getElementById("bgboxshow").style.display="none";
-        document.getElementById("bgboxshow_product_attribute").style.display="none";
-    }
-  </script>
-<?php 
-  }
-?>
+
+}
