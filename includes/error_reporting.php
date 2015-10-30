74bff0b41a7213d86616f68e235cbb35e586a495
web28 <web28@modified-shop.org>
2015-10-30 12:57:53 +0000

    update error_reporting.php
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@8982 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/error_reporting.php
+++ includes/error_reporting.php
@@ -34,22 +34,23 @@ function log_exception(Exception $e)
 
     if (is_object($e)) {
         $backtrace = debug_backtrace();
-        $error_number = (method_exists($e, 'getseverity') ? $e->getseverity() : 'UNDEFINED_ERROR');
-        $error_name = (($error_number != 'UNDEFINED_ERROR') ? error_level($error_number) : 'UNDEFINED_ERROR');
-        $error_line = $e->getLine();
-        $error_file = $e->getFile();
-        $error_message = $e->getMessage();
-        $index = md5($error_name.$error_line.$error_file.$error_message);
+        $error = array();
+        $error['number'] = (method_exists($e, 'getseverity') ? $e->getseverity() : 'UNDEFINED_ERROR');
+        $error['name'] = (($error['number'] != 'UNDEFINED_ERROR') ? error_level($error['number']) : 'UNDEFINED_ERROR');
+        $error['line'] = $e->getLine();
+        $error['file'] = $e->getFile();
+        $error['message'] = $e->getMessage();
+        $index = md5($error['name'].$error['line'].$error['file'].$error['message']);
     
         if (!isset($error_exceptions[$index])) {
             $error_exceptions[$index] = '<table style="width: 1000px; display: inline-block;">' . PHP_EOL .
-                                        '  <tr style="color:#000; background-color:#e6e6e6;"><th style="width:100px;">Type</th><td style="width:900px;">'.$error_name.'</td></tr>' . PHP_EOL .
-                                        '  <tr style="color:#000; background-color:#F0F0F0;"><th>Message</th><td>'.$error_message.'</td></tr>' . PHP_EOL .
-                                        '  <tr style="color:#000; background-color:#e6e6e6;"><th>File</th><td>'.$error_file.'</td></tr>' . PHP_EOL .
-                                        '  <tr style="color:#000; background-color:#F0F0F0;"><th>Line</th><td>'.$error_line.'</td></tr>' . PHP_EOL;
+                                        '  <tr style="color:#000; background-color:#e6e6e6;"><th style="width:100px;">Type</th><td style="width:900px;">'.$error['name'].'</td></tr>' . PHP_EOL .
+                                        '  <tr style="color:#000; background-color:#F0F0F0;"><th>Message</th><td>'.$error['message'].'</td></tr>' . PHP_EOL .
+                                        '  <tr style="color:#000; background-color:#e6e6e6;"><th>File</th><td>'.$error['file'].'</td></tr>' . PHP_EOL .
+                                        '  <tr style="color:#000; background-color:#F0F0F0;"><th>Line</th><td>'.$error['line'].'</td></tr>' . PHP_EOL;
                                         $err = 0;
                                         for ($i=0, $n=count($backtrace); $i<$n; $i++) {
-                                            if (isset($backtrace[$i]['file']) && $backtrace[$i]['file'] != $error_file && basename($backtrace[$i]['file']) != 'error_reporting.php') {
+                                            if (isset($backtrace[$i]['file']) && $backtrace[$i]['file'] != $error['file'] && basename($backtrace[$i]['file']) != 'error_reporting.php') {
                                                 $error_exceptions[$index] .= '  <tr style="color:#000; background-color:#e6e6e6;"><th>Backtrace #'.$err.'</th><td>'.$backtrace[$i]['file'].' called at Line '.$backtrace[$i]['line'].'</td></tr>' . PHP_EOL;
                                                 $err ++;
                                             }
@@ -57,17 +58,30 @@ function log_exception(Exception $e)
             $error_exceptions[$index] .= '</table>' . PHP_EOL .
                                          '<div style="height:1px; border-top:1px dotted #000; margin:10px 0px;"></div>';
 
-            // write Logfile
-            if ($error_number != E_NOTICE && $error_number != E_STRICT) {
-                error_log(strftime(STORE_PARSE_DATE_TIME_FORMAT) . ' ' . $error_name . ' - ' . html_entity_decode($error_message) . ' in File: ' . $error_file . ' on Line: ' . $error_line . "\n", 3, DIR_FS_LOG.'mod_error_' .date('Y-m-d') .'.log');
-                $err = 0;
-                for ($i=0, $n=count($backtrace); $i<$n; $i++) {
-                    if (isset($backtrace[$i]['file']) && $backtrace[$i]['file'] != $error_file && basename($backtrace[$i]['file']) != 'error_reporting.php') {
-                        error_log(strftime(STORE_PARSE_DATE_TIME_FORMAT) . ' Backtrace #'.$err.' - '.$backtrace[$i]['file'].' called at Line '.$backtrace[$i]['line'] . "\n", 3, DIR_FS_LOG.'mod_error_' .date('Y-m-d') .'.log');
-                        $err ++;
-                    }
-                }
+            // write error Logfile
+            if ($error['number'] != E_NOTICE && $error['number'] != E_STRICT  && $error['number'] != E_DEPRECATED && $error['number'] != E_USER_NOTICE) {
+                write_error_logfile($backtrace,$error,'mod_error');
             }
+            // write debug Logfile
+            if ($error['number'] == E_USER_NOTICE) {
+                write_error_logfile($backtrace,$error,'mod_debug');
+            }
+            // write deprecated Logfile
+            if ($error['number'] == E_DEPRECATED) {
+                //write_error_logfile($backtrace,$error,'mod_deprecated');
+            }
+            
+        }
+    }
+}
+
+function write_error_logfile($backtrace,$error,$filename) {
+    error_log(strftime(STORE_PARSE_DATE_TIME_FORMAT) . ' ' . $error['name'] . ' - ' . html_entity_decode($error['message']) . ' in File: ' . $error['file'] . ' on Line: ' . $error['line'] . "\n", 3, DIR_FS_LOG. $filename . '_' .date('Y-m-d') .'.log');
+    $err = 0;
+    for ($i=0, $n=count($backtrace); $i<$n; $i++) {
+        if (isset($backtrace[$i]['file']) && $backtrace[$i]['file'] != $error['file'] && basename($backtrace[$i]['file']) != 'error_reporting.php') {
+            error_log(strftime(STORE_PARSE_DATE_TIME_FORMAT) . ' Backtrace #'.$err.' - '.$backtrace[$i]['file'].' called at Line '.$backtrace[$i]['line'] . "\n", 3, DIR_FS_LOG. $filename . '_' .date('Y-m-d') .'.log');
+            $err ++;
         }
     }
 }
