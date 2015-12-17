9a1e8d2e6f3bce70c2c71a93759764270f72c7ce
web28 <web28@modified-shop.org>
2015-12-17 11:00:10 +0000

    add update compatibility for default payment modules
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@9137 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/modules/payment/cod.php
+++ includes/modules/payment/cod.php
@@ -11,7 +11,7 @@
    based on:
    (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
    (c) 2002-2003 osCommerce(cod.php,v 1.28 2003/02/14); www.oscommerce.com
-   (c) 2003	 nextcommerce (cod.php,v 1.7 2003/08/24); www.nextcommerce.org
+   (c) 2003  nextcommerce (cod.php,v 1.7 2003/08/24); www.nextcommerce.org
 
    third party contributions:
    - added max subtotal where cod allowed to config, noRiddle / web0null / web28
@@ -23,60 +23,66 @@
 
 class cod {
 
-	var $code, $title, $description, $enabled;
+  var $code, $title, $description, $enabled;
 
-	function __construct() {
-		global $order,$xtPrice;
+  function __construct() {
+    global $order,$xtPrice;
 
-		$this->code = 'cod';
-		$this->title = MODULE_PAYMENT_COD_TEXT_TITLE;
-		$this->description = MODULE_PAYMENT_COD_TEXT_DESCRIPTION;
-		$this->sort_order = MODULE_PAYMENT_COD_SORT_ORDER;
-		$this->enabled = ((MODULE_PAYMENT_COD_STATUS == 'True') ? true : false);
-		$this->info = MODULE_PAYMENT_COD_TEXT_INFO;
-		$this->cost = '';
-		$this->limit_subtotal = MODULE_PAYMENT_COD_LIMIT_ALLOWED; // for comparison to be able to limit order subtotal sum where cod allowed
+    $this->code = 'cod';
+    $this->title = MODULE_PAYMENT_COD_TEXT_TITLE;
+    $this->description = MODULE_PAYMENT_COD_TEXT_DESCRIPTION;
+    $this->sort_order = MODULE_PAYMENT_COD_SORT_ORDER;
+    $this->enabled = ((MODULE_PAYMENT_COD_STATUS == 'True') ? true : false);
+    $this->info = MODULE_PAYMENT_COD_TEXT_INFO;
+    $this->cost = '';
+    
+    //update compatibility
+    if (!defined('MODULE_PAYMENT_COD_LIMIT_ALLOWED')) {
+      xtc_db_query("insert into ".TABLE_CONFIGURATION." ( configuration_key, configuration_value,  configuration_group_id, sort_order, date_added) values ('MODULE_PAYMENT_COD_LIMIT_ALLOWED', '600', '6', '3', now())");
+    }
+    
+    $this->limit_subtotal = MODULE_PAYMENT_COD_LIMIT_ALLOWED; // for comparison to be able to limit order subtotal sum where cod allowed
 
-		if ((int) MODULE_PAYMENT_COD_ORDER_STATUS_ID > 0) {
-			$this->order_status = MODULE_PAYMENT_COD_ORDER_STATUS_ID;
-		}
+    if ((int) MODULE_PAYMENT_COD_ORDER_STATUS_ID > 0) {
+      $this->order_status = MODULE_PAYMENT_COD_ORDER_STATUS_ID;
+    }
 
-		if (is_object($order))
-			$this->update_status();
-	}
+    if (is_object($order))
+      $this->update_status();
+  }
 
-	function update_status() {
-		global $order;
-		if ($_SESSION['shipping']['id'] == 'selfpickup_selfpickup') {
-			$this->enabled = false;
-		}
-		if (($this->enabled == true) && ((int) MODULE_PAYMENT_COD_ZONE > 0)) {
-			$check_flag = false;
-			$check_query = xtc_db_query("select zone_id from ".TABLE_ZONES_TO_GEO_ZONES." where geo_zone_id = '".MODULE_PAYMENT_COD_ZONE."' and zone_country_id = '".$order->delivery['country']['id']."' order by zone_id");
-			while ($check = xtc_db_fetch_array($check_query)) {
-				if ($check['zone_id'] < 1) {
-					$check_flag = true;
-					break;
-				}
-				elseif ($check['zone_id'] == $order->delivery['zone_id']) {
-					$check_flag = true;
-					break;
-				}
-			}
+  function update_status() {
+    global $order;
+    if ($_SESSION['shipping']['id'] == 'selfpickup_selfpickup') {
+      $this->enabled = false;
+    }
+    if (($this->enabled == true) && ((int) MODULE_PAYMENT_COD_ZONE > 0)) {
+      $check_flag = false;
+      $check_query = xtc_db_query("select zone_id from ".TABLE_ZONES_TO_GEO_ZONES." where geo_zone_id = '".MODULE_PAYMENT_COD_ZONE."' and zone_country_id = '".$order->delivery['country']['id']."' order by zone_id");
+      while ($check = xtc_db_fetch_array($check_query)) {
+        if ($check['zone_id'] < 1) {
+          $check_flag = true;
+          break;
+        }
+        elseif ($check['zone_id'] == $order->delivery['zone_id']) {
+          $check_flag = true;
+          break;
+        }
+      }
 
-			if ($check_flag == false) {
-				$this->enabled = false;
-			}
-		}
+      if ($check_flag == false) {
+        $this->enabled = false;
+      }
+    }
 
-	}
+  }
 
-	function javascript_validation() {
-		return false;
-	}
+  function javascript_validation() {
+    return false;
+  }
 
-	function selection() {
-		global $xtPrice,$order;
+  function selection() {
+    global $xtPrice,$order;
 
       // limit sum where cod allowed
       if($this->limit_subtotal && ($xtPrice->xtcRemoveCurr($_SESSION['cart']->show_total()) >= $this->limit_subtotal)) {
@@ -94,7 +100,7 @@ class cod {
           $shipping_code = strtoupper(array_shift(explode('_',$_SESSION['shipping']['id'])));
         }
         $shipping_code = (isset($shipping_code) && $shipping_code == 'FREEAMOUNT') ? 'FREEAMOUNT_FREE' : 'FEE_' . $shipping_code;
-        
+
         $cod_zones = array();
         if (defined('MODULE_ORDER_TOTAL_COD_'. $shipping_code)) {
           $cod_zones = preg_split("/[:,]/", constant('MODULE_ORDER_TOTAL_COD_'. $shipping_code));
@@ -137,72 +143,72 @@ class cod {
         }
         $this->cost = '+ '.$cod_cost;
       }
-
+      
       return array ('id' => $this->code,
                     'module' => $this->title,
                     'description' => $this->info,
                     'module_cost'=>$this->cost
                    );
-	}
+  }
 
-	function pre_confirmation_check() {
-		return false;
-	}
+  function pre_confirmation_check() {
+    return false;
+  }
 
-	function confirmation() {
-		return false;
-	}
+  function confirmation() {
+    return false;
+  }
 
-	function process_button() {
-		return false;
-	}
+  function process_button() {
+    return false;
+  }
 
-	function before_process() {
-		return false;
-	}
+  function before_process() {
+    return false;
+  }
 
-	function after_process() {
-		global $insert_id;
+  function after_process() {
+    global $insert_id;
 
     if (isset($this->order_status) && $this->order_status) {
       xtc_db_query("UPDATE ".TABLE_ORDERS." SET orders_status='".$this->order_status."' WHERE orders_id='".$insert_id."'");
       xtc_db_query("UPDATE ".TABLE_ORDERS_STATUS_HISTORY." SET orders_status_id='".$this->order_status."' WHERE orders_id='".$insert_id."'");
     }
-	}
+  }
 
-	function get_error() {
-		return false;
-	}
+  function get_error() {
+    return false;
+  }
 
-	function check() {
-		if (!isset ($this->_check)) {
-			$check_query = xtc_db_query("select configuration_value from ".TABLE_CONFIGURATION." where configuration_key = 'MODULE_PAYMENT_COD_STATUS'");
-			$this->_check = xtc_db_num_rows($check_query);
-		}
-		return $this->_check;
-	}
+  function check() {
+    if (!isset ($this->_check)) {
+      $check_query = xtc_db_query("select configuration_value from ".TABLE_CONFIGURATION." where configuration_key = 'MODULE_PAYMENT_COD_STATUS'");
+      $this->_check = xtc_db_num_rows($check_query);
+    }
+    return $this->_check;
+  }
 
-	function install() {
-		xtc_db_query("insert into ".TABLE_CONFIGURATION." ( configuration_key, configuration_value,  configuration_group_id, sort_order, set_function, date_added) values ('MODULE_PAYMENT_COD_STATUS', 'True',  '6', '1', 'xtc_cfg_select_option(array(\'True\', \'False\'), ', now())");
-		xtc_db_query("insert into ".TABLE_CONFIGURATION." ( configuration_key, configuration_value,  configuration_group_id, sort_order, date_added) values ('MODULE_PAYMENT_COD_ALLOWED', '', '6', '0', now())");
-		xtc_db_query("insert into ".TABLE_CONFIGURATION." ( configuration_key, configuration_value,  configuration_group_id, sort_order, use_function, set_function, date_added) values ('MODULE_PAYMENT_COD_ZONE', '0', '6', '2', 'xtc_get_zone_class_title', 'xtc_cfg_pull_down_zone_classes(', now())");
-		xtc_db_query("insert into ".TABLE_CONFIGURATION." ( configuration_key, configuration_value,  configuration_group_id, sort_order, date_added) values ('MODULE_PAYMENT_COD_SORT_ORDER', '0',  '6', '0', now())");
-		xtc_db_query("insert into ".TABLE_CONFIGURATION." ( configuration_key, configuration_value,  configuration_group_id, sort_order, set_function, use_function, date_added) values ('MODULE_PAYMENT_COD_ORDER_STATUS_ID', '0','6', '0', 'xtc_cfg_pull_down_order_statuses(', 'xtc_get_order_status_name', now())");
-		xtc_db_query("insert into ".TABLE_CONFIGURATION." ( configuration_key, configuration_value,  configuration_group_id, sort_order, date_added) values ('MODULE_PAYMENT_COD_LIMIT_ALLOWED', '600', '6', '3', now())");
-	}
+  function install() {
+    xtc_db_query("insert into ".TABLE_CONFIGURATION." ( configuration_key, configuration_value,  configuration_group_id, sort_order, set_function, date_added) values ('MODULE_PAYMENT_COD_STATUS', 'True',  '6', '1', 'xtc_cfg_select_option(array(\'True\', \'False\'), ', now())");
+    xtc_db_query("insert into ".TABLE_CONFIGURATION." ( configuration_key, configuration_value,  configuration_group_id, sort_order, date_added) values ('MODULE_PAYMENT_COD_ALLOWED', '', '6', '0', now())");
+    xtc_db_query("insert into ".TABLE_CONFIGURATION." ( configuration_key, configuration_value,  configuration_group_id, sort_order, use_function, set_function, date_added) values ('MODULE_PAYMENT_COD_ZONE', '0', '6', '2', 'xtc_get_zone_class_title', 'xtc_cfg_pull_down_zone_classes(', now())");
+    xtc_db_query("insert into ".TABLE_CONFIGURATION." ( configuration_key, configuration_value,  configuration_group_id, sort_order, date_added) values ('MODULE_PAYMENT_COD_SORT_ORDER', '0',  '6', '0', now())");
+    xtc_db_query("insert into ".TABLE_CONFIGURATION." ( configuration_key, configuration_value,  configuration_group_id, sort_order, set_function, use_function, date_added) values ('MODULE_PAYMENT_COD_ORDER_STATUS_ID', '0','6', '0', 'xtc_cfg_pull_down_order_statuses(', 'xtc_get_order_status_name', now())");
+    xtc_db_query("insert into ".TABLE_CONFIGURATION." ( configuration_key, configuration_value,  configuration_group_id, sort_order, date_added) values ('MODULE_PAYMENT_COD_LIMIT_ALLOWED', '600', '6', '3', now())");
+  }
 
-	function remove() {
-		xtc_db_query("delete from ".TABLE_CONFIGURATION." where configuration_key in ('".implode("', '", $this->keys())."')");
-	}
+  function remove() {
+    xtc_db_query("delete from ".TABLE_CONFIGURATION." where configuration_key in ('".implode("', '", $this->keys())."')");
+  }
 
-	function keys() {
-		return array ('MODULE_PAYMENT_COD_STATUS',
+  function keys() {
+    return array ('MODULE_PAYMENT_COD_STATUS',
                   'MODULE_PAYMENT_COD_ALLOWED',
                   'MODULE_PAYMENT_COD_ZONE',
                   'MODULE_PAYMENT_COD_ORDER_STATUS_ID',
                   'MODULE_PAYMENT_COD_SORT_ORDER',
                   'MODULE_PAYMENT_COD_LIMIT_ALLOWED'
                   );
-	}
+  }
 }
 ?>
