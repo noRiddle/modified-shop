1eeb8bda2e6acbca6a1b6c1f24f80afd398ede9d
Tomcraft1980 <hallo@modified-shop.org>
2016-01-25 08:41:50 +0000

    Unified configure.php file and template
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@9258 79ac0487-9261-4f26-a34d-858dee0dcce0

--- _installer/includes/templates/configure.php
+++ _installer/includes/templates/configure.php
@@ -2,36 +2,37 @@
 $file_contents = 
 '<?php' . PHP_EOL .
 '/* --------------------------------------------------------------' . PHP_EOL .
+'   $Id$' . PHP_EOL .
 '' . PHP_EOL .
-'  modified eCommerce Shopsoftware' . PHP_EOL .
-'  http://www.modified-shop.org' . PHP_EOL .
+'   modified eCommerce Shopsoftware' . PHP_EOL .
+'   http://www.modified-shop.org' . PHP_EOL .
 '' . PHP_EOL .
 '   Copyright (c) 2009 - 2013 [www.modified-shop.org]' . PHP_EOL .
-'  --------------------------------------------------------------' . PHP_EOL .
-'  based on:' . PHP_EOL .
-'  (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)' . PHP_EOL .
-'  (c) 2002-2003 osCommerce (configure.php,v 1.13 2003/02/10); www.oscommerce.com' . PHP_EOL .
-'  (c) 2003 XT-Commerce (configure.php)' . PHP_EOL .
+'   --------------------------------------------------------------' . PHP_EOL .
+'   based on:' . PHP_EOL .
+'   (c) 2000-2001 The Exchange Project  (earlier name of osCommerce)' . PHP_EOL .
+'   (c) 2002-2003 osCommerce (configure.php,v 1.13 2003/02/10); www.oscommerce.com' . PHP_EOL .
+'   (c) 2003 XT-Commerce (configure.php)' . PHP_EOL .
 '' . PHP_EOL .
-'  Released under the GNU General Public License' . PHP_EOL .
-'  --------------------------------------------------------------*/' . PHP_EOL .
+'   Released under the GNU General Public License' . PHP_EOL .
+'   --------------------------------------------------------------*/' . PHP_EOL .
 '' . PHP_EOL .
-'// Define the webserver and path parameters' . PHP_EOL .
-'// * DIR_FS_* = Filesystem directories (local/physical)' . PHP_EOL .
-'// * DIR_WS_* = Webserver directories (virtual/URL)' . PHP_EOL .
+'  // Define the webserver and path parameters' . PHP_EOL .
+'  // * DIR_FS_* = Filesystem directories (local/physical)' . PHP_EOL .
+'  // * DIR_WS_* = Webserver directories (virtual/URL)' . PHP_EOL .
 '' . PHP_EOL .
-'// global defines' . PHP_EOL .
+'  // global defines' . PHP_EOL .
 '  define(\'HTTP_SERVER\', \'' . $http_server . '\'); // eg, http://localhost - should not be empty for productive servers' . PHP_EOL .
 '  define(\'HTTPS_SERVER\', \'' . $https_server . '\'); // eg, https://localhost - should not be empty for productive servers' . PHP_EOL .
 '  define(\'DIR_FS_DOCUMENT_ROOT\', \'' . DIR_FS_DOCUMENT_ROOT . '\'); // absolut path required' . PHP_EOL .
 '  define(\'DIR_WS_CATALOG\', \'' . $_POST['DIR_WS_CATALOG'] . '\'); // relative path required' . PHP_EOL .
 '  define(\'DIR_FS_CATALOG\', DIR_FS_DOCUMENT_ROOT);' . PHP_EOL .
 '' . PHP_EOL .
-'// secure SSL' . PHP_EOL .
+'  // secure SSL' . PHP_EOL .
 '  define(\'ENABLE_SSL\', ' . (($_POST['ENABLE_SSL'] == 'true') ? 'true' : 'false') . '); // secure webserver for checkout procedure?' . PHP_EOL .
 '  define(\'USE_SSL_PROXY\', ' . (($_POST['USE_SSL_PROXY'] == 'true') ? 'true' : 'false') . '); // using SSL proxy?' . PHP_EOL .
 '' . PHP_EOL .
-'// define our database connection' . PHP_EOL .
+'  // define our database connection' . PHP_EOL .
 '  define(\'DB_MYSQL_TYPE\', \'' . $_POST['DB_MYSQL_TYPE'] . '\'); // define mysql type set to \'mysql\' or \'mysqli\'' . PHP_EOL .
 '  define(\'DB_SERVER\', \'' . $_POST['DB_SERVER'] . '\'); // eg, localhost - should not be empty for productive servers' . PHP_EOL .
 '  define(\'DB_SERVER_USERNAME\', \'' . $_POST['DB_SERVER_USERNAME'] . '\');' . PHP_EOL .
@@ -42,10 +43,10 @@ $file_contents =
 '  define(\'DB_SERVER_CHARSET\', \'' . DB_SERVER_CHARSET . '\'); // set db charset \'utf8\' or \'latin1\'' . PHP_EOL .
 '' . PHP_EOL .
 '  if (DB_DATABASE != \'\') {' . PHP_EOL . 
-'  // admin directory' . PHP_EOL . 
-'    require_once (DIR_FS_CATALOG . \'inc/set_admin_directory.inc.php\');' . PHP_EOL .
+'    // set admin directory DIR_ADMIN' . PHP_EOL . 
+'    require_once(DIR_FS_CATALOG.\'inc/set_admin_directory.inc.php\');' . PHP_EOL .
 '' . PHP_EOL .
-'  // include standard settings' . PHP_EOL .
+'    // include standard settings' . PHP_EOL .
 '    require(DIR_FS_CATALOG.(defined(\'RUN_MODE_ADMIN\')? DIR_ADMIN : \'\').\'includes/paths.php\');'. PHP_EOL .
 '  }'. PHP_EOL .
 '?>';
