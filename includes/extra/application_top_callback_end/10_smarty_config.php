0553a78d6ecc3d5b3c297d7e81da2dcd69a53dcd
modGTB <gerhard@modified-shop.org>
2016-02-06 13:14:32 +0000

    prevent checking for specials and whats new for callbacks
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@9337 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/extra/application_top_callback_end/10_smarty_config.php
+++ includes/extra/application_top_callback_end/10_smarty_config.php
@@ -6,6 +6,9 @@ if (isset($_GET['show'])) {
 
 // load Template config
 if (file_exists(DIR_FS_CATALOG.'templates/'.CURRENT_TEMPLATE.'/config/config.php')) {
+  defined('SPECIALS_CATEGORIES') or define('SPECIALS_CATEGORIES', false);
+  defined('WHATSNEW_CATEGORIES') or define('WHATSNEW_CATEGORIES', false);
+
   require(DIR_FS_CATALOG.'templates/'.CURRENT_TEMPLATE.'/config/config.php');
 }
 ?>
