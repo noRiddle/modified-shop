553d26b1931d69115e86756eecf23a6766c4da20
web28 <web28@modified-shop.org>
2016-02-20 13:26:18 +0000

    add hook point
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@9411 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/define_add_select.php
+++ includes/define_add_select.php
@@ -11,21 +11,27 @@
    ---------------------------------------------------------------------------------------*/
 /*
 example for default.php
-can be used in extra hook points:
+can be used in extra/define_add_select/yourfile.php
 
-if (!isset($add_select_default)) $add_select_default = array();
 $add_select_default[] = 'p.products_extra_field';
 
 */
-  // used in /includes/modules/default.php
-  define('ADD_SELECT_DEFAULT', 'p.products_manufacturers_model, '.(isset($add_select_default) ? rtrim(implode(', ', $add_select_default), ',').', ' : ''));
+  $add_select_product = array();
+  $add_select_default = array();
+  $add_select_search = array();
+  $add_select_cart = array();
   
-  // used in /advanced_search_result.php
-  define('ADD_SELECT_SEARCH', 'p.products_manufacturers_model, '.(isset($add_select_search) ? rtrim(implode(', ', $add_select_search), ',').', ' : ''));
+  foreach(auto_include(DIR_FS_CATALOG.'includes/extra/define_add_select/','php') as $file) require ($file);
+
+  // used in /includes/modules/default.php - used for all product listings
+  define('ADD_SELECT_DEFAULT', 'p.products_manufacturers_model, '.(count($add_select_default) ? rtrim(implode(', ', $add_select_default), ',').', ' : ''));
   
-  // used in /includes/classes/product.php
-  define('ADD_SELECT_PRODUCT', (isset($add_select_product) ? rtrim(implode(', ', $add_select_product), ',').', ' : ''));
+  // used in /advanced_search_result.php - used for search results
+  define('ADD_SELECT_SEARCH', 'p.products_manufacturers_model, '.(count($add_select_search) ? rtrim(implode(', ', $add_select_search), ',').', ' : ''));
   
-  // used in /includes/classes/shopping_cart.php
-  define('ADD_SELECT_CART', (isset($add_select_cart) ? rtrim(implode(', ', $add_select_cart), ',').', ' : ''));
+  // used in /includes/classes/product.php - used for products
+  define('ADD_SELECT_PRODUCT', (count($add_select_product) ? rtrim(implode(', ', $add_select_product), ',').', ' : ''));
+  
+  // used in /includes/classes/shopping_cart.php -  used for cart details
+  define('ADD_SELECT_CART', (count($add_select_cart) ? rtrim(implode(', ', $add_select_cart), ',').', ' : ''));
 ?>
