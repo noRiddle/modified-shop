d16dafc6d3bf5a94df9eb8b13c32928d79302647
modGTB <gerhard@modified-shop.org>
2016-03-21 11:46:25 +0000

    avoid js error
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/tags/modified-shop-1.06@9607 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/includes/configuration_limits.php
+++ admin/includes/configuration_limits.php
@@ -25,3 +25,5 @@ $value_limits['MAX_DISPLAY_ADVANCED_SEARCH_RESULTS'] = array('min' => 1);
 $value_limits['STORE_DB_SLOW_QUERY_TIME'] = array('min' => 0);
 $value_limits['STORE_PAGE_PARSE_TIME_THRESHOLD'] = array('min' => 0);
 $value_limits['SECURITY_CODE_LENGTH'] = array('min' => 0);
+
+$value_limits['REVIEW_TEXT_MIN_LENGTH'] = array('min' => 0);
