b761eadf6c50a41546bf850149d4c9f606983283
Tomcraft1980 <hallo@modified-shop.org>
2016-06-14 15:29:02 +0000

    Update blz file
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@9976 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/blz_update.php
+++ admin/blz_update.php
@@ -16,7 +16,7 @@ require('includes/application_top.php');
 // include needed function
 require_once(DIR_FS_INC.'get_external_content.inc.php');
 
-$blz_file_default_link = 'http://www.bundesbank.de/Redaktion/DE/Downloads/Aufgaben/Unbarer_Zahlungsverkehr/Bankleitzahlen/2015_12_06/blz_2015_09_07_txt.txt?__blob=publicationFile';
+$blz_file_default_link = 'http://www.bundesbank.de/Redaktion/DE/Downloads/Aufgaben/Unbarer_Zahlungsverkehr/Bankleitzahlen/2016_09_04/blz_2016_06_06_txt.txt?__blob=publicationFile';
 
 require (DIR_WS_INCLUDES.'head.php');
 ?>
