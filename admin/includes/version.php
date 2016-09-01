f6c74c8d505f0667a842325b71e1b4aa9ab368f2
Tomcraft1980 <hallo@modified-shop.org>
2016-09-01 11:12:56 +0000

    Update project version in /trunk
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@10273 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/includes/version.php
+++ admin/includes/version.php
@@ -12,9 +12,9 @@ defined( '_VALID_XTC' ) or die( 'Direct Access to this location is not allowed.'
 
 define('PROJECT_MAJOR_VERSION', '2');
 define('PROJECT_MINOR_VERSION', '0.1.0');
-define('PROJECT_REVISION', '9678'); // ToDo before release!
+define('PROJECT_REVISION', '10274'); // ToDo before release!
 define('PROJECT_SERVICEPACK_VERSION', '');
-define('PROJECT_RELEASE_DATE', '2016-04-02'); // ToDo before release!
+define('PROJECT_RELEASE_DATE', '2016-10-01'); // ToDo before release!
 define('MINIMUM_DB_VERSION', '200'); // currently not in use
 
 // Define the project version
