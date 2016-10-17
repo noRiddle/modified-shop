1501439d5391c1fe1c940c79c301061ee60c93ef
Tomcraft1980 <hallo@modified-shop.org>
2016-10-17 15:07:23 +0000

    Added new database_version for 2.0.1.1
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@10320 79ac0487-9261-4f26-a34d-858dee0dcce0

---
+++ _installer/update/update_2.0.1.0_to_2.0.1.1.sql
@@ -0,0 +1,13 @@
+# -----------------------------------------------------------------------------------------
+#  $Id$
+#
+#  modified eCommerce Shopsoftware
+#  http://www.modified-shop.org
+#
+#  Copyright (c) 2009 - 2013 [www.modified-shop.org]
+#  -----------------------------------------------------------------------------------------
+
+#Tomcraft - 2016-10-17 - changed database_version
+INSERT INTO `database_version` (`version`) VALUES ('MOD_2.0.1.1');
+
+# Keep an empty line at the end of this file for the db_updater to work properly
