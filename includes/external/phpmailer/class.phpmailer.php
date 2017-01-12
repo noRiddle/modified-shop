c9d61ba95df09637c81825d75ef3350746db4e05
modGTB <gerhard@modified-shop.org>
2017-01-12 15:39:01 +0000

    fix #1105 - update PHPMailer for debug
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@10559 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/external/phpmailer/class.phpmailer.php
+++ includes/external/phpmailer/class.phpmailer.php
@@ -714,7 +714,7 @@ class PHPMailer
             return;
         }
         //Avoid clash with built-in function names
-        if (!in_array($this->Debugoutput, array('error_log', 'html', 'echo')) and is_callable($this->Debugoutput)) {
+        if (!in_array($this->Debugoutput, array('error_log', 'html', 'echo', 'trigger_error')) and is_callable($this->Debugoutput)) {
             call_user_func($this->Debugoutput, $str, $this->SMTPDebug);
             return;
         }
