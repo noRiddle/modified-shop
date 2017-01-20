931b10b2a942aedea66f12347eb54a0273c6806b
Tomcraft1980 <hallo@modified-shop.org>
2017-01-20 14:44:14 +0000

    Changed final revision number for 2.0.2.0
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@10590 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/includes/version.php
+++ admin/includes/version.php
@@ -10,9 +10,9 @@
 
 define('PROJECT_MAJOR_VERSION', '2');
 define('PROJECT_MINOR_VERSION', '0.2.0');
-define('PROJECT_REVISION', '10403'); // ToDo before release!
+define('PROJECT_REVISION', '10591'); // ToDo before release!
 define('PROJECT_SERVICEPACK_VERSION', '');
-define('PROJECT_RELEASE_DATE', '2016-11-09'); // ToDo before release!
+define('PROJECT_RELEASE_DATE', '2017-01-20'); // ToDo before release!
 define('MINIMUM_DB_VERSION', '200'); // currently not in use
 
 // Define the project version
