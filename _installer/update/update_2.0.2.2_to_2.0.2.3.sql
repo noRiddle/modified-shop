7003a6a911917a8158a734336a51de3c9c0493ce
Tomcraft1980 <hallo@modified-shop.org>
2017-06-15 16:04:50 +0000

    Addition on r10781 (fix #1179)
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@10802 79ac0487-9261-4f26-a34d-858dee0dcce0

--- _installer/update/update_2.0.2.2_to_2.0.2.3.sql
+++ _installer/update/update_2.0.2.2_to_2.0.2.3.sql
@@ -10,4 +10,8 @@
 #Tomcraft - 2017-03-08 - changed database_version
 INSERT INTO `database_version` (`version`) VALUES ('MOD_2.0.2.3');
 
+#GTB - 2017-06-10 - fix #1179
+UPDATE admin_access SET filemanager = 1 WHERE customers_id = 1 LIMIT 1;
+ALTER TABLE admin_access DROP fck_wrapper;
+
 # Keep an empty line at the end of this file for the db_updater to work properly
