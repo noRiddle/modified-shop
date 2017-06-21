952f814a4db539817a7447d3262942fba4e92437
Tomcraft1980 <hallo@modified-shop.org>
2017-06-21 09:46:28 +0000

    Updated shopgate module to version 2.9.38
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@10811 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/external/shopgate/plugin.php
+++ includes/external/shopgate/plugin.php
@@ -21,7 +21,7 @@
  * @author Shopgate GmbH <interfaces@shopgate.com>
  */
 
-define("SHOPGATE_PLUGIN_VERSION", "2.9.37");
+define("SHOPGATE_PLUGIN_VERSION", "2.9.38");
 
 /**
  * Modified eCommerce Plugin for Shopgate
