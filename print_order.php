2fc72c4a92de5336cdc31cc0e64c227130e4b969
Tomcraft1980 <hallo@modified-shop.org>
2017-09-14 13:21:32 +0000

    Extend print_order.php to display all success messages for all payment modules
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@10956 79ac0487-9261-4f26-a34d-858dee0dcce0

--- print_order.php
+++ print_order.php
@@ -67,11 +67,11 @@ if ((isset($_SESSION['customer_id']) && $_SESSION['customer_id'] == $order_check
   $smarty->assign('COMMENT', $order->info['comments']);
   $smarty->assign('DATE', xtc_date_long($order->info['date_purchased']));
 
-  if (strpos($order->info['payment_method'], 'paypalplus') !== false) {
-    require_once(DIR_FS_EXTERNAL.'paypal/classes/PayPalInfo.php');
-    $paypal = new PayPalInfo($order->info['payment_method']);      
-    $smarty->assign('PAYMENT_INFO', $paypal->get_payment_instructions($order->info['order_id']));
-  }
+  $last_order = $oID;
+  // load the selected payment module
+  require_once (DIR_WS_CLASSES . 'payment.php');
+  $payment_modules = new payment($order->info['payment_class']);
+  $smarty->assign('PAYMENT_INFO', $payment_modules->success());
 
   // dont allow cache
   $smarty->caching =0;
