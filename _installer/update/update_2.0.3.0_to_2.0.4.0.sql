9d8b8f0e4247809cf6f1a11ebc36c17522b58606
Tomcraft1980 <hallo@modified-shop.org>
2018-03-16 17:40:00 +0000

    Addition on r11093 (new products attributes handling)
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@11094 79ac0487-9261-4f26-a34d-858dee0dcce0

--- _installer/update/update_2.0.3.0_to_2.0.4.0.sql
+++ _installer/update/update_2.0.3.0_to_2.0.4.0.sql
@@ -13,4 +13,7 @@ INSERT INTO `database_version` (`version`) VALUES ('MOD_2.0.4.0');
 #Tomcraft - 2018-02-06 - change Online Dispute Resolution (odr) links from http to https
 UPDATE content_manager SET content_text = REPLACE(content_text, 'http://ec.europa.eu/consumers/odr', 'https://ec.europa.eu/consumers/odr');
 
+#GTB - 2018-03-16 - new products attributes handling
+ALTER TABLE products_options_values ADD products_options_values_sortorder INT(11) NOT NULL AFTER products_options_values_name;
+
 # Keep an empty line at the end of this file for the db_updater to work properly
