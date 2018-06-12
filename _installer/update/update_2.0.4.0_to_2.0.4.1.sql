2599058919f897ff32aa0b2b0c1280d10ffc60d9
modGTB <gerhard@modified-shop.org>
2018-06-12 12:07:00 +0000

    fix #1462
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@11276 79ac0487-9261-4f26-a34d-858dee0dcce0

--- _installer/update/update_2.0.4.0_to_2.0.4.1.sql
+++ _installer/update/update_2.0.4.0_to_2.0.4.1.sql
@@ -1,5 +1,5 @@
 # -----------------------------------------------------------------------------------------
-#  $Id: update_2.0.3.0_to_2.0.4.0.sql 11159 2018-05-30 10:54:05Z Tomcraft $
+#  $Id$
 #
 #  modified eCommerce Shopsoftware
 #  http://www.modified-shop.org
@@ -13,5 +13,8 @@ INSERT INTO `database_version` (`version`) VALUES ('MOD_2.0.4.1');
 #GTB - 2018-06-11 - fix #1378
 ALTER TABLE orders_products MODIFY products_weight DECIMAL(15,4) NOT NULL;
 ALTER TABLE products MODIFY products_weight DECIMAL(15,4) NOT NULL;
+
+#GTB - 2018-06-12 - fix #1462
+REPLACE INTO `customers_status` SELECT cs.`customers_status_id`, 2, cs.`customers_status_name`, cs.`customers_status_public`, cs.`customers_status_min_order`, cs.`customers_status_max_order`, cs.`customers_status_image`, cs.`customers_status_discount`, cs.`customers_status_ot_discount_flag`, cs.`customers_status_ot_discount`, cs.`customers_status_graduated_prices`, cs.`customers_status_show_price`, cs.`customers_status_show_price_tax`, cs.`customers_status_show_tax_total`, cs.`customers_status_add_tax_ot`, cs.`customers_status_payment_unallowed`, cs.`customers_status_shipping_unallowed`, cs.`customers_status_discount_attributes`, cs.`customers_fsk18`, cs.`customers_fsk18_display`, cs.`customers_status_write_reviews`, cs.`customers_status_read_reviews`, cs.`customers_status_reviews_status`, cs.`customers_status_specials` FROM `customers_status` cs WHERE cs.`customers_status_id` = '0' AND cs.`language_id` = '1';
 
 # Keep an empty line at the end of this file for the db_updater to work properly
