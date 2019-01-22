1252b7387feb3f9253ba57862ec51ffc8902a60b
modGTB <gerhard@modified-shop.org>
2019-01-22 15:10:03 +0000

    fix #1578
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@11459 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/classes/shopping_cart.php
+++ includes/classes/shopping_cart.php
@@ -973,52 +973,69 @@ class shoppingCart {
     
     $url = '';
     if (!empty($_SERVER['HTTP_REFERER']) 
-        && strpos($_SERVER['HTTP_REFERER'], FILENAME_SHOPPING_CART) === false  
-        && strpos($_SERVER['HTTP_REFERER'], 'in_cart') === false 
-        && strpos($_SERVER['HTTP_REFERER'], 'checkout_') === false
         && filter_var($_SERVER['HTTP_REFERER'], FILTER_VALIDATE_URL) !== false
         )
     {
+      $basename = '';
       $referer = parse_url($_SERVER['HTTP_REFERER']);
+
       if (isset($referer['host'])
           && (strpos(HTTP_SERVER, $referer['host']) !== false
               || strpos(HTTPS_SERVER, $referer['host']) !== false
               )
           )
       {
-        $params = '';
-        if (isset($referer['query'])) {
-          parse_str($referer['query'], $params_array);
-      
-          $valid_params = array(
-            'cPath',
-            'products_id',
-            'filter_id',
-            'filter',
-            'manufacturers_id',
-            'categories_id',
-            'inc_subcat',
-            'keywords',
-            'pfrom',
-            'pto',
-            'page',
-          );
-      
-          foreach ($params_array as $k => $v) {
-            if (!in_array($k, $valid_params)) {
-              unset($params_array[$k]);
+        switch (basename($referer['path'])) {
+          case FILENAME_LOGOFF:
+          case FILENAME_LOGIN:
+            $basename = FILENAME_DEFAULT;
+            break;
+          
+          default:
+            if (basename($referer['path']) != FILENAME_SHOPPING_CART
+                && strpos($referer['path'], 'checkout_') === false
+                )
+            {
+              $basename = ltrim($referer['path'], '/');
             }
-          }
-          $params = http_build_query($params_array, '', '&');
+            break;
         }
+      }      
+
+      $params = '';
+      if (isset($referer['query'])) {
+        parse_str($referer['query'], $params_array);
     
-        $url = xtc_href_link(ltrim($referer['path'], '/'), $params, $request_type);
+        $valid_params = array(
+          'cPath',
+          'products_id',
+          'filter_id',
+          'filter',
+          'manufacturers_id',
+          'categories_id',
+          'inc_subcat',
+          'keywords',
+          'pfrom',
+          'pto',
+          'page',
+        );
+    
+        foreach ($params_array as $k => $v) {
+          if (!in_array($k, $valid_params)) {
+            unset($params_array[$k]);
+          }
+        }
+        $params = http_build_query($params_array, '', '&');
+      }
+      
+      if ($basename != '') {
+        $url = xtc_href_link($basename, $params, $request_type);
 
         //new module support 
         $url = $this->shoppingCartModules->get_continue_shopping_link($url, $referer);
       }
     }
-    
+
     return $url;
   }
   
