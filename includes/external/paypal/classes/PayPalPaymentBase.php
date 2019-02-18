c7668e2ea61efec4b9d8a181767c0d5e818564ca
Tomcraft1980 <hallo@modified-shop.org>
2019-02-18 18:33:59 +0000

    update paypal - raise module version from 1.7 to 1.8
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@11538 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/external/paypal/classes/PayPalPaymentBase.php
+++ includes/external/paypal/classes/PayPalPaymentBase.php
@@ -27,7 +27,7 @@ class PayPalPaymentBase extends PayPalCommon {
     global $order;
 
     $this->code = $class;
-    $this->paypal_version = '1.7';
+    $this->paypal_version = '1.8';
 
     $this->admin_access_array = array(
       'paypal_info',
