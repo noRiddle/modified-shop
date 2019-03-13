04fa2558be4f1ce899b021d3a1fdb16a36663ca3
Tomcraft1980 <hallo@modified-shop.org>
2019-03-13 18:57:58 +0000

    update paypal - fixed typo (props noRiddle)
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@11550 79ac0487-9261-4f26-a34d-858dee0dcce0

--- lang/english/admin/english.php
+++ lang/english/admin/english.php
@@ -589,7 +589,7 @@ define('TEXT_PAYPAL_TAB_CONFIG', 'PayPal Configuration');
 define('TEXT_PAYPAL_TAB_PROFILE', 'PayPal Profile');
 define('TEXT_PAYPAL_TAB_WEBHOOK', 'PayPal Webhook');
 define('TEXT_PAYPAL_TAB_MODULE', 'PayPal Products');
-define('TEXT_PAYPAL_TAB_INFO', 'PayPal Informations');
+define('TEXT_PAYPAL_TAB_INFO', 'PayPal Information');
 
 define('TEXT_DEFAULT_SORT_ORDER_TITLE', 'Sort order');
 define('TEXT_DEFAULT_SORT_ORDER_DESC', 'Order of processing. Smallest number is executed first.');
