0dcd8f05fede786028f26bae9395f864f5212098
modGTB <gerhard@modified-shop.org>
2019-04-12 14:45:26 +0000

    fix #1550
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@11758 79ac0487-9261-4f26-a34d-858dee0dcce0

--- inc/check_country_required_zones.inc.php
+++ inc/check_country_required_zones.inc.php
@@ -1,37 +1,23 @@
 <?php
-/**
- * $Id$
- *
- * modified eCommerce Shopsoftware
- * http://www.modified-shop.org
- *
- * Copyright (c) 2009 - 2013 [www.modified-shop.org]
- *
- * Released under the GNU General Public License
- */
+/* -----------------------------------------------------------------------------------------
+   $Id$
+
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
+
+   Copyright (c) 2009 - 2013 [www.modified-shop.org]
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
  
  
-function check_country_required_zones($country_id) 
-{
-    $query = xtc_db_query(
-      "SELECT count(*) AS total  
-         FROM ".TABLE_ZONES." z 
-         JOIN ".TABLE_COUNTRIES." c 
-           ON c.countries_id = z.zone_country_id 
-        WHERE z.zone_country_id = '".(int)$country_id."'
-      "); 
-    
+  function check_country_required_zones($country_id) {
+    $query = xtc_db_query("SELECT SUM(c.required_zones) as total
+                             FROM ".TABLE_ZONES." z 
+                             JOIN ".TABLE_COUNTRIES." c 
+                               ON c.countries_id = z.zone_country_id 
+                            WHERE z.countries_id = '".(int)$country_id."'"); 
     $check = xtc_db_fetch_array($query); 
-    if ($check['total'] > 0) {
-        $query = xtc_db_query(
-          "SELECT required_zones
-            FROM ".TABLE_COUNTRIES."
-           WHERE countries_id  = '".(int)$country_id."'
-          ");
-        if (xtc_db_num_rows($query)) {
-            $dbData = xtc_db_fetch_array($query);
-            return $dbData['required_zones'];
-        } 
-    }
-    return true;
-}
+    return (($dbData['total'] > 0) ? true : false);
+  }
+?>
