6377ea9ffe70ea60d04897c62f5d1f3e6bd774d6
modGTB <gerhard@modified-shop.org>
2019-04-16 05:16:16 +0000

    revised  r11758 (fix #1550)
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@11780 79ac0487-9261-4f26-a34d-858dee0dcce0

--- inc/check_country_required_zones.inc.php
+++ inc/check_country_required_zones.inc.php
@@ -12,12 +12,12 @@
  
  
   function check_country_required_zones($country_id) {
-    $query = xtc_db_query("SELECT SUM(c.required_zones) as total
-                             FROM ".TABLE_ZONES." z 
-                             JOIN ".TABLE_COUNTRIES." c 
-                               ON c.countries_id = z.zone_country_id 
-                            WHERE c.countries_id = '".(int)$country_id."'"); 
-    $check = xtc_db_fetch_array($query); 
-    return (($dbData['total'] > 0) ? true : false);
+    $check_query = xtc_db_query("SELECT SUM(c.required_zones) as total
+                                   FROM ".TABLE_ZONES." z 
+                                   JOIN ".TABLE_COUNTRIES." c 
+                                     ON c.countries_id = z.zone_country_id 
+                                  WHERE c.countries_id = '".(int)$country_id."'"); 
+    $check = xtc_db_fetch_array($check_query); 
+    return (($check['total'] > 0) ? true : false);
   }
 ?>
