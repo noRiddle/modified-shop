818a6f208153168767ed0a83bb38cd023226a995
Tomcraft1980 <hallo@modified-shop.org>
2019-05-02 17:20:15 +0000

    update paypal - raise module version from 1.21 to 1.22 due to r11822 & r11823 (update paypal - fix r11816 (update paypal - add primary keys))
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@11824 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/external/paypal/classes/PayPalPaymentBase.php
+++ includes/external/paypal/classes/PayPalPaymentBase.php
@@ -27,7 +27,7 @@ class PayPalPaymentBase extends PayPalCommon {
     global $order;
 
     $this->code = $class;
-    $this->paypal_version = '1.21';
+    $this->paypal_version = '1.22';
 
     $this->admin_access_array = array(
       'paypal_info',
