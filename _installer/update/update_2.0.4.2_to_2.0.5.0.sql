adbde1d26dd2dce2015b34ccc918fdbca96592e7
Tomcraft1980 <hallo@modified-shop.org>
2019-06-28 11:30:49 +0000

    Revised r11863 (fix #1342)
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@11867 79ac0487-9261-4f26-a34d-858dee0dcce0

--- _installer/update/update_2.0.4.2_to_2.0.5.0.sql
+++ _installer/update/update_2.0.4.2_to_2.0.5.0.sql
@@ -161,7 +161,7 @@ ALTER TABLE `zones_to_geo_zones` MODIFY `zone_country_id` INT(11) NOT NULL;
 #GTB - 2019-06-28 - raise length for images
 ALTER TABLE `banners` MODIFY `banners_image` VARCHAR(255) NOT NULL;
 ALTER TABLE `categories` MODIFY `categories_image` VARCHAR(255) NOT NULL;
-ALTER TABLE `lanugages` MODIFY `images` VARCHAR(64) NOT NULL;
+ALTER TABLE `lanugages` MODIFY `image` VARCHAR(64) NOT NULL;
 ALTER TABLE `manufacturers` MODIFY `manufacturers_image` VARCHAR(255) NOT NULL;
 ALTER TABLE `products` MODIFY `products_image` VARCHAR(255) NOT NULL;
 ALTER TABLE `products_images` MODIFY `image_name` VARCHAR(255) NOT NULL;
