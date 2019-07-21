e4fd3e085f7bc36b10375128bdd64301f080a247
modGTB <gerhard@modified-shop.org>
2019-07-21 08:56:44 +0000

    update tags for checkout - fix #1585 & #1586
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@11944 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/modules/order/productTagsOnCheckout.php
+++ includes/modules/order/productTagsOnCheckout.php
@@ -57,10 +57,18 @@ class productTagsOnCheckout {  //Important same name as filename
 
     function cart_products($products_data, $products_id) 
     {
-      $tags_query = xtDBquery("SELECT pto.options_id,
+      $tags_query = xtDBquery("SELECT ".ADD_TAGS_SELECT."
+                                      pto.options_id,
                                       pto.options_name,
+                                      pto.options_description,
+                                      pto.sort_order AS options_sort_order,
+                                      pto.options_content_group,
                                       ptv.values_id,
-                                      ptv.values_name
+                                      ptv.values_name,
+                                      ptv.values_description,
+                                      ptv.sort_order AS values_sort_order,
+                                      ptv.values_image,
+                                      ptv.values_content_group
                                  FROM ".TABLE_PRODUCTS_TAGS." pt
                                  JOIN ".TABLE_PRODUCTS_TAGS_OPTIONS." pto
                                       ON pt.options_id = pto.options_id
@@ -70,28 +78,32 @@ class productTagsOnCheckout {  //Important same name as filename
                                       ON ptv.values_id = pt.values_id
                                          AND ptv.status = '1'
                                          AND ptv.languages_id = '".(int)$_SESSION['languages_id']."'
-                                WHERE pt.products_id = '".xtc_get_prid($products_id)."'
-                             ORDER BY pto.sort_order, ptv.sort_order");
+                                WHERE pt.products_id = '".(int)$products_id."'
+                             ORDER BY pt.sort_order, pto.sort_order, ptv.sort_order");
 
       if (xtc_db_num_rows($tags_query, true) > 0) {
         $module_content = array();
         while ($tags = xtc_db_fetch_array($tags_query, true)) {
           if (!isset($module_content[$tags['options_id']])) {
             $module_content[$tags['options_id']] = array('OPTIONS_NAME' => $tags['options_name'],
+                                                         'OPTIONS_ID' => $tags['options_id'],
+                                                         'OPTIONS_SORT_ORDER' => $tags['options_sort_order'],
+                                                         'OPTIONS_DESCRIPTION' => $tags['options_description'],
+                                                         'OPTIONS_CONTENT_LINK' => (($tags['options_content_group'] != '') ? xtc_href_link(FILENAME_POPUP_CONTENT, 'coID='.$tags['options_content_group'], 'NONSSL') : ''),
                                                          'DATA' => array());
           }
-          $module_content[$tags['options_id']]['DATA'][] = array('VALUES_NAME' => $tags['values_name']);
+          $module_content[$tags['options_id']]['DATA'][] = array('VALUES_NAME' => $tags['values_name'],
+                                                                 'VALUES_ID' => $tags['values_id'],
+                                                                 'VALUES_SORT_ORDER' => $tags['values_sort_order'],
+                                                                 'VALUES_DESCRIPTION' => $tags['values_description'],
+                                                                 'VALUES_IMAGE' => (($tags['values_image'] != '' && is_file(DIR_FS_CATALOG.DIR_WS_IMAGES.$tags['values_image'])) ? DIR_WS_BASE.DIR_WS_IMAGES.$tags['values_image'] : ''),
+                                                                 'VALUES_CONTENT_LINK' => (($tags['values_content_group'] != '') ? xtc_href_link(FILENAME_POPUP_CONTENT, 'coID='.$tags['values_content_group'], 'NONSSL') : ''),
+                                                                 );
+          foreach(auto_include(DIR_FS_CATALOG.'includes/extra/modules/products_tags_data/','php') as $file) require ($file);
         }
-  
-        if (count($module_content) > 0) {
-          foreach ($module_content as $option) {
-            $products_data['order_description'] .= '<b>'.$option['OPTIONS_NAME'].':</b> ';
-            foreach ($option['DATA'] as $value) {
-              $products_data['order_description'] .= $value['VALUES_NAME'].', ';
-            }
-            $products_data['order_description'] = substr($products_data['order_description'], 0, -2).'<br/>';
-          }
-        }
+        
+        foreach(auto_include(DIR_FS_CATALOG.'includes/extra/modules/products_tags_end/','php') as $file) require ($file);
+        $products_data['products_tags'] = $module_content;        
       }
       return $products_data;    
     }
