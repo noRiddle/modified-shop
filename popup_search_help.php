1aec4c11f5aa717397bc2fc7c2579cb1c680b66f
modGTB <gerhard@modified-shop.org>
2019-07-27 10:14:56 +0000

    fix notice
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@12027 79ac0487-9261-4f26-a34d-858dee0dcce0

--- popup_search_help.php
+++ popup_search_help.php
@@ -1,6 +1,6 @@
 <?php
 /* -----------------------------------------------------------------------------------------
-   $Id: popup_search_help.php 1238 2005-09-24 10:51:19Z mz $
+   $Id$
 
    XT-Commerce - community made shopping
    http://www.xt-commerce.com
@@ -23,7 +23,6 @@ $popup_smarty->assign('tpl_path', DIR_WS_BASE.'templates/'.CURRENT_TEMPLATE.'/')
 $popup_smarty->assign('html_params', ((TEMPLATE_HTML_ENGINE == 'xhtml') ? ' '.HTML_PARAMS : ' lang="'.$_SESSION['language_code'].'"'));
 $popup_smarty->assign('doctype', ((TEMPLATE_HTML_ENGINE == 'xhtml') ? ' PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"' : ''));
 $popup_smarty->assign('charset', $_SESSION['language_charset']);
-$popup_smarty->assign('title', htmlspecialchars($content_data['content_heading'], ENT_QUOTES, strtoupper($_SESSION['language_charset'])));
 if (DIR_WS_BASE == '') {
   $popup_smarty->assign('base', (($request_type == 'SSL') ? HTTPS_SERVER : HTTP_SERVER) . DIR_WS_CATALOG);
 }
