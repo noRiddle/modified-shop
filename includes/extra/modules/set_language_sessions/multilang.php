8d22cb7c5b076625433ba47ca154a6e6920a37f9
modGTB <gerhard@modified-shop.org>
2019-07-27 10:03:04 +0000

    fix warning
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@12024 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/extra/modules/set_language_sessions/multilang.php
+++ includes/extra/modules/set_language_sessions/multilang.php
@@ -14,6 +14,7 @@
       && defined('SEO_URL_MOD_CLASS')
       && defined('MODULE_MULTILANG_STATUS')
       && MODULE_MULTILANG_STATUS == 'true'
+      && !defined('RUN_MODE_ADMIN')
       )
   {
     $seo_url_sites = array(
