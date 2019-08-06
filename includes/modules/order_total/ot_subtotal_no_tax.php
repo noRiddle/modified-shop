3c72f3e5a4f1bbc6cc4a03cb5f17e0750e0772fd
modGTB <gerhard@modified-shop.org>
2019-08-06 16:21:58 +0000

    fix tax calculation
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@12070 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/modules/order_total/ot_subtotal_no_tax.php
+++ includes/modules/order_total/ot_subtotal_no_tax.php
@@ -38,7 +38,7 @@
       // merchant
       if (($_SESSION['customers_status']['customers_status_show_price_tax'] == 0 
            && $_SESSION['customers_status']['customers_status_add_tax_ot'] == 1
-          ) || ($_SESSION['customers_status']['customers_status_add_tax_ot'] == 0
+          ) || ($_SESSION['customers_status']['customers_status_show_price_tax'] == 0
                 && $_SESSION['customers_status']['customers_status_add_tax_ot'] == 0
                 && $order->delivery['country_id'] == STORE_COUNTRY
                 )
