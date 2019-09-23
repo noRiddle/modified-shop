6f71dca7ce6938352f1a36e6971616e6d6f9b649
modGTB <gerhard@modified-shop.org>
2019-09-23 09:31:03 +0000

    fix #1545 - update cookie consent
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@12157 79ac0487-9261-4f26-a34d-858dee0dcce0

--- lang/english/extra/cookieconsent.php
+++ lang/english/extra/cookieconsent.php
@@ -1,7 +1,7 @@
 <?php
  //additional fields
  define('TEXT_COOKIECONSENT_MESSAGE_INFO', 'Our website uses cookies to make your browsing expirience better. By using our site you agree to our use of cookies.');
- define('TEXT_COOKIECONSENT_MESSAGE_TRACKING', 'This website uses cookies to analyze website traffic and marketing efforts. By using our site, you agree to this use.');
+ define('TEXT_COOKIECONSENT_MESSAGE_TRACKING', 'In order to optimize our websites continuously and to be able to offer you a pleasant buying process, we would like to use cookies to analyze the page usage. As a visitor, you remain anonymous to us. We therefore ask you to agree to the cookie setting. You can revoke your consent at any time.');
  define('TEXT_COOKIECONSENT_DISSMISS', 'Agree!');
  define('TEXT_COOKIECONSENT_LINK', 'Learn more');
  define('TEXT_COOKIECONSENT_POLICY', 'Cookie Policy');
--- lang/german/extra/cookieconsent.php
+++ lang/german/extra/cookieconsent.php
@@ -1,9 +1,9 @@
 <?php
  //additional fields
  define('TEXT_COOKIECONSENT_MESSAGE_INFO', 'Um Ihnen ein besseres Nutzererlebnis zu bieten, verwenden wir Cookies. Durch Nutzung der Website stimmen Sie der Verwendung von Cookies zu.');
- define('TEXT_COOKIECONSENT_MESSAGE_TRACKING', 'Diese Website verwendet Cookies zur Analyse von Websitezugriffen und Marketingmaßnahmen. Durch die weitere Nutzung der Website stimmen Sie dieser Verwendung zu.');
+ define('TEXT_COOKIECONSENT_MESSAGE_TRACKING', 'Um unsere Webseiten fortlaufend zu optimieren und Ihnen damit einen angenehmen Kaufprozess anbieten zu können, möchten wir mit Hilfe von Cookies die Seitennutzung analysieren. Als Besucher bleiben Sie für uns anonym. Wir bitten Sie daher, in die Cookie-Setzung einzuwilligen. Ihre Einwilligung können Sie jederzeit widerrufen.');
  define('TEXT_COOKIECONSENT_DISSMISS', 'Einverstanden');
- define('TEXT_COOKIECONSENT_LINK', 'Mehr Informationen');
+ define('TEXT_COOKIECONSENT_LINK', 'Mehr Erfahren');
  define('TEXT_COOKIECONSENT_POLICY', 'Cookie Einstellungen');
  define('TEXT_COOKIECONSENT_ALLOW', 'Einverstanden');
  define('TEXT_COOKIECONSENT_DENY', 'Ablehnen');
