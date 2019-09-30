8e3b6720d481ae517a6ec0fde5778ff43c9542b3
Tomcraft1980 <hallo@modified-shop.org>
2019-09-30 19:58:25 +0000

    update paypal - raise module version from 1.27 to 1.28 due to r12214
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@12223 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/external/paypal/classes/PayPalPaymentBase.php
+++ includes/external/paypal/classes/PayPalPaymentBase.php
@@ -27,7 +27,7 @@ class PayPalPaymentBase extends PayPalCommon {
     global $order;
 
     $this->code = $class;
-    $this->paypal_version = '1.27';
+    $this->paypal_version = '1.28';
 
     $this->admin_access_array = array(
       'paypal_info',
