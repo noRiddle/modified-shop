09982c9c2b758606e9985cb0c66e43b48d4925d7
Tomcraft1980 <hallo@modified-shop.org>
2019-10-16 11:30:51 +0000

    fix #1545 - update cookie consent
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@12284 79ac0487-9261-4f26-a34d-858dee0dcce0

--- lang/german/extra/cookieconsent.php
+++ lang/german/extra/cookieconsent.php
@@ -2,7 +2,7 @@
  //additional fields
  define('TEXT_COOKIECONSENT_MESSAGE_INFO', 'Um Ihnen ein besseres Nutzererlebnis zu bieten, verwenden wir Cookies. Durch Nutzung der Website stimmen Sie der Verwendung von Cookies zu.');
  define('TEXT_COOKIECONSENT_MESSAGE_TRACKING', 'Um unsere Webseiten fortlaufend zu optimieren und Ihnen damit einen angenehmen Kaufprozess anbieten zu können, möchten wir mit Hilfe von Cookies die Seitennutzung analysieren. Als Besucher bleiben Sie für uns anonym. Wir bitten Sie daher, in die Cookie-Setzung einzuwilligen. Ihre Einwilligung können Sie jederzeit widerrufen.');
- define('TEXT_COOKIECONSENT_DISSMISS', 'Einverstanden');
+ define('TEXT_COOKIECONSENT_DISSMISS', 'Verstanden!');
  define('TEXT_COOKIECONSENT_LINK', 'Mehr Erfahren');
  define('TEXT_COOKIECONSENT_POLICY', 'Cookie Einstellungen');
  define('TEXT_COOKIECONSENT_ALLOW', 'Akzeptieren');
