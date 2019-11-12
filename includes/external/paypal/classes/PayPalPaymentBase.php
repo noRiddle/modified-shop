77bbba41a96fc3e3583f5f796f0d00cc90c088ea
Tomcraft1980 <hallo@modified-shop.org>
2019-11-12 19:14:27 +0000

    update paypal - raise module version from 1.31 to 1.32 due to r12400 & r12408
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@12409 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/external/paypal/classes/PayPalPaymentBase.php
+++ includes/external/paypal/classes/PayPalPaymentBase.php
@@ -27,7 +27,7 @@ class PayPalPaymentBase extends PayPalCommon {
     global $order;
 
     $this->code = $class;
-    $this->paypal_version = '1.31';
+    $this->paypal_version = '1.32';
 
     $this->admin_access_array = array(
       'paypal_info',
