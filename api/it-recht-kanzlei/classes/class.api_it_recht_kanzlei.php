a0224194aa9935c0722a8857768bd8db2172dd08
Tomcraft1980 <hallo@modified-shop.org>
2019-12-04 16:19:10 +0000

    Raised module version on IT-Recht Kanzlei module from 2.1.10 to 2.2.0 due to r12252 (update it recht kanzlei for new content manager)
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@12462 79ac0487-9261-4f26-a34d-858dee0dcce0

--- api/it-recht-kanzlei/classes/class.api_it_recht_kanzlei.php
+++ api/it-recht-kanzlei/classes/class.api_it_recht_kanzlei.php
@@ -18,7 +18,7 @@
 
 class api_it_recht_kanzlei {
   
-  public $modulversion = '2.1.10'; // shopversion . major . minor
+  public $modulversion = '2.2.0'; // shopversion . major . minor
   var $api_action_flag, 
       $api_version_flag, 
       $api_username_flag, 
