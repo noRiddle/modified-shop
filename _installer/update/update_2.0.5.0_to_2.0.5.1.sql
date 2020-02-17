f898b4698ffcad74db51cdb3263c81c04784b40f
Tomcraft1980 <hallo@modified-shop.org>
2020-02-17 15:24:14 +0000

    Addition on r12419 - Force Session Cookie Usage also on updated shops
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@12568 79ac0487-9261-4f26-a34d-858dee0dcce0

--- _installer/update/update_2.0.5.0_to_2.0.5.1.sql
+++ _installer/update/update_2.0.5.0_to_2.0.5.1.sql
@@ -10,4 +10,7 @@
 #Tomcraft - 2018-06-11 - changed database_version
 INSERT INTO `database_version` (`version`) VALUES ('MOD_2.0.5.1');
 
+#GTB - 2019-11-18 - Force Cookie Usage, see: https://trac.modified-shop.org/changeset/12419
+UPDATE configuration SET configuration_value = 'True' WHERE configuration_key = 'SESSION_FORCE_COOKIE_USE';
+
 # Keep an empty line at the end of this file for the db_updater to work properly
