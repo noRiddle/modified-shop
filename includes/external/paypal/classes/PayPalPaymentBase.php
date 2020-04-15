f2b8b09f65b8e2bcd493a001855ce7f5350c8a2b
Tomcraft1980 <hallo@modified-shop.org>
2020-04-15 14:01:33 +0000

    update paypal - raise module version from 1.42 to 1.43 due to r12701
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@12702 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/external/paypal/classes/PayPalPaymentBase.php
+++ includes/external/paypal/classes/PayPalPaymentBase.php
@@ -27,7 +27,7 @@ class PayPalPaymentBase extends PayPalCommon {
     global $order;
 
     $this->code = $class;
-    $this->paypal_version = '1.42';
+    $this->paypal_version = '1.43';
 
     $this->admin_access_array = array(
       'paypal_info',
