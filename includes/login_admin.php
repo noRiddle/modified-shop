2f5843fca00bbd1edfbc9a11e15fb5ddc0264751
Tomcraft1980 <hallo@modified-shop.org>
2020-04-20 20:39:25 +0000

    fix #1806 - fix typo in meta title on login_admin.php
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@12717 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/login_admin.php
+++ includes/login_admin.php
@@ -230,7 +230,7 @@ if(isset($_POST['repair'])  || isset($_POST['show_error'])) {
 <html>
 <head>
 <meta charset="utf-8" />
-<title>Administator-Login</title>
+<title>Administrator-Login</title>
 <meta name="robots" content="noindex, nofollow, noodp" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 <style type="text/css">
