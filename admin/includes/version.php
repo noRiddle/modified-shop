5801d894e01f95261420e1ac56d60100e8f80e7a
Tomcraft1980 <hallo@modified-shop.org>
2020-04-23 13:56:45 +0000

    fix typo in /admin/includes/version.php
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@12726 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/includes/version.php
+++ admin/includes/version.php
@@ -19,7 +19,7 @@ define('PROJECT_RELEASE_DATE', '2020-04-22'); // ToDo before release!
 define('MINIMUM_DB_VERSION', '200'); // currently not in use
 
 // Define the project version
-$version = 'modified eCommerce Shopssoftware v' . PROJECT_MAJOR_VERSION . '.' . PROJECT_MINOR_VERSION . ' rev ' . PROJECT_REVISION . ((PROJECT_SERVICEPACK_VERSION != '') ? ' SP' . PROJECT_SERVICEPACK_VERSION : ''). ' dated: ' . PROJECT_RELEASE_DATE;
+$version = 'modified eCommerce Shopsoftware v' . PROJECT_MAJOR_VERSION . '.' . PROJECT_MINOR_VERSION . ' rev ' . PROJECT_REVISION . ((PROJECT_SERVICEPACK_VERSION != '') ? ' SP' . PROJECT_SERVICEPACK_VERSION : ''). ' dated: ' . PROJECT_RELEASE_DATE;
 defined('PROJECT_VERSION') OR define('PROJECT_VERSION', $version);
 
 define('PROJECT_VERSION_NO', PROJECT_MAJOR_VERSION . '.' . PROJECT_MINOR_VERSION);
