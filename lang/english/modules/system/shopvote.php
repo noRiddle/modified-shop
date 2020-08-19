51ceeb66d91021cb9c060d086f99352a706ecd0c
Tomcraft1980 <hallo@modified-shop.org>
2020-08-19 14:43:36 +0000

    fix #1875 - revised text for Shopvote plugin
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@12866 79ac0487-9261-4f26-a34d-858dee0dcce0

--- lang/english/modules/system/shopvote.php
+++ lang/english/modules/system/shopvote.php
@@ -14,7 +14,7 @@
   define('MODULE_SHOPVOTE_STATUS_DESC','Module status');
   define('MODULE_SHOPVOTE_STATUS_INFO','Active');
   define('MODULE_SHOPVOTE_TEXT_TITLE', 'Shopvote');
-  define('MODULE_SHOPVOTE_TEXT_DESCRIPTION', 'With this plugin, you can easily integrate the best features of the SHOPVOTE evaluation portal into your shop. The integrated EasyReviews function automatically sends a legally safe customer request to your customers. Your customers can quickly and easily evaluate your company. The RatingStars feature ensures the best presentation of your ratings on your website and, on the other hand, the automatic generation of the evaluation stars in Google\'s organic search results.');    
+  define('MODULE_SHOPVOTE_TEXT_DESCRIPTION', 'With this plugin you can easily integrate the best features of the SHOPVOTE rating portal into your shop. The integrated EasyReviews function automatically and legally compliant sends your customers an evaluation request if the customer has previously agreed to the use of their data for evaluation reminders after the purchase has been completed. Your customers can thus evaluate your company quickly and easily. In the customer area of Shopvote, you can set the time after which Shopvote should send the review requests to your customers. The RatingStars function ensures the optimal presentation of your ratings on your website on the one hand and the automatic generation of rating stars in Google\'s organic search results on the other.');    
   define('MODULE_SHOPVOTE_SHOPID_TITLE', 'Shop-ID');
   define('MODULE_SHOPVOTE_SHOPID_DESC', 'Enter the Shop-ID.');
   define('MODULE_SHOPVOTE_API_KEY_TITLE', 'API-Key');
