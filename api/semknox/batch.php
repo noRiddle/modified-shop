ad6557911165734728abd79e9fcc74ffb8831471
modGTB <gerhard@modified-shop.org>
2021-01-26 08:28:26 +0000

    Semknox integration
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@13234 79ac0487-9261-4f26-a34d-858dee0dcce0

--- api/semknox/batch.php
+++ api/semknox/batch.php
@@ -16,6 +16,21 @@
       && MODULE_SEMKNOX_SYSTEM_STATUS == 'true'
       )
   {
+    // set the language
+    include_once (DIR_WS_MODULES.'set_language_sessions.php');
+
+    // language translations
+    require_once (DIR_WS_LANGUAGES.$_SESSION['language'].'/'.$_SESSION['language'].'.php');
+
+    // write customers status in session
+    require_once (DIR_WS_INCLUDES.'write_customers_status.php');
+
+    // content, product, category - sql group_check/fsk_lock
+    require_once (DIR_WS_INCLUDES.'define_conditions.php');
+
+    // add_select
+    require_once (DIR_WS_INCLUDES.'define_add_select.php');
+
     require_once(DIR_FS_EXTERNAL.'semknox/Semknox.php');
 
     require_once(DIR_WS_CLASSES . 'language.php');
