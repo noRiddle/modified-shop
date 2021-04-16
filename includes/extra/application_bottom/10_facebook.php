4ca88897d2f7c4c38457e6d7c13cf4500892b1b6
modGTB <gerhard@modified-shop.org>
2021-04-16 13:29:56 +0000

    fix #2024 - update facebook pixel
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@13523 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/extra/application_bottom/10_facebook.php
+++ includes/extra/application_bottom/10_facebook.php
@@ -12,49 +12,68 @@
 
     
   if (TRACKING_FACEBOOK_ACTIVE == 'true'
-      && strpos($PHP_SELF, FILENAME_CHECKOUT_SUCCESS) !== false
       && ((TRACKING_COUNT_ADMIN_ACTIVE == 'true' && $_SESSION['customers_status']['customers_status_id'] == '0')
           || $_SESSION['customers_status']['customers_status_id'] != '0'
           )
       )
-  {  
-    // include needed functions
-    require_once (DIR_FS_INC.'get_order_total.inc.php');
-  
-    $query = xtc_db_query("SELECT currency
+  {            
+    $beginCode = '<script>';
+    if (defined('MODULE_COOKIE_CONSENT_STATUS') && strtolower(MODULE_COOKIE_CONSENT_STATUS) == 'true' && (in_array(6, $_SESSION['tracking']['allowed']) || defined('COOKIE_CONSENT_NO_TRACKING'))) {
+      $beginCode = '<script async data-type="text/javascript" type="as-oil" data-purposes="6" data-managed="as-oil">';
+    }
+        
+    $beginCode .= "
+    !function (f, b, e, v, n, t, s) {
+      if (f.fbq) return; n = f.fbq = function () {
+        n.callMethod ?
+          n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+      }; if (!f._fbq) f._fbq = n;
+      n.push = n; n.loaded = !0; n.version = '2.0'; n.queue = []; t = b.createElement(e); t.async = !0;
+      t.src = v; s = b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t, s)
+    }(window,document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+
+    fbq('init', '".TRACKING_FACEBOOK_ID."');
+    fbq('track', 'PageView');
+    ";
+
+    $trackingCode = null;
+
+    $endCode = '
+  </script>
+  <noscript>
+    <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id='.TRACKING_FACEBOOK_ID.'&amp;ev=PageView&amp;noscript=1"/>
+    ';
+
+    if (strpos($PHP_SELF, FILENAME_CHECKOUT_SUCCESS) !== false
+        && !in_array('FB-'.$last_order, $_SESSION['tracking']['order'])
+        )
+    {
+      // include needed functions
+      require_once (DIR_FS_INC.'get_order_total.inc.php');
+      
+      $_SESSION['tracking']['order'][] = 'FB-'.$last_order;
+
+      $query = xtc_db_query("SELECT currency
                              FROM " . TABLE_ORDERS . "
                             WHERE orders_id = '" . $last_order . "'");
-    $orders = xtc_db_fetch_array($query);
-  
-    $id = TRACKING_FACEBOOK_ID;
-  
-    if (!in_array('FB-'.$last_order, $_SESSION['tracking']['order'])) {  
-      $_SESSION['tracking']['order'][] = 'FB-'.$last_order;
+      $orders = xtc_db_fetch_array($query);
       $total = get_order_total($last_order);
-      $beginCode = '<script>';
-      if (defined('MODULE_COOKIE_CONSENT_STATUS') && strtolower(MODULE_COOKIE_CONSENT_STATUS) == 'true' && (in_array(6, $_SESSION['tracking']['allowed']) || defined('COOKIE_CONSENT_NO_TRACKING'))) {
-        $beginCode = '<script async data-type="text/javascript" type="as-oil" data-purposes="6" data-managed="as-oil">';
-      }
-      $beginCode .= '
-      (function() {
-        var _fbq = window._fbq || (window._fbq = []);
-        if (!_fbq.loaded) {
-          var fbds = document.createElement(\'script\');
-          fbds.async = true;
-          fbds.src = \'//connect.facebook.net/en_US/fbds.js\';
-          var s = document.getElementsByTagName(\'script\')[0];
-          s.parentNode.insertBefore(fbds, s);
-          _fbq.loaded = true;
-        }
-      })();
+
+      $trackingCode = '
+      fbq(\'track\', \'Purchase\', {
+        currency: "'.$orders['currency'].'", 
+        value: '.round($total, 2).'
+      });
       ';
 
-      $endCode = 'window._fbq = window._fbq || [];
-      window._fbq.push([\'track\', \''.$id.'\', {\'value\':\''.$total.'\',\'currency\':\''.$orders['currency'].'\'}]);
-    </script>
+      $endCode .= '<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id='.TRACKING_FACEBOOK_ID.'&amp;ev=Purchase&amp;cd[value]='.round($total, 2).'&amp;cd[currency]='.$orders['currency'].'&amp;noscript=1"/>
       ';
     }
-  
-    echo $beginCode . $endCode;  
+    
+    $endCode .= '
+  </noscript>
+  ';
+    
+    echo $beginCode . $trackingCode . $endCode;  
   }
 ?>
