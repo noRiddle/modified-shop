5c389e6138053658c55b3ee8474f7f2779f22626
modGTB <gerhard@modified-shop.org>
2021-10-07 15:28:36 +0000

    partly revoke r13759 (optimize performance for url (props noRiddle)) - revoked accidentally committed code
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@13762 79ac0487-9261-4f26-a34d-858dee0dcce0

--- inc/xtc_get_products_stock.inc.php
+++ inc/xtc_get_products_stock.inc.php
@@ -16,39 +16,20 @@
    ---------------------------------------------------------------------------------------*/
    
   function xtc_get_products_stock($products_id) {
-    global $modified_cache;
     static $products_quantity_array;
-
-    if (!is_object($modified_cache)) {
-      require_once(DIR_FS_CATALOG.'includes/classes/modified_cache.php');
-      $_mod_cache_class = strtolower(DB_CACHE_TYPE).'_cache';
-      if (!class_exists($_mod_cache_class)) {
-        $_mod_cache_class = 'modified_cache';
-      }
-      $modified_cache = $_mod_cache_class::getInstance();
-    }
 
     if (!isset($products_quantity_array)) {
       $products_quantity_array = array();
     }
     
     if (!isset($products_quantity_array[$products_id])) {
-      $id = 'stock_'.$products_id;
-      $modified_cache->setID($id);
-    
-      if ($modified_cache->isHit() === true) {
-        $products_quantity_array[$products_id] = $modified_cache->get();
-      } else {
-        $products_quantity_array[$products_id] = 0;
-        $products_query = xtc_db_query("SELECT products_quantity
-                                          FROM ".TABLE_PRODUCTS." 
-                                         WHERE products_id = '".(int)$products_id."'");
-        if (xtc_db_num_rows($products_query) > 0) {
-          $products = xtc_db_fetch_array($products_query);
-          $products_quantity_array[$products_id] = $products['products_quantity'];
-        }
-        
-        $modified_cache->set($products_quantity_array[$products_id]);
+      $products_quantity_array[$products_id] = 0;
+      $products_query = xtc_db_query("SELECT products_quantity
+                                        FROM ".TABLE_PRODUCTS." 
+                                       WHERE products_id = '".(int)$products_id."'");
+      if (xtc_db_num_rows($products_query) > 0) {
+        $products = xtc_db_fetch_array($products_query);
+        $products_quantity_array[$products_id] = $products['products_quantity'];
       }
     }
     
