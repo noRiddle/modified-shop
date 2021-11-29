b4acafee04b85bb2240377bea5ae923ea7444636
Tomcraft1980 <hallo@modified-shop.org>
2021-11-29 09:38:06 +0000

    update dhl module version to 1.07 due to r13789, r13845 & r13846
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@13847 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/includes/modules/system/dhl.php
+++ admin/includes/modules/system/dhl.php
@@ -18,7 +18,7 @@
     function __construct() {
       global $order;
       
-      $this->version = '1.06';
+      $this->version = '1.07';
       $this->code = 'dhl';
       $this->title = MODULE_DHL_TEXT_TITLE;
       $this->description = MODULE_DHL_TEXT_DESCRIPTION.'<br><br><br><b>Version</b><br>'.$this->version;
