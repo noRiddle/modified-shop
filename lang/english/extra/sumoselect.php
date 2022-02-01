41a6606132fa55fb411027a4bdbd6edea6dbde95
modGTB <gerhard@modified-shop.org>
2022-02-01 11:24:34 +0000

    add language files for sumoselect search in country and state
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@14012 79ac0487-9261-4f26-a34d-858dee0dcce0

---
+++ lang/english/extra/sumoselect.php
@@ -0,0 +1,14 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id$
+
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
+
+   Copyright (c) 2009 - 2013 [www.modified-shop.org]
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
+
+ define('TEXT_SUMOSELECT_SEARCH', 'Search...');
+ define('TEXT_SUMOSELECT_NO_RESULT', 'no result for \"{0}\"');
---
+++ lang/german/extra/sumoselect.php
@@ -0,0 +1,14 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id$
+
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
+
+   Copyright (c) 2009 - 2013 [www.modified-shop.org]
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------------------------*/
+
+ define('TEXT_SUMOSELECT_SEARCH', 'Suchbegriff...');
+ define('TEXT_SUMOSELECT_NO_RESULT', 'Keine Treffer f&uuml;r \"{0}\"');
