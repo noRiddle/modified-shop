4ed6c5c2091c49f60e9cd2a6c69ba6aec381a427
Tomcraft1980 <hallo@modified-shop.org>
2022-02-07 10:24:00 +0000

    fix #1820 - update smarty to version 3.1.44 (reimplement custom modifications & fixes for modified-shop)
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@14023 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/external/smarty/smarty_3/Smarty.class.php
+++ includes/external/smarty/smarty_3/Smarty.class.php
@@ -28,6 +28,30 @@
  * @author    Rodney Rehm
  * @package   Smarty
  */
+
+/**
+ * define smarty plugindir in template
+ */
+define('MY_CURRENT_TEMPLATE', defined('CURRENT_TEMPLATE') ? CURRENT_TEMPLATE : 'tpl_modified');
+define('MY_TEMPLATE_PLUGINS', DIR_FS_CATALOG.'templates/'.MY_CURRENT_TEMPLATE.'/smarty/');
+define('MY_SHOP_PLUGINS', DIR_FS_EXTERNAL.'smarty/plugins/');
+
+/**
+ * define smarty charset
+ */
+if (isset($_SESSION['language_charset'])) {
+  $charset = $_SESSION['language_charset'];
+} else {
+  $charset = 'ISO-8859-1';
+  if (defined('DB_SERVER_CHARSET')
+      && DB_SERVER_CHARSET == 'utf8'
+      )
+  {
+    $charset = 'UTF-8';
+  }
+}
+define('SMARTY_RESOURCE_CHAR_SET', $charset);
+
 /**
  * set SMARTY_DIR to absolute path to Smarty library files.
  * Sets SMARTY_DIR only if user application has not already defined it.
@@ -91,6 +115,8 @@ require_once SMARTY_SYSPLUGINS_DIR . 'smarty_variable.php';
 require_once SMARTY_SYSPLUGINS_DIR . 'smarty_template_source.php';
 require_once SMARTY_SYSPLUGINS_DIR . 'smarty_template_resource_base.php';
 require_once SMARTY_SYSPLUGINS_DIR . 'smarty_internal_resource_file.php';
+include_once SMARTY_SYSPLUGINS_DIR . 'smarty_compatibility.php';
+include_once SMARTY_SYSPLUGINS_DIR . 'smarty_cacheresource_phpfastcache.php';
 
 /**
  * This is the main Smarty class
@@ -106,7 +132,7 @@ require_once SMARTY_SYSPLUGINS_DIR . 'smarty_internal_resource_file.php';
  * @method int compileAllConfig(string $extension = '.conf', bool $force_compile = false, int $time_limit = 0, $max_errors = null)
  * @method int clearCompiledTemplate($resource_name = null, $compile_id = null, $exp_time = null)
  */
-class Smarty extends Smarty_Internal_TemplateBase
+class Smarty extends Smarty_Compatibility
 {
     /**
      * smarty version
@@ -481,7 +507,7 @@ class Smarty extends Smarty_Internal_TemplateBase
      *
      * @var string
      */
-    public $caching_type = 'file';
+    public $caching_type = 'phpfastcache';
 
     /**
      * config type
@@ -676,7 +702,23 @@ class Smarty extends Smarty_Internal_TemplateBase
         if (is_callable('mb_internal_encoding')) {
             mb_internal_encoding(Smarty::$_CHARSET);
         }
+        if (is_callable('mb_regex_encoding')) {
+            mb_regex_encoding(Smarty::$_CHARSET);
+        }
         $this->start_time = microtime(true);
+
+        // set default dirs
+        $this->setTemplateDir(DIR_FS_CATALOG . 'templates' . DIRECTORY_SEPARATOR)
+             ->addTemplateDir(DIR_FS_CATALOG . 'templates' . DIRECTORY_SEPARATOR . MY_CURRENT_TEMPLATE . DIRECTORY_SEPARATOR)
+             ->setCompileDir(DIR_FS_CATALOG . 'templates_c' . DIRECTORY_SEPARATOR)
+             ->setPluginsDir(SMARTY_PLUGINS_DIR)
+             ->setCacheDir(DIR_FS_CATALOG . 'cache' . DIRECTORY_SEPARATOR)
+             ->setConfigDir(DIR_FS_CATALOG . 'lang' . DIRECTORY_SEPARATOR)
+             ->addConfigDir(DIR_FS_CATALOG . 'templates' . DIRECTORY_SEPARATOR . MY_CURRENT_TEMPLATE . DIRECTORY_SEPARATOR . 'lang' . DIRECTORY_SEPARATOR)
+             ->addPluginsDir(MY_TEMPLATE_PLUGINS)
+             ->addPluginsDir(MY_SHOP_PLUGINS)
+             ->registerCacheResource('phpfastcache', new Smarty_CacheResource_Phpfastcache());
+
         if (isset($_SERVER[ 'SCRIPT_NAME' ])) {
             Smarty::$global_tpl_vars[ 'SCRIPT_NAME' ] = new Smarty_Variable($_SERVER[ 'SCRIPT_NAME' ]);
         }
