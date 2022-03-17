7210653d2889bd5cfab4a16b862765ffc53edb27
modGTB <gerhard@modified-shop.org>
2022-03-17 08:13:03 +0000

    use db cache for redirect
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@14151 79ac0487-9261-4f26-a34d-858dee0dcce0

--- redirect.php
+++ redirect.php
@@ -24,12 +24,12 @@ require_once (DIR_FS_INC.'xtc_get_banners_url.inc.php');
 if (isset($_GET['action'])) {
   switch ($_GET['action']) {
     case 'banner' :
-      $banner_query = xtc_db_query("SELECT banners_url 
-                                      FROM ".TABLE_BANNERS." 
-                                     WHERE banners_id = '".(int) $_GET['goto']."'
-                                       AND trim(banners_url) != ''");
-      if (xtc_db_num_rows($banner_query)) {
-        $banner = xtc_db_fetch_array($banner_query);
+      $banner_query = xtDBquery("SELECT banners_url 
+                                   FROM ".TABLE_BANNERS." 
+                                  WHERE banners_id = '".(int) $_GET['goto']."'
+                                    AND trim(banners_url) != ''");
+      if (xtc_db_num_rows($banner_query, true)) {
+        $banner = xtc_db_fetch_array($banner_query, true);
         xtc_update_banner_click_count($_GET['goto']);
       
         xtc_redirect(xtc_get_banners_url($banner['banners_url']));
@@ -38,14 +38,14 @@ if (isset($_GET['action'])) {
 
     case 'product' :
       if (isset ($_GET['id'])) {
-        $product_query = xtc_db_query("SELECT products_url 
-                                         FROM ".TABLE_PRODUCTS_DESCRIPTION." 
-                                        WHERE products_id='".(int) $_GET['id']."'
-                                          AND trim(products_name) != ''           
-                                          AND trim(products_url) != ''           
-                                          AND language_id='".(int) $_SESSION['languages_id']."'");
-        if (xtc_db_num_rows($product_query)) {
-          $product = xtc_db_fetch_array($product_query);
+        $product_query = xtDBquery("SELECT products_url 
+                                      FROM ".TABLE_PRODUCTS_DESCRIPTION." 
+                                     WHERE products_id='".(int) $_GET['id']."'
+                                       AND trim(products_name) != ''           
+                                       AND trim(products_url) != ''           
+                                       AND language_id='".(int) $_SESSION['languages_id']."'");
+        if (xtc_db_num_rows($product_query, true)) {
+          $product = xtc_db_fetch_array($product_query, true);
 
           xtc_redirect(check_url_scheme($product['products_url']));
         }
@@ -54,23 +54,23 @@ if (isset($_GET['action'])) {
 
     case 'manufacturer' :
       if (isset ($_GET['manufacturers_id'])) {
-        $manufacturer_query = xtc_db_query("SELECT manufacturers_url 
-                                              FROM ".TABLE_MANUFACTURERS_INFO." 
-                                             WHERE manufacturers_id = '".(int) $_GET['manufacturers_id']."' 
-                                               AND languages_id = '".(int) $_SESSION['languages_id']."'
-                                               AND trim(manufacturers_url) != ''");
-        if (xtc_db_num_rows($manufacturer_query) < 1) {
+        $manufacturer_query = xtDBquery("SELECT manufacturers_url 
+                                           FROM ".TABLE_MANUFACTURERS_INFO." 
+                                          WHERE manufacturers_id = '".(int) $_GET['manufacturers_id']."' 
+                                            AND languages_id = '".(int) $_SESSION['languages_id']."'
+                                            AND trim(manufacturers_url) != ''");
+        if (xtc_db_num_rows($manufacturer_query, true) < 1) {
           // no url exists for the selected language, lets use the default language then
-          $manufacturer_query = xtc_db_query("SELECT mi.languages_id, 
-                                                     mi.manufacturers_url 
-                                                FROM ".TABLE_MANUFACTURERS_INFO." mi
-                                                JOIN ".TABLE_LANGUAGES." l 
-                                                     ON mi.languages_id = l.languages_id
-                                                        AND l.code = '".DEFAULT_LANGUAGE."'
-                                               WHERE mi.manufacturers_id = '".(int) $_GET['manufacturers_id']."'
-                                                 AND trim(mi.manufacturers_url) != ''");
-          if (xtc_db_num_rows($manufacturer_query) > 0) {
-            $manufacturer = xtc_db_fetch_array($manufacturer_query);
+          $manufacturer_query = xtDBquery("SELECT mi.languages_id, 
+                                                  mi.manufacturers_url 
+                                             FROM ".TABLE_MANUFACTURERS_INFO." mi
+                                             JOIN ".TABLE_LANGUAGES." l 
+                                                  ON mi.languages_id = l.languages_id
+                                                     AND l.code = '".DEFAULT_LANGUAGE."'
+                                            WHERE mi.manufacturers_id = '".(int) $_GET['manufacturers_id']."'
+                                              AND trim(mi.manufacturers_url) != ''");
+          if (xtc_db_num_rows($manufacturer_query, true) > 0) {
+            $manufacturer = xtc_db_fetch_array($manufacturer_query, true);
             xtc_db_query("UPDATE ".TABLE_MANUFACTURERS_INFO." 
                              SET url_clicked = url_clicked+1, 
                                  date_last_click = now() 
@@ -97,4 +97,3 @@ if (isset($_GET['action'])) {
 
 // default
 xtc_redirect(xtc_href_link(FILENAME_DEFAULT));
-?>
