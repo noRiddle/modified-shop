f95b7003ef5fbc8720ca52b9f2d36c93dedaa22e
Tomcraft1980 <hallo@modified-shop.org>
2022-03-23 16:20:52 +0000

    revised r14173 (update payone - reduce sql) - format code
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@14186 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/external/payone/classes/PayonePayment.php
+++ includes/external/payone/classes/PayonePayment.php
@@ -395,11 +395,11 @@ class PayonePayment {
 
 	function check() {
 		if (!isset ($this->_check)) {
-      if (defined('MODULE_PAYMENT_'.strtoupper($this->code).'_STATUS')) {
-        $this->_check = true;
-      } else {
-        $check_query = xtc_db_query("select configuration_value from ".TABLE_CONFIGURATION." where configuration_key = 'MODULE_PAYMENT_".  strtoupper($this->code) ."_STATUS'");
-        $this->_check = xtc_db_num_rows($check_query);
+			if (defined('MODULE_PAYMENT_'.strtoupper($this->code).'_STATUS')) {
+				$this->_check = true;
+			} else {
+				$check_query = xtc_db_query("select configuration_value from ".TABLE_CONFIGURATION." where configuration_key = 'MODULE_PAYMENT_".	 strtoupper($this->code) ."_STATUS'");
+				$this->_check = xtc_db_num_rows($check_query);
 			}
 		}
 		return $this->_check;
