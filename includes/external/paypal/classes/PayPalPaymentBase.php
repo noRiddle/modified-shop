983da1b3fbcc27db2ba8f90bdda4123035a6cfa4
modGTB <gerhard@modified-shop.org>
2022-03-23 09:24:50 +0000

    update paypal - reduce sql
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@14170 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/external/paypal/classes/PayPalPaymentBase.php
+++ includes/external/paypal/classes/PayPalPaymentBase.php
@@ -280,10 +280,14 @@ class PayPalPaymentBase extends PayPalCommon {
 
   function check() {
     if (!isset($this->_check)) {
-      $check_query = xtc_db_query("SELECT configuration_value 
-                                     FROM ".TABLE_CONFIGURATION." 
-                                    WHERE configuration_key = 'MODULE_PAYMENT_".strtoupper($this->code)."_STATUS'");
-      $this->_check = xtc_db_num_rows($check_query);
+      if (defined('MODULE_PAYMENT_'.strtoupper($this->code).'_STATUS')) {
+        $this->_check = true;
+      } else {
+        $check_query = xtc_db_query("SELECT configuration_value 
+                                       FROM ".TABLE_CONFIGURATION." 
+                                      WHERE configuration_key = 'MODULE_PAYMENT_".strtoupper($this->code)."_STATUS'");
+        $this->_check = xtc_db_num_rows($check_query);
+      }
     }
     return $this->_check;
   }
