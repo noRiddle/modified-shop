79510c8950a3b7bf797b2216dd7d8b835a3b87cb
Tomcraft1980 <hallo@modified-shop.org>
2022-05-03 15:54:19 +0000

    revoked r14409 (update paypal - raise module version from 1.78 to 1.79 due to r14373, r14374, r14375, r14376, r14377 & r14379)
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@14412 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/external/paypal/classes/PayPalPaymentBase.php
+++ includes/external/paypal/classes/PayPalPaymentBase.php
@@ -30,7 +30,7 @@ class PayPalPaymentBase extends PayPalCommon {
     global $order;
 
     $this->code = $class;
-    $this->paypal_version = '1.79';
+    $this->paypal_version = '1.78';
 
     $this->admin_access_array = array(
       'paypal_info',
