cb2c1faffe963900c62cbc622f513ff5acd066bb
modGTB <gerhard@modified-shop.org>
2022-06-14 07:53:47 +0000

    fix empty payment
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@14527 79ac0487-9261-4f26-a34d-858dee0dcce0

--- _installer/update/update_2.0.7.0_to_2.0.7.1.sql
+++ _installer/update/update_2.0.7.0_to_2.0.7.1.sql
@@ -1,5 +1,5 @@
 # -----------------------------------------------------------------------------------------
-#  $Id: update_2.0.7.0_to_2.0.7.1.sql 14470 2022-05-17 10:47:29Z Tomcraft $
+#  $Id$
 #
 #  modified eCommerce Shopsoftware
 #  http://www.modified-shop.org
@@ -9,5 +9,9 @@
 
 #GTB - 2022-06-14 - changed database_version
 INSERT INTO `database_version` (`version`) VALUES ('MOD_2.0.7.1');
+
+#GTB - 2022-06-14 - fix empty payment
+UPDATE `orders` SET `payment_class` = 'no_payment' WHERE `payment_class` = '';
+UPDATE `orders` SET `payment_method` = 'no_payment' WHERE `payment_method` = '';
 
 # Keep an empty line at the end of this file for the db_updater to work properly
