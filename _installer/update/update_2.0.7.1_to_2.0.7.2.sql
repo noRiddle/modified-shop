29e63162447a3f4daa42a451fa1dd0844aae5ed4
Tomcraft1980 <hallo@modified-shop.org>
2022-07-04 18:18:34 +0000

    Add update_2.0.7.1_to_2.0.7.2.sql
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@14620 79ac0487-9261-4f26-a34d-858dee0dcce0

---
+++ _installer/update/update_2.0.7.1_to_2.0.7.2.sql
@@ -0,0 +1,13 @@
+# -----------------------------------------------------------------------------------------
+#  $Id$
+#
+#  modified eCommerce Shopsoftware
+#  http://www.modified-shop.org
+#
+#  Copyright (c) 2009 - 2013 [www.modified-shop.org]
+#  -----------------------------------------------------------------------------------------
+
+#Tomcraft - 2022-07-04 - changed database_version
+INSERT INTO `database_version` (`version`) VALUES ('MOD_2.0.7.2');
+
+# Keep an empty line at the end of this file for the db_updater to work properly
