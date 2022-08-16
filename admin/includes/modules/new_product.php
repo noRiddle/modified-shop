69fec3b03e6fb6259904c712f37acaddc6acdf5b
modGTB <gerhard@modified-shop.org>
2022-08-16 13:18:16 +0000

    revised r14684 (fix #2297 - set cPath for products)
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@14691 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/includes/modules/new_product.php
+++ admin/includes/modules/new_product.php
@@ -111,7 +111,7 @@
   $form_action = isset($_GET['pID']) ? 'update_product' : 'insert_product';
   $form_action .= ((isset($_GET['origin']) && $_GET['origin'] != '') ? '&origin='.$_GET['origin'] : '');
   
-  echo xtc_draw_form('new_product', FILENAME_CATEGORIES, xtc_get_all_get_params(array('action', 'cPath', 'origin')).'cpath='.$cPath.'&action='.$form_action, 'post', 'id="new_product" enctype="multipart/form-data"' . $confirm_submit); 
+  echo xtc_draw_form('new_product', FILENAME_CATEGORIES, xtc_get_all_get_params(array('action', 'cPath', 'origin')).'cPath='.$cPath.'&action='.$form_action, 'post', 'id="new_product" enctype="multipart/form-data"' . $confirm_submit); 
   echo xtc_draw_hidden_field('products_quantity_before_edit', isset($prod_quantity['products_quantity']) ? $prod_quantity['products_quantity'] : 0);
   echo '<input type="submit" style="display:none;" value="'.BUTTON_SAVE.'"/>';
   ?>
