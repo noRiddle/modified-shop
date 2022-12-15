e86624dc723229e2dab36ac004efdd4005081bce
modGTB <gerhard@modified-shop.org>
2022-12-15 14:35:34 +0000

    fix #1673 & #2317 - show payment informations in print order
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@14826 79ac0487-9261-4f26-a34d-858dee0dcce0

--- print_order.php
+++ print_order.php
@@ -62,12 +62,16 @@ if ($customer_id > 0
   $smarty->assign('DATE', xtc_date_long($order->info['date_purchased']));
   $smarty->assign('SHIPPING_CLASS', $order->info['shipping_class']);
 
-  $last_order = $oID;
-  // load the selected payment module
-  require_once (DIR_WS_CLASSES . 'payment.php');
-  $payment_modules = new payment($order->info['payment_class']);
-  $smarty->assign('PAYMENT_INFO', $payment_modules->success());
-  $smarty->assign('PAYMENT_METHOD', $payment_modules::payment_title($order->info['payment_method'],$oID));
+  // Payment Method
+  if ($order->info['payment_method'] != '' && $order->info['payment_method'] != 'no_payment') {  
+    $_SESSION['billing_zone'] = $order->billing['country_iso_2'];
+    $last_order = $order->info['order_id'];
+    require_once (DIR_WS_CLASSES . 'payment.php');
+    $payment_modules = new payment($order->info['payment_class']);
+    $smarty->assign('PAYMENT_INFO', $payment_modules->success());
+    $smarty->assign('PAYMENT_METHOD', $payment_modules::payment_title($order->info['payment_method'], $order->info['order_id']));
+    unset($_SESSION['billing_zone']);
+  }
 
   // dont allow cache
   $smarty->caching =0;
