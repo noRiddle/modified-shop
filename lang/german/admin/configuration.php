239bb0fb94c412bbc1755e2f4c0588f3d24f9e63
Tomcraft1980 <hallo@modified-shop.org>
2022-12-21 11:28:01 +0000

    revised r14866 (fix #2333 - update autocomplete search) - readded accidentally deleted language definition
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@14870 79ac0487-9261-4f26-a34d-858dee0dcce0

--- lang/german/admin/configuration.php
+++ lang/german/admin/configuration.php
@@ -829,6 +829,8 @@ define('MAX_DISPLAY_PRODUCTS_CATEGORY_TITLE', 'Maximale Artikel');
 define('MAX_DISPLAY_PRODUCTS_CATEGORY_DESC', 'Maximale Anzahl an Artikeln aus der gleichen Kategorie.');
 define('MAX_DISPLAY_ADVANCED_SEARCH_RESULTS_TITLE', 'Anzahl Suchergebnisse');
 define('MAX_DISPLAY_ADVANCED_SEARCH_RESULTS_DESC', 'Maximum Anzahl der Artikel die pro Seite in den Suchergebnissen (advanced_search_result.php) angezeigt werden sollen.');
+define('MAX_DISPLAY_PRODUCTS_HISTORY_TITLE' , 'Anzahl der History');
+define('MAX_DISPLAY_PRODUCTS_HISTORY_DESC' , 'Maximum Anzahl an Artikeln, die zuletzt besucht wurden, im Account anzeigen.');
 define('MAX_DISPLAY_ADVANCED_SEARCH_RESULTS_TITLE', 'Anzahl Suchergebnisse');
 define('MAX_DISPLAY_ADVANCED_SEARCH_RESULTS_DESC', 'Maximum Anzahl der Artikel die pro Seite in den Suchergebnissen (advanced_search_result.php) angezeigt werden sollen.');
 define('MAX_DISPLAY_SEARCH_AC_RESULTS_TITLE' , 'Anzahl Autocomplete Suchergebnisse');
