079532769e57fdd1a12f5771e50e1a05cfa7ad90
modGTB <gerhard@modified-shop.org>
2023-01-25 16:34:14 +0000

    fix #2256 - update shipcloud - show menu only if access granted
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@14900 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/external/shipcloud/column_left.php
+++ includes/external/shipcloud/column_left.php
@@ -12,14 +12,23 @@
 
   defined( '_VALID_XTC' ) or die( 'Direct Access to this location is not allowed.' );
   
+  if (!isset($menu_access)) {
+    $menu_access = array();
+    $menu_aoutput = true;
+  }
+    
   if ((isset($admin_access['shipcloud_pickup']) && $admin_access['shipcloud_pickup'] == '1')
       && (isset($admin_access['shipcloud']) && $admin_access['shipcloud'] == '1')
       )
   {      
-    echo '<li><a href="' . xtc_href_link(FILENAME_SHIPCLOUD) . '" class="menuBoxContentLinkSub"> -' . BOX_SHIPCLOUD . '</a><ul>';
-    echo '<li><a href="' . xtc_href_link('shipcloud_pickup.php', '') . '" class="menuBoxContentLink"> -' . BOX_SHIPCLOUD_PICKUP . '</a></li>';
-    echo '  </ul></li>';
+    $menu_access[] = '<li><a href="' . xtc_href_link(FILENAME_SHIPCLOUD) . '" class="menuBoxContentLinkSub"> -' . BOX_SHIPCLOUD . '</a><ul>';
+    $menu_access[] = '  <li><a href="' . xtc_href_link('shipcloud_pickup.php', '') . '" class="menuBoxContentLink"> -' . BOX_SHIPCLOUD_PICKUP . '</a></li>';
+    $menu_access[] = '</ul></li>';
   } elseif (isset($admin_access['shipcloud']) && $admin_access['shipcloud'] == '1') {
-    echo '<li><a href="' . xtc_href_link(FILENAME_SHIPCLOUD, '') . '" class="menuBoxContentLink"> -' . BOX_SHIPCLOUD . '</a></li>';
+    $menu_access[] = '<li><a href="' . xtc_href_link(FILENAME_SHIPCLOUD, '') . '" class="menuBoxContentLink"> -' . BOX_SHIPCLOUD . '</a></li>';
   }
-?>
+  
+  if ($menu_aoutput === true) {
+    echo implode(PHP_EOL, $menu_access);
+    unset($menu_access);
+  }
