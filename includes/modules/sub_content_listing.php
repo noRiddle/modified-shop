973c199008133864ac3464bb5a574b5a80a239b6
modGTB <gerhard@modified-shop.org>
2023-01-30 15:32:55 +0000

    fix #2427 - breadcrumb and box view for sub content
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@14935 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/modules/sub_content_listing.php
+++ includes/modules/sub_content_listing.php
@@ -36,20 +36,20 @@ if (!$module_smarty->is_cached(CURRENT_TEMPLATE.'/module/sub_content_listing.htm
     $shop_content_sub_query_1 = xtDBquery("SELECT c2.content_title,
                                                   c2.content_group,
                                                   c2.content_text,
+                                                  c2.content_status,
                                                   c1.content_title as title, 
                                                   c1.content_group as group_id,
+                                                  c1.content_status as parent_status,
                                                   c1.parent_id
                                              FROM ".TABLE_CONTENT_MANAGER." c1
                                              JOIN ".TABLE_CONTENT_MANAGER." c2
                                                   ON c2.content_id = c1.parent_id
                                                      AND c2.file_flag = c1.file_flag
-                                                     AND c2.content_status = '1'
                                                      AND c2.content_active = '1'
                                                      AND c2.languages_id = '".(int)$_SESSION['languages_id']."'
                                                      ".CONTENT_CONDITIONS_C2."
                                             WHERE c1.parent_id = '".$shop_content_data['content_id']."'
                                               AND c1.content_status = '1'
-                                              AND c1.content_active = '1'
                                               AND c1.languages_id = '".(int)$_SESSION['languages_id']."'
                                                   ".CONTENT_CONDITIONS_C1."
                                          ORDER BY c1.sort_order");
@@ -65,19 +65,19 @@ if (!$module_smarty->is_cached(CURRENT_TEMPLATE.'/module/sub_content_listing.htm
                                                   c3.content_text,
                                                   c2.content_title as title, 
                                                   c2.content_group as group_id,
-                                                  c2.parent_id
+                                                  c2.content_status,
+                                                  c2.parent_id,
+                                                  c3.content_status as parent_status
                                              FROM ".TABLE_CONTENT_MANAGER." c1
                                              JOIN ".TABLE_CONTENT_MANAGER." c2
                                                   ON c1.parent_id = c2.parent_id
                                                      AND c2.file_flag = c1.file_flag
-                                                     AND c2.content_status = '1'
                                                      AND c2.content_active = '1'
                                                      AND c2.languages_id = '".(int)$_SESSION['languages_id']."'
                                                      ".CONTENT_CONDITIONS_C2."
                                              JOIN ".TABLE_CONTENT_MANAGER." c3
                                                   ON c3.content_id = c2.parent_id
                                                      AND c3.file_flag = c2.file_flag
-                                                     AND c3.content_status = '1'
                                                      AND c3.content_active = '1'
                                                      AND c3.languages_id = '".(int)$_SESSION['languages_id']."'
                                                      ".CONTENT_CONDITIONS_C3."
@@ -96,21 +96,26 @@ if (!$module_smarty->is_cached(CURRENT_TEMPLATE.'/module/sub_content_listing.htm
     $sub_content = array();
     $parent_content = array();
     while ($shop_sub_content = xtc_db_fetch_array($shop_content_sub_query, true)) {
-      $sub_content[] = array(
-        'CONTENT_TITLE' => $shop_sub_content['title'],
-        'CONTENT_GROUP' => $shop_sub_content['group_id'],
-        'CONTENT_LINK' => xtc_href_link(FILENAME_CONTENT, xtc_content_link($shop_sub_content['group_id'], $shop_sub_content['title']), 'NONSSL')
-      );
+      if ($shop_sub_content['content_status'] == 1) {
+        $sub_content[] = array(
+          'CONTENT_TITLE' => $shop_sub_content['title'],
+          'CONTENT_GROUP' => $shop_sub_content['group_id'],
+          'CONTENT_LINK' => xtc_href_link(FILENAME_CONTENT, xtc_content_link($shop_sub_content['group_id'], $shop_sub_content['title']), 'NONSSL')
+        );
+      }
       if (count($parent_content) == 0) {
         if ($add_breadcrumb === true) {
+          $add_breadcrumb = false;
           $breadcrumb->add($shop_sub_content['content_title'], xtc_href_link(FILENAME_CONTENT, xtc_content_link($shop_sub_content['content_group'], $shop_sub_content['content_title'])));
         }
-        $parent_content = array(
-          'CONTENT_TITLE' => $shop_sub_content['content_title'],
-          'CONTENT_TEXT' => $shop_sub_content['content_text'],
-          'CONTENT_GROUP' => $shop_sub_content['content_group'],
-          'CONTENT_LINK' => xtc_href_link(FILENAME_CONTENT, xtc_content_link($shop_sub_content['content_group'], $shop_sub_content['content_title']), 'NONSSL')
-        );
+        if ($shop_sub_content['parent_status'] == 1) {
+          $parent_content = array(
+            'CONTENT_TITLE' => $shop_sub_content['content_title'],
+            'CONTENT_TEXT' => $shop_sub_content['content_text'],
+            'CONTENT_GROUP' => $shop_sub_content['content_group'],
+            'CONTENT_LINK' => xtc_href_link(FILENAME_CONTENT, xtc_content_link($shop_sub_content['content_group'], $shop_sub_content['content_title']), 'NONSSL')
+          );
+        }
       }
     }
 
