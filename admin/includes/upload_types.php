2e7701a6dbca22b0eb209cc96d1535507f2c2430
modGTB <gerhard@modified-shop.org>
2023-04-19 05:53:38 +0000

    fix #2184 - add svg as image upload type
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@15094 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/includes/upload_types.php
+++ admin/includes/upload_types.php
@@ -8,8 +8,8 @@
    Copyright (c) 2009 - 2013 [www.modified-shop.org] 
    --------------------------------------------------------------*/
 
-  $accepted_image_extensions = array("jpg","jpeg","jpe","gif","png","bmp","tiff","tif","bmp");
-  $accepted_image_mime_types = array("image/jpeg","image/gif","image/png","image/bmp");
+  $accepted_image_extensions = array("jpg","jpeg","jpe","gif","png","tiff","tif","bmp","svg");
+  $accepted_image_mime_types = array("image/jpeg","image/gif","image/png","image/bmp","image/svg","image/svg+xml");
 
   $accepted_file_extensions = array("txt","csv","tsv");
   $accepted_file_mime_types = array("text/plain","text/csv","text/comma-separated-values","text/tab-separated-values");
