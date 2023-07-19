a9018e9dd35b9fbc0eb1d2ca288b97142621922c
Tomcraft1980 <hallo@modified-shop.org>
2023-07-19 11:23:55 +0000

    update shopvote to version 1.19 due to r15355
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@15358 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/includes/modules/system/shopvote.php
+++ admin/includes/modules/system/shopvote.php
@@ -18,7 +18,7 @@ class shopvote
 
     function __construct() 
     {
-        $this->version = '1.18';
+        $this->version = '1.19';
         $this->code = 'shopvote';
         $this->title = MODULE_SHOPVOTE_TEXT_TITLE;
         $this->description = MODULE_SHOPVOTE_TEXT_DESCRIPTION;
