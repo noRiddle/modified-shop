4ce39b0a70afe0a3063588a9b54406a08e17df35
Tomcraft1980 <hallo@modified-shop.org>
2023-11-20 21:02:06 +0000

    update internetmarke to version 1.20 due to r15577
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@15583 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/includes/modules/system/internetmarke.php
+++ admin/includes/modules/system/internetmarke.php
@@ -23,7 +23,7 @@
 
       function __construct() 
       {          
-          $this->version = '1.19';
+          $this->version = '1.20';
           $this->code = 'internetmarke';
           $this->title = MODULE_INTERNETMARKE_TEXT_TITLE;
           $this->description = MODULE_INTERNETMARKE_TEXT_DESCRIPTION;
