2b8aced6cec8c5559df87d768a2255e69c23f505
Tomcraft1980 <hallo@modified-shop.org>
2024-01-16 10:49:22 +0000

    update paypal - raise module version from 1.89 to 1.90 due to r15631, r15635 & r15655
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@15693 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/external/paypal/classes/PayPalPaymentBase.php
+++ includes/external/paypal/classes/PayPalPaymentBase.php
@@ -30,7 +30,7 @@ class PayPalPaymentBase extends PayPalCommon {
     global $order;
 
     $this->code = $class;
-    $this->paypal_version = '1.89';
+    $this->paypal_version = '1.90';
 
     $this->admin_access_array = array(
       'paypal_info',
