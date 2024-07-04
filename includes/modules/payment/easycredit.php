2f092df61819d26a7f18ad2b1b0d80d3f61b09e9
Tomcraft1980 <hallo@modified-shop.org>
2024-07-04 08:24:26 +0000

    update easycredit to version 1.26 due to r16023
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@16027 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/modules/payment/easycredit.php
+++ includes/modules/payment/easycredit.php
@@ -71,7 +71,7 @@ class easycredit {
   function __construct() {
     global $order, $main;
 
-    $this->version = '1.25';
+    $this->version = '1.26';
     $this->code = 'easycredit';
     $this->title = MODULE_PAYMENT_EASYCREDIT_TEXT_TITLE;
     $this->description = MODULE_PAYMENT_EASYCREDIT_TEXT_DESCRIPTION;
