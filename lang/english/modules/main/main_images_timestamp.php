41e6e32aa428276a720cd5ec8c8fffb9fc22ad3d
modGTB <gerhard@modified-shop.org>
2024-07-10 10:16:50 +0000

    class extension for categories and manufacturers images timestamp
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@16043 79ac0487-9261-4f26-a34d-858dee0dcce0

---
+++ lang/english/modules/main/main_images_timestamp.php
@@ -0,0 +1,14 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id$
+
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
+
+   Copyright (c) 2009 - 2013 [www.modified-shop.org]
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+  define('MODULE_MAIN_MAIN_IMAGES_TIMESTAMP_TITLE', 'Timestamp for product images');
+  define('MODULE_MAIN_MAIN_IMAGES_TIMESTAMP_DESCRIPTION', 'Additional parameter for categoies images and manufacturers images');
---
+++ lang/german/modules/main/main_images_timestamp.php
@@ -0,0 +1,14 @@
+<?php
+/* -----------------------------------------------------------------------------------------
+   $Id$
+
+   modified eCommerce Shopsoftware
+   http://www.modified-shop.org
+
+   Copyright (c) 2009 - 2013 [www.modified-shop.org]
+   -----------------------------------------------------------------------------------------
+   Released under the GNU General Public License 
+   ---------------------------------------------------------------------------------------*/
+
+  define('MODULE_MAIN_MAIN_IMAGES_TIMESTAMP_TITLE', 'Timestamp für Kategoriebilder und Herstellerbilder');
+  define('MODULE_MAIN_MAIN_IMAGES_TIMESTAMP_DESCRIPTION', 'Zusätzlicher Timestamp Paremeter für Kategoriebilder und Herstellerbilder');
