d04116643ef67d35dfa3fde6b8a2463145d46f94
modGTB <gerhard@modified-shop.org>
2024-07-17 10:15:06 +0000

    update installer to smarty 4
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@16071 79ac0487-9261-4f26-a34d-858dee0dcce0

--- _installer/includes/config.php
+++ _installer/includes/config.php
@@ -15,7 +15,7 @@
   
   // config
   define('EMAIL_SQL_ERRORS', 'false');
-  define('TEMPLATE_ENGINE','smarty_3');
+  define('TEMPLATE_ENGINE','smarty_4');
   define('SEARCH_ENGINE_FRIENDLY_URLS', 'false');
   define('SESSION_FORCE_COOKIE_USE', 'True');
   define('DEFAULT_TEMPLATE', 'tpl_modified_responsive');
