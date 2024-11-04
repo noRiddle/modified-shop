0a80ea7c31920888e669769f0f37aecde69b84bc
modGTB <gerhard@modified-shop.org>
2024-11-04 10:55:19 +0000

    fix #2877 - update autocomplete search
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@16205 79ac0487-9261-4f26-a34d-858dee0dcce0

--- templates/tpl_modified_nova/javascript/extra/autocomplete.js.php
+++ templates/tpl_modified_nova/javascript/extra/autocomplete.js.php
@@ -11,36 +11,36 @@
    --------------------------------------------------------------*/
 ?>
 <script>
+	<?php if (SEARCH_AC_STATUS == 'true' || (basename($PHP_SELF) != FILENAME_SHOPPING_CART && !strpos($PHP_SELF, 'checkout'))) { ?>	
+	function ac_closing() {
+		setTimeout("$('#suggestions').slideUp();", 100);
+	}
+  <?php } ?>
   <?php if (SEARCH_AC_STATUS == 'true') { ?>
   var session_id = '<?php echo xtc_session_id(); ?>';
-  $('body').on('keydown paste cut input focus', '#inputString', delay(function() {
-    if ($(this).length == 0) {
-      $('#suggestions').hide();
-    } else {
-      var post_params = $('#quick_find').serialize();
-      
-      $.ajax({
-        dataType: "json",
-        type: 'post',
-        url: '<?php echo DIR_WS_BASE; ?>ajax.php?ext=get_autocomplete&MODsid='+session_id,
-        data: post_params,
-        cache: false,
-        async: true,
-        success: function(data) {
-          if (data !== null && typeof data === 'object') {
-            if (data.result !== null && data.result != undefined && data.result != '') {
-              $('#autoSuggestionsList').html(decode_ajax(data.result));
-              $('#suggestions').slideDown();
-            } else {
-              $('#suggestions').slideUp();
-            }
+  
+  function ac_ajax_call(post_params) {
+    $.ajax({
+      dataType: "json",
+      type: 'post',
+      url: '<?php echo DIR_WS_BASE; ?>ajax.php?ext=get_autocomplete&MODsid='+session_id,
+      data: post_params,
+      cache: false,
+      async: true,
+      success: function(data) {
+        if (data !== null && typeof data === 'object') {
+          if (data.result !== null && data.result != undefined && data.result != '') {
+            $('#autoSuggestionsList').html(ac_decode(data.result));
+            $('#suggestions').slideDown();
+          } else {
+            $('#suggestions').slideUp();
           }
         }
-      });    
-    }
-  }, 500));
+      }
+    });    
+  }
   
-  function delay(fn, ms) {
+  function ac_delay(fn, ms) {
     let timer = 0;
     return function(args) {
       clearTimeout(timer);
@@ -48,22 +48,36 @@
     }
   }
 
-  function decode_ajax(encodedString) {
+  function ac_decode(encodedString) {
     var textArea = document.createElement('textarea');
     textArea.innerHTML = encodedString;
   
     return textArea.value;
   }
 
-  $('body').on('click', function (e) {
-    if ($(e.target).closest("#suggestions").length === 0) {
+  $('body').on('keydown paste cut input focus', '#inputString', ac_delay(function() {
+    if ($(this).length == 0) {
+      $('#suggestions').hide();
+    } else {
+      let post_params = $('#quick_find').serialize();
+      ac_ajax_call(post_params);
+    }
+  }, 500));
+
+  $('body').on('click', function (e) {    
+    if ($(e.target).closest("#suggestions").length === 0
+        && $(e.target).closest("#quick_find").length === 0
+        )
+    {
       ac_closing();
     }
   });
+
+  <?php if(defined('SEARCH_AC_CATEGORIES') && SEARCH_AC_CATEGORIES == 'true') { ?>
+  $('body').on('change', '#cat_search', ac_delay(function() {
+    let post_params = $('#quick_find').serialize();
+    ac_ajax_call(post_params);
+  }, 500));
   <?php } ?>
-	<?php if (SEARCH_AC_STATUS == 'true' || (basename($PHP_SELF) != FILENAME_SHOPPING_CART && !strpos($PHP_SELF, 'checkout'))) { ?>	
-	function ac_closing() {
-		setTimeout("$('#suggestions').slideUp();", 100);
-	}
   <?php } ?>
 </script>  
