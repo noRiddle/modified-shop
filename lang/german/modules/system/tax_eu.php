98139121ca9b773cf9ffeb874795a92f02110434
Tomcraft1980 <hallo@modified-shop.org>
2025-02-05 20:36:27 +0000

    revised r16308 (update tax eu to inform if the api could not be reached) - fix typo
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@16310 79ac0487-9261-4f26-a34d-858dee0dcce0

--- lang/german/modules/system/tax_eu.php
+++ lang/german/modules/system/tax_eu.php
@@ -13,4 +13,4 @@
 define('MODULE_TAX_EU_TEXT_TITLE', 'Steuers&auml;tze f&uuml;r One-Stop-Shop (OSS) der EU');
 define('MODULE_TAX_EU_TEXT_DESCRIPTION', 'Steuers&auml;tze und Steuerzonen f&uuml;r One-Stop-Shop (OSS) der EU installieren');
 define('MODULE_TAX_EU_TEXT_DESCRIPTION_PROCESSED', 'Es wurden bereits alle Steuers&auml;tze der EU Mitgliedsstaaten eingef&uuml;gt.<br/><br/>');
-define('MODULE_TAX_EU_ERROR_API', 'Steuers&auml;tze konnten nicht importiert werden. Bitte versuchen sie es zu einem sp&auml;teren Zeitpunkt noch einmal.');
+define('MODULE_TAX_EU_ERROR_API', 'Steuers&auml;tze konnten nicht importiert werden. Bitte versuchen Sie es zu einem sp&auml;teren Zeitpunkt noch einmal.');
