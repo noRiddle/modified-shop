97e32b295a0306faee555b785b1a3d0cf33d2a1b
modGTB <gerhard@modified-shop.org>
2025-04-01 15:40:23 +0000

    Add update_3.1.3_to_3.1.4.sql
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@16387 79ac0487-9261-4f26-a34d-858dee0dcce0

---
+++ _installer/update/update_3.1.3_to_3.1.4.sql
@@ -0,0 +1,13 @@
+# -----------------------------------------------------------------------------------------
+#  $Id$
+#
+#  modified eCommerce Shopsoftware
+#  http://www.modified-shop.org
+#
+#  Copyright (c) 2009 - 2013 [www.modified-shop.org]
+#  -----------------------------------------------------------------------------------------
+
+#GTB - 2024-12-03 - changed database_version
+INSERT INTO `database_version` (`version`, `date_added`) VALUES ('MOD_3.1.4', NOW());
+
+# Keep an empty line at the end of this file for the db_updater to work properly
