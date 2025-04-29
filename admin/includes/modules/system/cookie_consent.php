0e57baeeadb37587bd79b6cd781ecb0d3626ba27
Tomcraft1980 <hallo@modified-shop.org>
2025-04-29 10:12:20 +0000

    update OIL.js Cookie Consent to version 1.49 due to r16420
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@16421 79ac0487-9261-4f26-a34d-858dee0dcce0

--- admin/includes/modules/system/cookie_consent.php
+++ admin/includes/modules/system/cookie_consent.php
@@ -26,7 +26,7 @@ if (!class_exists('cookie_consent')) {
     var $_check;
 
     function __construct() {
-      $this->version = '1.48';
+      $this->version = '1.49';
       $this->code = 'cookie_consent';
       $this->title = 'Cookie Consent';
       $this->description = 'Cookie Consent-Modul based on oil.js';
