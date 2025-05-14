f8bec1e6f34ebfc8bf580c6eb46240534787795e
Tomcraft1980 <hallo@modified-shop.org>
2025-05-14 10:57:11 +0000

    update paypal - raise module version from 1.102 to 1.103 due to r16449
    git-svn-id: https://svn.modified-shop.org/svn/modified-shop/trunk@16451 79ac0487-9261-4f26-a34d-858dee0dcce0

--- includes/external/paypal/classes/PayPalPaymentBase.php
+++ includes/external/paypal/classes/PayPalPaymentBase.php
@@ -57,7 +57,7 @@ class PayPalPaymentBase extends PayPalCommon {
     global $order;
 
     $this->code = $class;
-    $this->paypal_version = '1.102';
+    $this->paypal_version = '1.103';
 
     $this->admin_access_array = array(
       'paypal_info',
